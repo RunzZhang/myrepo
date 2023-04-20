a283d41a7e0e6ca8b93a39b3d10fff34eb4a3498
RunzZhang <52052572+RunzZhang@users.noreply.github.com>
2023-04-20 09:05:11 -0700

    Update momentum_conservation.py

--- momentum_conservation.py
+++ momentum_conservation.py
@@ -29,6 +29,11 @@ class momentum_check():
         self.gamma3_p[3] = self.gamma3_Info[2]
         self.gamma4_p[3] = self.gamma4_Info[2]
         self.neutron_p[3] = self.neutron_Info[2]
+        print("gamma1_p",self.gamma1_p)
+        print("gamma2_p", self.gamma1_p)
+        print("gamma3_p", self.gamma1_p)
+        print("gamma4_p", self.gamma1_p)
+        print("neutron_p", self.neutron_p)
     def check_momentum(self):
         neutron_x_s = (2*939*41*self.neutron_p[3])**0.5*self.neutron_p[0]/(self.neutron_p[0]**2+self.neutron_p[1]**2+self.neutron_p[2]**2)**0.5
         neutron_y_s = (2 * 939 * 41*self.neutron_p[3]) ** 0.5 * self.neutron_p[1] / (
@@ -41,8 +46,10 @@ class momentum_check():
 
         for i in range(0,4):
             gamma_x_s += self.gamma_list[i][0]*self.gamma_list[i][3]/(self.gamma_list[i][0]**2+self.gamma_list[i][1]**2+self.gamma_list[i][2]**2)**0.5
+
             gamma_y_s += self.gamma_list[i][1] * self.gamma_list[i][3] / (
                         self.gamma_list[i][0] ** 2 + self.gamma_list[i][1] ** 2 + self.gamma_list[i][2] ** 2) ** 0.5
+
             gamma_z_s += self.gamma_list[i][2] * self.gamma_list[i][3] / (
                         self.gamma_list[i][0] ** 2 + self.gamma_list[i][1] ** 2 + self.gamma_list[i][2] ** 2) ** 0.5
 
