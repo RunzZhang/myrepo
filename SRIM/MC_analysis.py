24698bbbb263dec8a23e83f81b18cb9bb7a4189a
RunzZhang <52052572+RunzZhang@users.noreply.github.com>
2023-10-18 11:43:14 -0500

    Update MC_analysis.py

--- SRIM/MC_analysis.py
+++ SRIM/MC_analysis.py
@@ -11,7 +11,11 @@ e = 1.602*10**(-19)
 address1 ="/data/runzezhang/result/SRIM_MC/MC_20231018_5582"
 address2 ="/data/runzezhang/result/SRIM_MC/MC_20231018_4745"
 address3 ="/data/runzezhang/result/SRIM_MC/MC_20231018_3700"
+weight1 = 1
+weight2 = 1
+weight3 = 1
 address_list = [address1,address2,address3]
+weight_list = [weight1, weight2, weight3]
 
 
 def data_pick(address):
@@ -36,6 +40,35 @@ def plot_chains():
     plt.xlabel("energy/eV")
     plt.ylabel("N/bin")
     plt.show()
+
+def plot_chain_sum():
+    raw_data = []
+    bin_n = 500
+    raw_data_ev = []
+    consistant_data = []
+    consistant_weight =[]
+    for address in address_list:
+        raw_data.append(data_pick(address))
+    # raw data is a data list
+    # change data value from J into eV
+    for i in range(len(raw_data)):
+        for j in range(len(raw_data[i])):
+            # add eV energy to a 1D list
+            #together with its weights
+            consistant_data.append(raw_data[i][j]/e)
+            consistant_weight.append(weight_list[i])
+    plt.hist(consistant_data,bins = bin_n, density = True, weight = consistant_weight)
+    plt.xlabel("energy/eV")
+    plt.ylabel("P")
+    plt.show()
+
+if __name__ =="__main__":
+    plot_chains()
+    # plot_chain_sum()
+
+
+
+
 
 
 
