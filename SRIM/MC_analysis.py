1c9516ed47814f6ab1028402d295f52482f94313
RunzZhang <52052572+RunzZhang@users.noreply.github.com>
2023-10-19 10:19:54 -0500

    Update MC_analysis.py

--- SRIM/MC_analysis.py
+++ SRIM/MC_analysis.py
@@ -36,6 +36,7 @@ def plot_chains():
     for data in raw_data:
         for data_ele in data:
             data_buffer.append(round(data_ele/e,3))
+            print(data_ele/e)
         raw_data_ev.append(data_buffer)
         print("finish one run")
     print(raw_data_ev[0])
