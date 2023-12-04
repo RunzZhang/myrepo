aedadbe5b0b154e76cdb14dfe79d56c91f4abaf6
RunzZhang <52052572+RunzZhang@users.noreply.github.com>
2023-12-04 11:07:45 -0600

    Update TN_MC_full_argon.py

--- SRIM/TN_MC_full_argon.py
+++ SRIM/TN_MC_full_argon.py
@@ -86,7 +86,7 @@ class MC_sim_full_argon():
         self.gamma_emission_list_1d = []
         self.gamma_emission_list_2d = []
         # self.gamma_sim(10000)
-        # self.MC_sim(self.runtime)
+        self.MC_sim(self.runtime)
         self.data_analysis(self.address)
 
 
