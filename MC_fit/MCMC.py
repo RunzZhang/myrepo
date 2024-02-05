d0a54c9f76f7a263bf64582a97a1a62db379ffdd
RunzZhang <52052572+RunzZhang@users.noreply.github.com>
2024-02-05 14:44:19 -0600

    Update MCMC.py

--- MC_fit/MCMC.py
+++ MC_fit/MCMC.py
@@ -154,6 +154,7 @@ class multi_MC():
         print("2d nodes", nodes_2D)
         node_2d_array = np.array(nodes_2D)
         for i in range(len(nodes_2D[0])):
+            print(i)
             mean_node[i] = (np.mean(node_2d_array[i,:]))
             sigma_node[i] = (np.std(node_2d_array[i, :]))
             max_node[i] = mean_node[i]+sigma_node[i]
