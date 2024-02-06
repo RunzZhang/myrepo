19a5851a25404ce45aa0b4b58068fa18deb868e3
RunzZhang <52052572+RunzZhang@users.noreply.github.com>
2024-02-06 10:46:37 -0600

    Update MCMC.py

--- MC_fit/MCMC.py
+++ MC_fit/MCMC.py
@@ -113,7 +113,7 @@ class multi_MC():
             return R
         """
         # self.main(self.flist, self.alist, self.pnlist, self.aplist)
-        # self.multirun(self.runN)
+        self.multirun(self.runN)
         self.analysis_results(self.runN)
 
             
