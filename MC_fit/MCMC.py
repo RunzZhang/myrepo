40df986cea1fe1c7cc50782e9bcdfa2c64049a42
RunzZhang <52052572+RunzZhang@users.noreply.github.com>
2024-02-08 11:38:16 -0600

    Update MCMC.py

--- MC_fit/MCMC.py
+++ MC_fit/MCMC.py
@@ -91,7 +91,7 @@ class multi_MC():
 
         self.runN = 20
         self.runlist =[]
-        self.threshold = 299
+        self.threshold = 413
         self.sig_high = 50
         self.sig_low = 50
         self.ceil =1.5* self.threshold
@@ -101,7 +101,7 @@ class multi_MC():
             self.fileprefix = sys.argv[1]
         except:
             self.fileprefix = "./Test_Dump/test2"
-        self.save_path = "/data/runzezhang/result/chi2_test_TN299/"
+        self.save_path = "/data/runzezhang/result/chi2_test_TN413/"
         """ 
         def NucleationEfficiency(r,T,sigma):
             #A=1/2
