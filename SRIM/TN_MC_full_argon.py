ddb518e3c93588dbd53f9ce75450ad57aa5c6386
RunzZhang <52052572+RunzZhang@users.noreply.github.com>
2024-02-21 13:21:12 -0600

    Update TN_MC_full_argon.py

--- SRIM/TN_MC_full_argon.py
+++ SRIM/TN_MC_full_argon.py
@@ -458,6 +458,8 @@ class MC_sim_full_argon():
                     x_bins_high.append(2*x_bins[i])
                 elif y_bins[i]<= y_bins_high[k] and y_bins[i] > y_bins_high[k+1]:
                     x_bins_high.append(x_bins[k])
+                else:
+                    print("skipped", k)
 
         plt.plot(x_bins, x_bins, color="blue", label='ideal reconsctruct E threshold')
         plt.plot(x_bins,x_bins_low,color = "red", label = 'reconstruct low limit with uncertainty '+ str(uncertainty))
