2d1aee18391128bff0da2f3212b0b8d070f38e96
RunzZhang <52052572+RunzZhang@users.noreply.github.com>
2024-02-28 10:39:38 -0600

    Update MCMC.py

--- MC_fit/MCMC.py
+++ MC_fit/MCMC.py
@@ -101,7 +101,7 @@ class multi_MC():
             self.fileprefix = sys.argv[1]
         except:
             self.fileprefix = "./Test_Dump/test2"
-        self.save_path = "/data/runzezhang/result/chi2_test_fixedNCrate_TN400_v2/"
+        self.save_path = "/data/runzezhang/result/chi2_test_fixedNCrate_TN700_v2/"
         """ 
         def NucleationEfficiency(r,T,sigma):
             #A=1/2
