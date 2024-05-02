c7c1cca23f93f8bf995e3f6a327f8105331085a1
RunzZhang <52052572+RunzZhang@users.noreply.github.com>
2024-05-02 11:34:22 -0500

    Update readroot.py

--- readroot.py
+++ readroot.py
@@ -2,6 +2,6 @@ import uproot
 filename = "/data/runzezhang/Geant4Simulaions/g411_TN/dmx.root"
 file = uproot.open(filename)["tree"]
 print(file.keys())
-df = file.arrays(["Hit", "x"], library="pd")
-Capture = df['Hit'].tolist()
-Elastic = df['x'].tolist()
+df = file.arrays(["Event", "x"], library="pd")
+Capture = df['Event'].tolist()
+# Elastic = df['x'].tolist()
