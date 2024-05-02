96cb79ff9546ce6653b40281e6de91dddca2d0ce
RunzZhang <52052572+RunzZhang@users.noreply.github.com>
2024-05-02 11:44:49 -0500

    Update readroot.py

--- readroot.py
+++ readroot.py
@@ -1,6 +1,7 @@
 import uproot
 filename = "/data/runzezhang/Geant4Simulaions/g411_TN/dmx.root"
 file = uproot.open(filename)["tree"]
+print(file)
 print(file.keys())
 df = file.arrays(["Event"], library="pd")
 # df = file.arrays(["Event", "x"], library="pd")
