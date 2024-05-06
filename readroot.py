d5806feaaf93ac2f57f585d8918ed4ba24c38050
RunzZhang <52052572+RunzZhang@users.noreply.github.com>
2024-05-06 13:41:48 -0500

    Update readroot.py

--- readroot.py
+++ readroot.py
@@ -1,17 +1,17 @@
 """This file is for analyze the root files"""
 
 import uproot
-# filename = "/data/runzezhang/Geant4Simulaions/g411_TN/dmx.root"
-filename = "/data/runzezhang/result/TN_sims/dmx.root"
-file = uproot.open(filename)["tree"]
-print(file)
-print(file.keys())
-
-df = file.arrays(["Event","name","Parent ID","Track ID","Step ID","X/mm","Kinetic/keV","Recoiled/keV","Volume","Process"], library="pd")
-df = df.head(1000)
-# df = file.arrays(["Event", "x"], library="pd")
-Capture = df['Event'].tolist()
-print(df)
+# # filename = "/data/runzezhang/Geant4Simulaions/g411_TN/dmx.root"
+# filename = "/data/runzezhang/result/TN_sims/dmx.root"
+# file = uproot.open(filename)["tree"]
+# print(file)
+# print(file.keys())
+#
+# df = file.arrays(["Event","name","Parent ID","Track ID","Step ID","X/mm","Kinetic/keV","Recoiled/keV","Volume","Process"], library="pd")
+# df = df.head(1000)
+# # df = file.arrays(["Event", "x"], library="pd")
+# Capture = df['Event'].tolist()
+# print(df)
 
 class ReadRoot():
     def __init__(self):
