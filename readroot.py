32a50f5292f36884adb438fe87bd6383caa36501
RunzZhang <52052572+RunzZhang@users.noreply.github.com>
2024-05-06 14:13:40 -0500

    Update readroot.py

--- readroot.py
+++ readroot.py
@@ -3,6 +3,37 @@
 import uproot
 # filename = "/data/runzezhang/Geant4Simulaions/g411_TN/dmx.root"
 
+class RestructureRoot():
+    def __init__(self):
+        self.filepath = "/data/runzezhang/result/TN_sims/dmx.root"
+        self.reconstruct_filepath = "/data/runzezhang/result/TN_sims/dmx_rc.root"
+        self.file = uproot.open(self.filepath)["tree"]
+        print("columns: ",self.file.keys())
+        #['Event', 'name', 'Parent ID', 'Track ID', 'Step ID', 'X/mm', 'Y/mm', 'Z/mm', 'Kinetic/keV', 'Recoiled/keV', 'Volume', 'Process']
+        self.selected_columns = ["Event","name","Parent ID","Track ID","Step ID","X/mm","Kinetic/keV","Recoiled/keV","Volume","Process"]
+        self.rows = 1000
+        # self.df = self.file.arrays(self.selected_columns, library="pd").head(self.rows)
+        self.df = self.file.arrays(self.selected_columns, library="pd")
+
+        self.reconstruct()
+
+    def reconstruct(self):
+        event_number = self.df[:]["Event"]
+        print(event_number[:20])
+        started_point = 0
+        temp_point = 1
+        # find event number 1's index
+        for i in range(0,len(event_number)):
+            if event_number[i]==1:
+                started_point = i
+        print("start",started_point)
+         # then if there is 0 in the event number, replace it with last none-zero event number
+        for i in range(started_point, len(event_number)):
+            if event_number[i]!=0:
+                temp_point = event_number
+            else:
+                event_number[i] = temp_point
+        print("end",event_number[:20])
 
 class ReadRoot():
     def __init__(self):
@@ -30,4 +61,5 @@ class ReadRoot():
 
 
 if __name__ =="__main__":
-    RR = ReadRoot()
+    ReR = RestructureRoot()
+    # RR = ReadRoot()
