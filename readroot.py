82670c4979df23cb72c881757c11e4131248861c
RunzZhang <52052572+RunzZhang@users.noreply.github.com>
2024-05-06 13:41:14 -0500

    Update readroot.py

--- readroot.py
+++ readroot.py
@@ -1,24 +1,32 @@
+"""This file is for analyze the root files"""
+
 import uproot
 # filename = "/data/runzezhang/Geant4Simulaions/g411_TN/dmx.root"
 filename = "/data/runzezhang/result/TN_sims/dmx.root"
 file = uproot.open(filename)["tree"]
 print(file)
 print(file.keys())
-# "Event");
-#   man->CreateNtupleDColumn("name");
-#   man->CreateNtupleDColumn("Parent ID");
-#   man->CreateNtupleDColumn("Track ID");
-#   man->CreateNtupleDColumn("Step ID");
-#   man->CreateNtupleDColumn("X/mm");
-#   man->CreateNtupleDColumn("Y/mm");
-#   man->CreateNtupleDColumn("Z/mm");
-#   man->CreateNtupleSColumn("Kinetic/keV");
-#   man->CreateNtupleDColumn("Recoiled/keV");
-#   man->CreateNtupleDColumn("Volume");
-#   man->CreateNtupleDColumn("Process")
+
 df = file.arrays(["Event","name","Parent ID","Track ID","Step ID","X/mm","Kinetic/keV","Recoiled/keV","Volume","Process"], library="pd")
 df = df.head(1000)
 # df = file.arrays(["Event", "x"], library="pd")
 Capture = df['Event'].tolist()
 print(df)
-# Elastic = df['x'].tolist()
+
+class ReadRoot():
+    def __init__(self):
+        self.filepath = "/data/runzezhang/result/TN_sims/dmx.root"
+        self.file = uproot.open(self.filepath)["tree"]
+        print("columns: ",self.file.keys())
+        #['Event', 'name', 'Parent ID', 'Track ID', 'Step ID', 'X/mm', 'Y/mm', 'Z/mm', 'Kinetic/keV', 'Recoiled/keV', 'Volume', 'Process']
+        self.selected_columns = ["Event","name","Parent ID","Track ID","Step ID","X/mm","Kinetic/keV","Recoiled/keV","Volume","Process"]
+        self.rows = 1000
+        self.df = self.file.arrays(self.selected_columns, library="pd").head(self.rows)
+        print(self.df)
+
+
+
+
+
+if __name__ =="__main__":
+    RR = ReadRoot()
