bdbdb10e4ea0eb91de671ec2e67576957008a94e
RunzZhang <52052572+RunzZhang@users.noreply.github.com>
2024-05-06 13:49:19 -0500

    Update readroot.py

--- readroot.py
+++ readroot.py
@@ -12,7 +12,8 @@ class ReadRoot():
         #['Event', 'name', 'Parent ID', 'Track ID', 'Step ID', 'X/mm', 'Y/mm', 'Z/mm', 'Kinetic/keV', 'Recoiled/keV', 'Volume', 'Process']
         self.selected_columns = ["Event","name","Parent ID","Track ID","Step ID","X/mm","Kinetic/keV","Recoiled/keV","Volume","Process"]
         self.rows = 1000
-        self.df = self.file.arrays(self.selected_columns, library="pd").head(self.rows)
+        # self.df = self.file.arrays(self.selected_columns, library="pd").head(self.rows)
+        self.df = self.file.arrays(self.selected_columns, library="pd")
         print(self.df)
         self.process_summary()
 
