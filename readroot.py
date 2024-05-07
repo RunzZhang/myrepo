3671f173097c103923094dbc1bd124088f08c2f3
RunzZhang <52052572+RunzZhang@users.noreply.github.com>
2024-05-07 11:39:43 -0500

    Update readroot.py

--- readroot.py
+++ readroot.py
@@ -162,7 +162,7 @@ class ReadRoot():
         self.df_Gamma = pd.DataFrame()
         for index in range(len(self.df_Ncapture.index)):
             temp_df = self.df[
-                (self.df['name'] == 'gamma') ][
+                (self.df['name'] == 'gamma') &(self.df['Event']==self.df_Ncapture.loc[index,'Event'])][
                 ["name", "Kinetic/keV", "Volume"]]
             # temp_df = self.df[(self.df['name']=='gamma')&(self.df['Event']==self.df_Ncapture.loc[index,'Event'])&(self.df['Parent ID']==self.df_Ncapture.loc[index,'Track ID'])][["name","Kinetic/keV","Volume"]]
             self.df_Gamma = pd.concat([self.df_Gamma, temp_df], axis=0,ignore_index=True)
