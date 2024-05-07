b393d1713f27bf27be470622e7d4fcd2555372cb
RunzZhang <52052572+RunzZhang@users.noreply.github.com>
2024-05-07 10:50:25 -0500

    Update readroot.py

--- readroot.py
+++ readroot.py
@@ -152,6 +152,7 @@ class ReadRoot():
                 volume_clean.append(element)
         print(volume_clean)
     def Capture_spectrum(self):
+        print(self.df.dtypes)
         self.df_Ncapture = self.df[(self.df["name"]=='neutron')&(self.df["Process"]=='nCapture')&(self.df["Volume"]!='LAr_phys')][['Event','Track ID']]
         # self.df_Ncapture = self.df[(self.df["name"]=='neutron')&(self.df["Process"]=='nCapture')&(self.df["Volume"]!='LAr_phys')]
         # ["Event", 'Track ID']
