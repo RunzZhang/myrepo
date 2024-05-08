824749d78f30ce2592a8a021020c3561b993eefa
RunzZhang <52052572+RunzZhang@users.noreply.github.com>
2024-05-08 15:08:45 -0500

    Update readroot.py

--- readroot.py
+++ readroot.py
@@ -196,7 +196,7 @@ class ReadRoot():
         parent_p = []
 
         for index in range(len(self.gamma_Scint.index)):
-
+            print(index)
             if self.gamma_Scint.iloc[index]['Event']> event_p:
                 event_p = self.gamma_Scint.iloc[index]['Event']
                 track_p = []
