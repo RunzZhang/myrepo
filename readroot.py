95ccd8843f35f107dbf5e6758c79b4f2160c2817
RunzZhang <52052572+RunzZhang@users.noreply.github.com>
2024-05-08 11:07:53 -0500

    Update readroot.py

--- readroot.py
+++ readroot.py
@@ -177,6 +177,8 @@ class ReadRoot():
     def test_merge(self):
         df_a = pd.DataFrame({'A': [2], 'B':[3],'C':[5], 'D':[8]})
         df_b = pd.DataFrame({'B': [2,3,6,3], 'C': [3,5,9,5], 'F': [5,9,10,6], 'G': [8,10,7,9]})
+        print('a',df_a)
+        print('b',df_b)
         merged_df = pd.merge(df_b, df_a, on=['B','C'], how='inner')
         print(merged_df)
 
