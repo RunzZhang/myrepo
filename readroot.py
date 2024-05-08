216655f0c36989fca25db7c98ab823d1cacaa362
RunzZhang <52052572+RunzZhang@users.noreply.github.com>
2024-05-08 11:54:01 -0500

    Update readroot.py

--- readroot.py
+++ readroot.py
@@ -105,11 +105,11 @@ class ReadRoot():
         # self.df = self.file.arrays(self.selected_columns, library="pd").head(self.rows)
         # # process data so that it is easier to read
         self.df = self.file.arrays(self.selected_columns, library="pd")
-        # print(self.df)
-        self.reidx_event()
+        print(self.df["Kinetic/keV"].head(20))
+        # self.reidx_event()
         # self.string_summary()
-        self.Capture_spectrum()
-        self.Gamma_spectrum()
+        # self.Capture_spectrum()
+        # self.Gamma_spectrum()
     # there was some 0 in event columns, set them to corresponding value
     # for example 001002003 will be 001112223
     def reidx_event(self):
