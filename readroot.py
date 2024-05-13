924dee837353520f42ddf1083b9379d9ecb79b5f
RunzZhang <52052572+RunzZhang@users.noreply.github.com>
2024-05-13 14:36:53 -0500

    Update readroot.py

--- readroot.py
+++ readroot.py
@@ -356,9 +356,7 @@ class ReadRoot():
         # select all neutron events
         self.df_neutron = self.df[self.df['name'] == 'neutron']
         print(self.df_neutron.head(10))
-        # 191 first elastic scatter and recoil argon and then capture by other volume
-        # self.df_test_merge = self.df[(self.df["Event"]==191)&((self.df["name"]=='Ar36')|(self.df["name"]=='Ar37')|(self.df["name"]=='Ar40')|(self.df["name"]=='Ar41')|(self.df["name"]=='neutron'))]
-        # self.df_test_merge.to_csv("/data/runzezhang/result/TN_sims/dmx_argon_ncap_test.csv")
+
         # select neutron events whose Event number is same as argon event and track id is argon's parent ID
         self.df_neutron_mom = pd.merge(self.df_neutron, self.df_Ar_recoil_merge,on=['Event','Track ID'], how='inner')
         print(self.df_neutron_mom.head(20))
@@ -379,7 +377,9 @@ class ReadRoot():
         # find the first argon recoiled
 
         self.df_argon_ela_merged = self.keep_1st(self.df_argon_ela_merged,["Event", "Track ID"])
-
+        # 168 first elastic scatter and recoil argon and then capture by other volume
+        self.df_test_merge = self.df[(self.df["Event"]==168)&((self.df["name"]=='Ar36')|(self.df["name"]=='Ar37')|(self.df["name"]=='Ar40')|(self.df["name"]=='Ar41')|(self.df["name"]=='neutron'))]
+        self.df_test_merge.to_csv("/data/runzezhang/result/TN_sims/dmx_argon_ncap_test.csv")
         print(self.df_argon_ela_merged.head(20), "\nlen",len(self.df_argon_ela_merged.index))
         # save these gamma event
         self.df_argon_ela_merged.to_csv("/data/runzezhang/result/TN_sims/dmx_argon_elastic.csv", index=False)
