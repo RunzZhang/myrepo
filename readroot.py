3fcf179b4e879b70ffe071a126d6ac30b2df620d
RunzZhang <52052572+RunzZhang@users.noreply.github.com>
2024-05-13 11:30:52 -0500

    Update readroot.py

--- readroot.py
+++ readroot.py
@@ -356,14 +356,14 @@ class ReadRoot():
         # select all neutron events
         self.df_neutron = self.df[self.df['name'] == 'neutron']
         print(self.df_neutron.head(10))
-        # 191
-        self.df_test_merge = self.df[(self.df["Event"]==191)&((self.df["name"]=='Ar36')|(self.df["name"]=='Ar37')|(self.df["name"]=='Ar40')|(self.df["name"]=='Ar41')|(self.df["name"]=='neutron'))]
-        self.df_test_merge.to_csv("/data/runzezhang/result/TN_sims/dmx_argon_ncap_test.csv")
+        # 191 first elastic scatter and recoil argon and then capture by other volume
+        # self.df_test_merge = self.df[(self.df["Event"]==191)&((self.df["name"]=='Ar36')|(self.df["name"]=='Ar37')|(self.df["name"]=='Ar40')|(self.df["name"]=='Ar41')|(self.df["name"]=='neutron'))]
+        # self.df_test_merge.to_csv("/data/runzezhang/result/TN_sims/dmx_argon_ncap_test.csv")
         # select neutron events whose Event number is same as argon event and track id is argon's parent ID
         self.df_neutron_mom = pd.merge(self.df_neutron, self.df_Ar_recoil_merge,on=['Event','Track ID'], how='inner')
         print(self.df_neutron_mom.head(20))
         print("neutron unique", self.df_neutron_mom['Process'].unique())
-        self.df_neutron_ncap = self.df_neutron_mom[self.df_neutron_mom['Process']=='nCapture']
+        self.df_neutron_ncap = self.df_neutron_mom[(self.df_neutron_mom['Process']=='nCapture')&(self.df_neutron_mom['Volume']=='LAr_phys')]
         self.df_neutron_ncap = self.keep_1st(self.df_neutron_ncap, ["Event", "Track ID"])
         self.df_neutron_ncap.to_csv("/data/runzezhang/result/TN_sims/dmx_argon_ncap.csv")
         print("neutron cap", len(self.df_neutron_ncap.index))
