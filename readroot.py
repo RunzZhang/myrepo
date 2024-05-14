0c7bb4dda7055b932cb4d5e7629727b940127a10
RunzZhang <52052572+RunzZhang@users.noreply.github.com>
2024-05-14 15:35:07 -0500

    Update readroot.py

--- readroot.py
+++ readroot.py
@@ -246,7 +246,7 @@ class ReadRoot():
             (self.df_gamma_rw['Volume'] == 'LAr_phys') | (self.df_gamma_rw['Volume'] == 'hydraulic_fluid_phys')]
         self.gamma_Scint = self.keep_1st(self.gamma_Scint)
         print("scint",self.gamma_Scint)
-        print("scint2",self.gamma_Scint[self.gamma_Scint["Parent ID"]==1])
+        print("scint2",self.gamma_Scint[self.gamma_Scint["Parent ID"]!=1])
     def plot_gamma(self):
         self.gamma = pd.read_csv(self.base_path +"gamma_scint2.csv")
         # add gamma energy together for same event
