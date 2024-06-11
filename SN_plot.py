6b8d9fa9bac04d7a4646d1b9099065327ab07927
RunzZhang <52052572+RunzZhang@users.noreply.github.com>
2024-06-11 13:49:07 -0500

    Update SN_plot.py

--- SN_plot.py
+++ SN_plot.py
@@ -134,7 +134,7 @@ class SN():
 
         lines = [line1, line2, line3]
         labels = [line.get_label() for line in lines]
-        fig.legend(lines, labels, loc='upper right', bbox_to_anchor=(0.75, 0.75))
+        fig.legend(lines, labels, loc='upper right', bbox_to_anchor=(0.9, 0.75))
         # Show the plot
         plt.show()
 
