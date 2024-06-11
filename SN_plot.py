9212ec6b6b4cac170b45619065da060ae1d11336
RunzZhang <52052572+RunzZhang@users.noreply.github.com>
2024-06-11 12:48:28 -0500

    Update SN_plot.py

--- SN_plot.py
+++ SN_plot.py
@@ -67,7 +67,7 @@ class SN():
     #     print("len",len(self.noise_p_raw_list))
     def hist_info(self):
 
-        plt.hist(self.noise_raw_list,color='blue',label='noise')
+        # plt.hist(self.noise_raw_list,color='blue',label='noise')
         plt.hist(self.sig_raw_list, color="red", label='signal')
         plt.xlabel("photon detected by SiPM #", fontsize=16)
         plt.ylabel("signal/noise rate #/s", fontsize=16)
