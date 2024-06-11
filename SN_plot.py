b7075c489275bf03504de267e781de770ac7526f
RunzZhang <52052572+RunzZhang@users.noreply.github.com>
2024-06-11 13:13:39 -0500

    Update SN_plot.py

--- SN_plot.py
+++ SN_plot.py
@@ -9,7 +9,7 @@ class SN():
 
 
     def old_read_files(self):
-        self.rate = 1
+        self.rate = 435.6 #/s
         self.G4_events= 1E6
         self.G4_sig_time=(self.G4_events / self.rate)
         with open("/data/runzezhang/result/TN_e_sims/Ar_photon_CF.csv", 'r') as file:
@@ -24,7 +24,7 @@ class SN():
         # self.sig_raw_list = list(map(float, self.sig_raw_list))
 
         print( len(self.sig_raw_list))
-        self.G4_noise_time = 1
+        self.G4_noise_time = 1E7/self.rate
         with open("/data/runzezhang/result/TN_e_sims/scatter_spectrum_CF.csv", 'r') as file:
             reader = csv.reader(file)
             # Read the first row (assuming single row for simplicity)
