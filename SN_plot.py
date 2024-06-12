15a6f211fc887d49a5ca124c63240d1b8db969bd
RunzZhang <52052572+RunzZhang@users.noreply.github.com>
2024-06-12 16:54:39 -0700

    Update SN_plot.py

--- SN_plot.py
+++ SN_plot.py
@@ -11,11 +11,11 @@ class SN():
     def old_read_files(self):
         # self.capture_ratio = 1.164E-3 # 1125eV
         self.capture_ratio = 0.121 # 400 eV
-        # self.rate = 435.6 #/s # CF neutron rate
-        self.rate = 0.56 #AmLi neutron rate
+        self.rate = 435.6 #/s # CF neutron rate
+        # self.rate = 0.56 #AmLi neutron rate
         self.G4_events= 1E6
         self.G4_sig_time=(self.G4_events / self.rate)
-        with open("/data/runzezhang/result/TN_e_sims/Ar_photon_AmLi.csv", 'r') as file:
+        with open("/data/runzezhang/result/TN_e_sims/Ar_photon_CF.csv", 'r') as file:
             reader = csv.reader(file)
             # Read the first row (assuming single row for simplicity)
             number_list = next(reader)
@@ -26,7 +26,7 @@ class SN():
         #
         # self.sig_raw_list = list(map(float, self.sig_raw_list))
 
-        print( len(self.sig_raw_list))
+        print("capture event number", len(self.sig_raw_list))
         self.G4_noise_time = 1E7/self.rate
         with open("/data/runzezhang/result/TN_e_sims/scatter_spectrum_AmLi.csv", 'r') as file:
             reader = csv.reader(file)
@@ -45,7 +45,7 @@ class SN():
         threshold_list  = []
         for i in range(0,max_noise_photon):
             threshold_list.append(i)
-        self.plot_sn(threshold_list)
+        # self.plot_sn(threshold_list)
         # self.hist_info()
 
 
