57605e22457ce6ac6bdbdcde8f77417207ad51a7
RunzZhang <52052572+RunzZhang@users.noreply.github.com>
2024-06-12 20:43:59 -0700

    Update readroot.py

--- readroot.py
+++ readroot.py
@@ -374,6 +374,7 @@ class ReadRoot():
         # add gamma up
         self.electron_recoiled_list  = summed_values["Recoiled/keV"].to_list()
         p_observed = []
+        print("recoil list", len(self.electron_recoiled_list))
         for i in range(len(self.electron_recoiled_list)):
             # 40 /keV 0.03 and 0.2 PCE and PDE
             p_observed.append(self.electron_recoiled_list[i]*1E6*40*0.03*0.2/(1000))
