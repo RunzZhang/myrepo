12dad99b24e5fd10046899379e56a83165a7a947
RunzZhang <52052572+RunzZhang@users.noreply.github.com>
2024-06-12 22:26:38 -0700

    Update SN_plot.py

--- SN_plot.py
+++ SN_plot.py
@@ -9,9 +9,9 @@ class SN():
 
 
     def old_read_files(self):
-        # self.capture_ratio = 1.164E-3 # 1125eV
+        self.capture_ratio = 1.164E-3 # 1125eV
         self.capture_ratio = 0.121 # 400 eV
-        self.rate = 435.6 #/s # CF neutron rate
+        # self.rate = 435.6 #/s # CF neutron rate
         # self.rate = 0.56 #AmLi neutron rate
         self.G4_events= 1E6
         self.G4_sig_time=(self.G4_events / self.rate)
