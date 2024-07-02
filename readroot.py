36aafbb9e81132d0066f0ed7ca505e4d1fae390b
RunzZhang <52052572+RunzZhang@users.noreply.github.com>
2024-07-02 13:51:36 -0700

    Update readroot.py

--- readroot.py
+++ readroot.py
@@ -113,8 +113,8 @@ class ReadRoot():
         self.modify_df()
 
         # self.gamma_event()
-        self.LAr_gamma_event()
-
+        # self.LAr_gamma_event()
+        self.single_e_n_capture_event()
         # self.FN_spectrum_v2()
         # self.plot_elastic()
 
