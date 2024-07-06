17e9a5774715e84f9705305794a6288cf78e55da
RunzZhang <52052572+RunzZhang@users.noreply.github.com>
2024-07-05 18:38:01 -0700

    Update SN_plot.py

--- SN_plot.py
+++ SN_plot.py
@@ -150,7 +150,7 @@ class SN():
         plt.ylabel("signal/background rate #/s", fontsize=16)
         plt.yscale('log')
         print("sig total rate", len(sig_normalized_counts)*self.capture_ratio/self.G4_sig_time)
-        print("back", len(noise_normalized_counts)*noise_counts /self.G4_sig_time)
+        print("back", len(noise_counts /self.G4_sig_time))
         plt.legend()
         plt.show()
     def prepare(self, threshold):
