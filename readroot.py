1a93705850db1b3084ee48b0a60c5f43858240fa
RunzZhang <52052572+RunzZhang@users.noreply.github.com>
2024-07-23 16:14:00 -0700

    Update readroot.py

--- readroot.py
+++ readroot.py
@@ -330,7 +330,7 @@ class ReadRoot():
         n_list = self.last_cross["Event"].to_list()
         self.N_check = self.df[self.df["Event"].isin(self.last_cross_list) & (
                     (self.df["name"] == 'neutron') | (self.df["name"] == 'Ar40') | (self.df["name"] == 'Ar36'))]
-        self.N_check.to_csv(self.base_path + "dmx_single_n_gamma_CF_neutron_list_loop.csv", index=False)
+        self.N_check.to_csv(self.base_path + "dmx_single_n_gamma_CF_neutron_list_loop1.csv", index=False)
         # n_list = self.LAr_n_merged["Event"].to_list()
         # self.N_check = self.df[self.df["Event"].isin(n_list) & (
         #         (self.df["name"] == 'neutron') | (self.df["name"] == 'Ar40') | (self.df["name"] == 'Ar36'))]
