eb2060c094754a5a0120ef28825384863585953e
RunzZhang <52052572+RunzZhang@users.noreply.github.com>
2024-07-25 11:00:14 -0700

    Update readroot.py

--- readroot.py
+++ readroot.py
@@ -295,6 +295,8 @@ class ReadRoot():
                         self.df["Volume"] == 'LAr_phys')][
             ['Event', 'Track ID']]
         self.elcap = pd.merge(self.df_Ncapture, self.df_Nscatter_wo, on=['Event'], how='left', indicator=True)
+        print("cap wo", len(self.df_Ncapture["Event"].unique()))
+        print("Ela",len(self.df_Nscatter_wo["Event"].unique()))
         print("Elcap", len(self.elcap["Event"].unique()))
         print("compt", len(self.compt_scatter["Event"].unique()))
         print("inelastic", self.df_Ninelastic.head(10))
