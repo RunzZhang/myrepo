c0d0762825ef63b20faf49088995b5c8f01e7f1f
RunzZhang <52052572+RunzZhang@users.noreply.github.com>
2024-08-07 19:38:21 -0700

    Update Combined.py

--- Combined.py
+++ Combined.py
@@ -10,7 +10,7 @@ import matplotlib.ticker as ticker
 
 filename = "/data/runzezhang/result/TN_sims2/CF252BigCap6,10Compt.root" #Sap5cmx7.5cm3cmPad10cm25cmAmLi
 
-threshold = 0
+threshold = 1250
 highethreshold = 16700
 
 file = uproot.open(filename)["tree"] #From NCaptureCount
