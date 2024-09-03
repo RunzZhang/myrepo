b46297cca040fe02755c929eb02c75783b1b184e
RunzZhang <52052572+RunzZhang@users.noreply.github.com>
2024-09-03 13:23:17 -0700

    Update readroot.py

--- readroot.py
+++ readroot.py
@@ -413,10 +413,10 @@ class ReadRoot():
 
     def single_e_n_capture_event(self):
         # self.LAr_n_single_test()
-        self.Capture_n_scatter_spectrum()
-        self.single_n_find_gamma_e()
-        # self.Capture_n_scatter_spectrum_loop()
-        # self.single_n_find_gamma_e_loop()
+        # self.Capture_n_scatter_spectrum()
+        # self.single_n_find_gamma_e()
+        self.Capture_n_scatter_spectrum_loop()
+        self.single_n_find_gamma_e_loop()
     def Gamma_spectrum(self):
         self.df_gamma_rw = pd.read_csv(self.base_path +"dmx_gamma.csv")
         print(self.df_gamma_rw[["Kinetic/keV"]].head(20))
