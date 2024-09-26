8f40432fa6fb65fe5419912bcfb48a33cb3da1e7
RunzZhang <52052572+RunzZhang@users.noreply.github.com>
2024-09-26 10:57:14 -0700

    Update SN_plot.py

--- SN_plot.py
+++ SN_plot.py
@@ -5,8 +5,8 @@ import numpy as np
 class SN():
     def __init__(self):
         self.base_path = '/data/runzezhang/result/TN_sims3/'
-        self.old_read_files()
-        # self.read_files_s_to_N1()
+        # self.old_read_files()
+        self.read_files_s_to_N1()
 
 
     def old_read_files(self):
