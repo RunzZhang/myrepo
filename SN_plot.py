03bd7b1408f1e9709a2e72582ef2b020c52f1c1c
RunzZhang <52052572+RunzZhang@users.noreply.github.com>
2025-01-08 13:55:21 -0800

    Update SN_plot.py

--- SN_plot.py
+++ SN_plot.py
@@ -32,9 +32,9 @@ class SN():
         self.G4_noise_time = 1E7/self.rate
         # with open("/data/runzezhang/result/TN_e_sims/scatter_spectrum_CF.csv", 'r') as file:
         # Noise 1
-        with open(self.base_path + "photon_capture_n_sing_scatterg_AmLi.csv", 'r') as file:
+        # with open(self.base_path + "photon_capture_n_sing_scatterg_AmLi.csv", 'r') as file:
         # Noise 2
-        # with open(self.base_path + "n_huge_scatterg_CF.csv", 'r') as file:
+        with open(self.base_path + "n_huge_scatterg_CF.csv", 'r') as file:
             reader = csv.reader(file)
             # Read the first row (assuming single row for simplicity)
             number_list = next(reader)
