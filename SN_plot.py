a0d738a2bd86ea6d9c5f88cf5553f9f976d0fded
RunzZhang <52052572+RunzZhang@users.noreply.github.com>
2025-01-07 16:37:45 -0800

    Update SN_plot.py

--- SN_plot.py
+++ SN_plot.py
@@ -51,7 +51,9 @@ class SN():
         # form the threshold function
         threshold_list  = []
         for i in range(0,max_noise_photon):
+            print(i)
             threshold_list.append(i)
+        print("ready to generate graph")
         self.plot_sn(threshold_list)
         self.hist_info()
     def read_files_s_to_N1(self):
