93d8cee8fab9fab09d32d8ac1f04728ee46c069a
RunzZhang <52052572+RunzZhang@users.noreply.github.com>
2025-01-17 12:02:50 -0800

    Update readroot.py

--- readroot.py
+++ readroot.py
@@ -661,8 +661,8 @@ class ReadRoot():
 
     def Huge_scatter_event(self):
         # single scatter spectrum
-        # self.Huge_scatter_spectrum()
-        self.Huge_scatter_spectrum_CF()
+        self.Huge_scatter_spectrum()
+        # self.Huge_scatter_spectrum_CF()
         # self.Huge_scatter_spectrum_CF_fake()
 
     def LAr_gamma_event(self):
