5552e9bd51dfabde09896aad5cdc129fec7639cd
RunzZhang <52052572+RunzZhang@users.noreply.github.com>
2025-01-17 14:33:06 -0800

    Update SN_plot.py

--- SN_plot.py
+++ SN_plot.py
@@ -9,10 +9,10 @@ class SN():
         self.old_read_files()
         # self.read_files_s_to_N1()
 
-
+# main funtion we use
     def old_read_files(self):
-        # self.capture_ratio = 1.164E-3 # 1125eV
-        self.capture_ratio = 0.121 # 400 eV
+        self.capture_ratio = 1.164E-3 # 1125eV
+        # self.capture_ratio = 0.121 # 400 eV
         # self.rate = 435.6 #/s # CF neutron rate
         self.rate = 0.56 #AmLi neutron rate
         self.G4_events= 1E6
@@ -71,8 +71,8 @@ class SN():
         self.hist_info()
     def read_files_s_to_N1(self):
 
-        # self.capture_ratio = 1.164E-3 # 1125eV
-        self.capture_ratio = 0.121 # 400 eV
+        self.capture_ratio = 1.164E-3 # 1125eV
+        # self.capture_ratio = 0.121 # 400 eV
         # self.capture_ratio = 1 # no cut
         self.rate = 435.6 #/s # CF neutron rate
         # self.rate = 0.56 #AmLi neutron rate
@@ -242,7 +242,7 @@ class SN():
         labels = [line.get_label() for line in lines]
         fig.legend(lines, labels, loc='upper right', bbox_to_anchor=(0.9, 0.85))
         # Show the plot
-        name = "AmLi Signal Noise #2 400 eV"
+        name = "AmLi Signal Noise #2 1125 eV"
         plt.title(name, fontsize = 16)
         plt.savefig(self.plot_path + name)
         # plt.show()
