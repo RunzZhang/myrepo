cdadfb427459ca98b3cd9e7e10256fee9e3bc538
RunzZhang <52052572+RunzZhang@users.noreply.github.com>
2025-01-17 15:19:06 -0800

    Update readroot.py

--- readroot.py
+++ readroot.py
@@ -102,8 +102,8 @@ class ReadRoot():
         self.base_path = "/data/runzezhang/result/TN_sims3/"
         self.base_path2 = "/data/runzezhang/result/TN_sims3/"
         self.plot_path = '/data/runzezhang/result/TN_sims3/plot/'
-        self.filepath = self.base_path +"dmx_lr.root"
-        # self.filepath = self.base_path + "dmx_AmLi.root"
+        # self.filepath = self.base_path +"dmx_lr.root"
+        self.filepath = self.base_path + "dmx_AmLi.root"
         self.file = uproot.open(self.filepath)["tree"]
         print("columns: ",self.file.keys())
         #['Event', 'name', 'Parent ID', 'Track ID', 'Step ID', 'X/mm', 'Y/mm', 'Z/mm', 'Kinetic/keV', 'Recoiled/keV', 'Volume', 'Process']
