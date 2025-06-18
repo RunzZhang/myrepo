ece8a9d035391253f58ab812ddfa1ccfee847495
RunzZhang <52052572+RunzZhang@users.noreply.github.com>
2025-06-17 19:57:52 -0700

    1.5 density

--- SRIM/Eq_sol.py
+++ SRIM/Eq_sol.py
@@ -47,7 +47,7 @@ class E_loss_solve():
         self.Tar_Den = 1.597E+22 # atoms/cm3 = 1.06g/cm3
         self.alpha_inv = 137
         self.LSS_factor = 1.6918*(10**(-2))# LSS variable transformation by SRIM table from 1.06 density
-        self.test_TarDen = 1.597E+22 *0.5
+        self.test_TarDen = 1.597E+22 *1.5
         print("factor",self.factor*self.Tar_Den*10**8)
         print("LSS factor inverse", 1/self.LSS_factor)
 
