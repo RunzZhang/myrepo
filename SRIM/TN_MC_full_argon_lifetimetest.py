779224f7774821605a1682ed3156462f45aaf759
RunzZhang <52052572+RunzZhang@users.noreply.github.com>
2025-06-18 10:35:22 -0700

    05 full spectrum

--- SRIM/TN_MC_full_argon_lifetimetest.py
+++ SRIM/TN_MC_full_argon_lifetimetest.py
@@ -35,7 +35,7 @@ class MC_sim_full_argon():
         # self.address = "/data/runzezhang/result/SRIM_MC/MC_argon_el_full_20231107"
         # self.address = "/data/runzezhang/result/SRIM_MC/MC_argon_full_20231129_6299_-01"
         self.old_address = "/data/runzezhang/result/SRIM_MC/MC_argon_full_20231206_full"
-        self.address = "/data/runzezhang/result/SRIM_MC/MC_argon_full_20250601_D_ori_15"
+        self.address = "/data/runzezhang/result/SRIM_MC/MC_argon_full_20250601_D_ori_05"
         # to change the density, change the self.address and change the plot name/legend name and change the self.test_Target_Den in Eq_sol
         self.plot_address = "/data/runzezhang/result/New_density_MC/"
 
@@ -377,7 +377,7 @@ class MC_sim_full_argon():
         for i in range(len(hist_result_old[1]) - 1):
             x_bins_old.append((hist_result_old[1][i] + hist_result_old[1][i + 1]) / 2)
 
-        plt.plot(x_bins, hist_result[0], color="blue",label= "dominate branch ori 15 density")
+        plt.plot(x_bins, hist_result[0], color="blue",label= "dominate branch ori 05 density")
         plt.plot(x_bins_old, hist_result_old[0], color="orange", label="1.4g/cm3")
         plt.grid(True, which='both', linestyle='--', linewidth=0.5)
         plt.minorticks_on()
@@ -388,7 +388,7 @@ class MC_sim_full_argon():
         plt.xticks(fontsize=18)
         plt.xlim([0, 1200])
         plt.ylim([1E-5,0.1])
-        plot_name = 'D_ori_15D.png'
+        plot_name = 'D_ori_05D.png'
         plt.legend()
         plt.savefig(self.plot_address+plot_name, bbox_inches='tight')
         # plt.show()
