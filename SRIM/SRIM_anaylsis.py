37f8087ea6e844ced44f4a7e55894d65465aa969
RunzZhang <52052572+RunzZhang@users.noreply.github.com>
2025-06-28 21:33:12 -0700

    Update SRIM_anaylsis.py

--- SRIM/SRIM_anaylsis.py
+++ SRIM/SRIM_anaylsis.py
@@ -352,7 +352,7 @@ class SRIM_Table():
         self.energy_startpoint ="9.99999 eV"
         self.file_name_edit = self.file_name[0:-4] + '_edit.txt'
         self.address = "/data/runzezhang/result/New_density_MC/"
-        self.plot_name = "theory_data_compare.png"
+        self.plot_name = "theory_data_compare_fit.png"
 
 
         self.step = 200  # in ev
