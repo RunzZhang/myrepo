2d33126c6865dc95485bb74df5f6399d550ba969
RunzZhang <52052572+RunzZhang@users.noreply.github.com>
2025-06-28 17:09:26 -0700

    Update TN_MC_full_argon.py

--- SRIM/TN_MC_full_argon.py
+++ SRIM/TN_MC_full_argon.py
@@ -116,7 +116,7 @@ class MC_sim_full_argon():
         self.gamma_emission_list_1d = []
         self.gamma_emission_list_2d = []
         # self.gamma_sim(10000)
-        self.MC_sim(self.runtime)
+        # self.MC_sim(self.runtime)
         self.data_analysis(self.address)
         # self.plot_pile_up()
 
@@ -329,6 +329,7 @@ class MC_sim_full_argon():
         plt.xlim([0, 1200])
         plt.ylim([1E-5,0.1])
         plt.legend()
+        plt.savefig("/data/runzezhang/result/SRIM_MC/old_sepctrum.png")
         plt.show()
 
 
