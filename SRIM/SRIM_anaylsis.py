ba715fdea7854e15111e2f201975c6692c78a954
RunzZhang <52052572+RunzZhang@users.noreply.github.com>
2025-07-01 14:07:37 -0700

    Update SRIM_anaylsis.py

--- SRIM/SRIM_anaylsis.py
+++ SRIM/SRIM_anaylsis.py
@@ -385,7 +385,8 @@ class SRIM_Table():
         self.data_ini()
         self.fetch_data()
         self.compare_theory()
-        self.plot_data()
+        # self.plot_data()
+        self.plot_data_err() # estimate the uncertainty from time constant
 
     def data_ini(self):
 
