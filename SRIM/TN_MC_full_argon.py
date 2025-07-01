9f0b5d2405e32de8acbe2f10f8bb653bfd26e5f9
RunzZhang <52052572+RunzZhang@users.noreply.github.com>
2025-07-01 15:29:36 -0700

    08 new D

--- SRIM/TN_MC_full_argon.py
+++ SRIM/TN_MC_full_argon.py
@@ -37,7 +37,7 @@ class MC_sim_full_argon():
         # self.address = "/data/runzezhang/result/SRIM_MC/MC_argon_el_full_20231107"
         # self.address = "/data/runzezhang/result/SRIM_MC/MC_argon_full_20231129_6299_-01"
         self.old_address = "/data/runzezhang/result/SRIM_MC/MC_argon_full_20231206_full"
-        self.address = "/data/runzezhang/result/SRIM_MC/MC_argon_full_20250101_LSS08"
+        self.address = "/data/runzezhang/result/SRIM_MC/MC_argon_full_20250101_LSS07"
         self.plot_address = "/data/runzezhang/result/New_density_MC/"
 
 
@@ -373,7 +373,7 @@ class MC_sim_full_argon():
         for i in range(len(hist_result_old[1]) - 1):
             x_bins_old.append((hist_result_old[1][i] + hist_result_old[1][i + 1]) / 2)
 
-        plt.plot(x_bins, hist_result[0], color="blue",label= "LSS 0.8")
+        plt.plot(x_bins, hist_result[0], color="blue",label= "LSS 0.7")
         plt.plot(x_bins_old, hist_result_old[0], color="orange", label="1.4g/cm3")
         plt.grid(True, which='both', linestyle='--', linewidth=0.5)
         plt.minorticks_on()
@@ -384,7 +384,7 @@ class MC_sim_full_argon():
         plt.xticks(fontsize=18)
         plt.xlim([0, 1200])
         plt.ylim([1E-5,0.1])
-        plot_name = 'LSS08factor.png'
+        plot_name = 'LSS07factor.png'
         plt.legend()
         plt.savefig(self.plot_address+plot_name, bbox_inches='tight')
         # plt.show()
