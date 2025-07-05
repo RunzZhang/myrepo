6b43f6cfae720cb74ae9647558e917e6776b236d
RunzZhang <52052572+RunzZhang@users.noreply.github.com>
2025-07-05 10:49:21 -0700

    fixed all bugs

--- chunk_root_format.py
+++ chunk_root_format.py
@@ -22,8 +22,8 @@ class ReadRoot:
         self.signal_path_mid = self.base_path + self.signal_mid
         self.signal_path = self.base_path + self.signal
 
-        # self.filepath = self.base_path + "dmx_Cf_1E7.root"
-        self.filepath = self.base_path + "dmx_AmLi.root" # test
+        self.filepath = self.base_path + "dmx_Cf_1E7.root"
+        # self.filepath = self.base_path + "dmx_AmLi.root" # test
         self.tree_name = "tree"  # Assuming your TTree is named "tree"
 
         # Define the columns you want to read and write
@@ -32,7 +32,7 @@ class ReadRoot:
 
     def chunk_and_write_root(self, num_chunks=20, output_dir=None):
         if output_dir is None:
-            output_dir = os.path.join(self.base_path, "chunked_root_files_test")
+            output_dir = os.path.join(self.base_path, "chunked_root_files")
         os.makedirs(output_dir, exist_ok=True)
 
         with uproot.open(self.filepath) as file:
--- readrootlist.py
+++ readrootlist.py
@@ -1177,8 +1177,8 @@ class ReadRoot():
 
 if __name__ =="__main__":
     # ReR = RestructureRoot()
-    # RR = ReadRoot()
-    test_write()
+    RR = ReadRoot()
+    # test_write()
 
     # find corrupted file entries
     # num= find_entries()
