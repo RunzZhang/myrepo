bc59f0df2524c8859c77529a38c3a5d18fa3a979
RunzZhang <52052572+RunzZhang@users.noreply.github.com>
2025-07-05 10:29:46 -0700

    Update readrootlist.py

--- readrootlist.py
+++ readrootlist.py
@@ -166,7 +166,7 @@ class ReadRoot():
         # self.df = self.file.arrays(self.selected_columns, library="pd").head(self.rows)
         # # process data so that it is easier to read
         # first 1000 rows
-        self.df = self.file.arrays(self.selected_columns, library="pd", entry_start=0,entry_stop=30633066)
+        self.df = self.file.arrays(self.selected_columns, library="pd", entry_start=30633056,entry_stop=30633066)
         self.modify_df()
 
 
