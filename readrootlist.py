ead6e024c047dc2019616dc66d3ee5ff0dfdf56c
RunzZhang <52052572+RunzZhang@users.noreply.github.com>
2025-07-05 10:26:59 -0700

    Update readrootlist.py

--- readrootlist.py
+++ readrootlist.py
@@ -157,7 +157,7 @@ class ReadRoot():
 
         self.filepath = self.base_path + f"dmx_Cf_1E7_{i}.root"
         self.file = uproot.open(self.filepath)["tree"]
-        print("columns: ", self.file.keys())
+        # print("columns: ", self.file.keys())
         # ['Event', 'name', 'Parent ID', 'Track ID', 'Step ID', 'X/mm', 'Y/mm', 'Z/mm', 'Kinetic/keV', 'Recoiled/keV', 'Volume', 'Process']
         self.selected_columns = ["Event", "name", "Parent ID", "Track ID", "Step ID", "X/mm", "Kinetic/keV",
                                  "Recoiled/keV", "Volume", "Process"]
