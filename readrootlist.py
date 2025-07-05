e469cb4e91e883e8015a42dfea2efc514cb9f0fa
RunzZhang <52052572+RunzZhang@users.noreply.github.com>
2025-07-05 10:13:05 -0700

    Update readrootlist.py

--- readrootlist.py
+++ readrootlist.py
@@ -64,9 +64,9 @@ def test_write():
         tree = f["tree"]  # Or whatever your tree name is
         print("Successfully opened the file!")
         # Optional: Try to read a few entries to confirm data is there
-        df_test = tree.arrays(["Event", "Kinetic/keV"], library="pd", entry_start=10496663981109070736 ,entry_stop=10496663981109071745)
-        # df_test = tree.arrays(["Event", "Kinetic/keV"], library="pd",
-        #                       entry_stop=10)
+        # df_test = tree.arrays(["Event", "Kinetic/keV"], library="pd", entry_start=10496663981109070736 ,entry_stop=10496663981109071745)
+        df_test = tree.arrays(["Event", "Kinetic/keV"], library="pd",
+                              entry_stop=10)
 
         print("First 10 entries:", df_test)
     except Exception as e:
