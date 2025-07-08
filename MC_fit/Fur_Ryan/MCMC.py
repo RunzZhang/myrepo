dbeb43ab6f24f417a235254c8cd5473d46e9a8f9
RunzZhang <52052572+RunzZhang@users.noreply.github.com>
2025-07-07 22:51:35 -0700

    save path same consistent to noah settings

--- MC_fit/Fur_Ryan/MCMC.py
+++ MC_fit/Fur_Ryan/MCMC.py
@@ -107,7 +107,7 @@ class multi_MC():
         except:
             self.fileprefix = "/home/runze/Documents/results/TN_sims_2E5/"
 
-        self.save_path = "/home/runze/Documents/results/TN_sims_2E5/"
+        self.save_path = self.fileprefix
         """ 
         def NucleationEfficiency(r,T,sigma):
             #A=1/2
