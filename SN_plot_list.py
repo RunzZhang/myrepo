a17bc4c564f8aa5d0b8b84645ddd79dfd2ab57aa
RunzZhang <52052572+RunzZhang@users.noreply.github.com>
2025-07-08 18:41:35 -0700

    Update SN_plot_list.py

--- SN_plot_list.py
+++ SN_plot_list.py
@@ -165,7 +165,7 @@ class SN():
             else:
                 point.append(i)
                 # print("point", point)
-                SN_ratio.append(max(SN_ratio)) # append line in the graph
+                # SN_ratio.append(max(SN_ratio)) # append line in the graph
                 SN_ratio.append(max(SN_ratio)*1E5)  # append inf line in the graph
         for j in range(len(signal_rate_list)):
             if photon_n_list[j]>200:
