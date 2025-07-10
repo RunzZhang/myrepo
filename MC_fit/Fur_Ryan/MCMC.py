9fab10621e5aa367d2cdf242e71844307aaeab8a
RunzZhang <52052572+RunzZhang@users.noreply.github.com>
2025-07-09 17:44:45 -0700

    Update MCMC.py

--- MC_fit/Fur_Ryan/MCMC.py
+++ MC_fit/Fur_Ryan/MCMC.py
@@ -860,11 +860,10 @@ class multi_MC():
         # print (np.shape(data))
         print("pho_test", 0, np.shape(data))
         dataMem = np.zeros(np.shape(data))
-        # dataMem[:, 1] = data[:, 1] # original
-        # dataMem[:, 2] = data[:, 2]
+        dataMem[:, 1] = data[:, 1] # original
+        dataMem[:, 2] = data[:, 2]
 
-        dataMem[:, 0] = data[:, 0]
-        dataMem[:, 1] = data[:, 1]
+
         print("pho_test", 1 )
         # T=80
         # sigLow=10
