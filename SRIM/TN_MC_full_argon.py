a5b2c0c68fd7af91f9f6120c1a50b8e06ae169eb
RunzZhang <52052572+RunzZhang@users.noreply.github.com>
2025-07-16 11:51:14 -0500

    Update TN_MC_full_argon.py

--- SRIM/TN_MC_full_argon.py
+++ SRIM/TN_MC_full_argon.py
@@ -134,11 +134,11 @@ class MC_sim_full_argon():
         # self.plot_spectrums_sigma()
 
         # self.plot_spectrums_sigma_LSS()
-        # self.predicted_bubble_events_LSS()
+        self.predicted_bubble_events_LSS()
         # self.LSS_introduced_uncertainty()
 
         # self.plot_spectrums_sigma_t()
-        self.predicted_bubble_events_t()
+        # self.predicted_bubble_events_t()
     def data_preparation(self):
 
         for i in range(len(self.argon_list)):# for each chain
