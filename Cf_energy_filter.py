1dc2c3a42b9b045ee668e441a79fcf3b79870d11
RunzZhang <52052572+RunzZhang@users.noreply.github.com>
2025-08-12 11:50:55 -0700

    Update Cf_energy_filter.py

--- Cf_energy_filter.py
+++ Cf_energy_filter.py
@@ -84,7 +84,7 @@ def raw_to_list(path):
 intens_fn[-1]= 1e-1 # modify last 0 to be plot right in log scale
 print((ene_fn,intens_fn))
 
-plt.plot(ene_fn,intens_fn,label='Cf252 Neutron Spectrum')
+plt.plot(ene_fn,intens_fn,label='$^{252}$Cf Neutron Spectrum')
 plt.plot(ene_tn,intens_tn,label='Outgoing Neutron Spectrum Through Sapphire')
 plt.xscale("log")
 plt.yscale("log")
