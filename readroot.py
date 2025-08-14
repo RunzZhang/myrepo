09c8c95e041ea1e19d763b627cfa039a064becc6
RunzZhang <52052572+RunzZhang@users.noreply.github.com>
2025-08-13 17:00:25 -0700

    Update readroot.py

--- readroot.py
+++ readroot.py
@@ -131,7 +131,7 @@ class ReadRoot():
 
         # self.gamma_event()
         # false noise 2, need to relocate directory
-        self.Huge_scatter_event()
+        # self.Huge_scatter_event()
         # signal rate, caputre in liquid argon
         # self.LAr_gamma_event()
         # single elastic scatter and capture false signal 1
@@ -141,6 +141,8 @@ class ReadRoot():
 
         # self.find_multiplicity()
         # self.Check_inelastic()
+        # test elatic and inelastic effect
+        self.bubble_rate()
 
     # there was some 0 in event columns, set them to corresponding value
     # for example 001002003 will be 001112223
@@ -356,9 +358,9 @@ class ReadRoot():
         print("sing", self.df_sing_Nscatter)
         print("multi", self.df_multi_Nscatter)
         # self.df_cap_gamma = pd.DataFrame('Event','Track ID')
-        # merged_df = pd.merge(self.df_sing_Nscatter, self.df_Ninelastic, on=['Event'], how='left', indicator=True)
-        # filtered_df = merged_df[merged_df['_merge'] == 'left_only'].drop(columns=['_merge'])
-        filtered_df = self.df_sing_Nscatter
+        merged_df = pd.merge(self.df_sing_Nscatter, self.df_Ninelastic, on=['Event'], how='left', indicator=True)
+        filtered_df = merged_df[merged_df['_merge'] == 'left_only'].drop(columns=['_merge'])
+        # filtered_df = self.df_sing_Nscatter
         print("merged_xor,\n", filtered_df.head(10))
         # 2nd filter filter out ncapture recoiled energy
         merged_df2 = pd.merge(filtered_df, self.df_capture, on=['Event'], how='left', indicator=True)
@@ -422,7 +424,8 @@ class ReadRoot():
         plt.yscale("Log")
         print("scatter number", len(scatter_ene))
         # before 21434
-        # add elastic 12945
+        # add elastic > 0.001 12945
+        # only elastic
         plt.xlabel("scatter energy per Event")
         # plt.show()
         plt.savefig(self.plot_path + "n_huge_scatter_ene_AmLi2.png")
