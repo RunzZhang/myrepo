3c606f4d9367d7f87214045e3a364c57d525a9c3
RunzZhang <52052572+RunzZhang@users.noreply.github.com>
2025-08-17 16:47:19 -0700

    Update readroot_gamma.py

--- readroot_gamma.py
+++ readroot_gamma.py
@@ -870,7 +870,7 @@ class ReadRoot():
         self.df.to_csv(self.false_3_path_mid, index=False)
         # z face is 1150mm
         self.df_gamma_income = self.df[
-            (self.df["name"] == 'gamma') & (self.df["Z/mm"] >=1140)& (self.df["Z/mm"] <=1160)& (self.df["Parent ID"] ==0)][
+            (self.df["name"] == 'gamma') & (self.df["Z/mm"] >=1240)& (self.df["Z/mm"] <=1260)& (self.df["Parent ID"] ==0)][
             ['Event', 'Volume', 'Track ID', 'X/mm','Y/mm','Z/mm','px/MeV','py/MeV','pz/MeV',"Kinetic/keV",'Parent ID']]
         print("first iincome",self.df_gamma_income.head(100))
         self.df_gamma_income = self.keep_1st(self.df_gamma_income)
@@ -886,7 +886,12 @@ class ReadRoot():
 
 
         # add gamma up
-        self.gamma_Ek_list = gamma_energy["Kinetic/keV"].to_list()
+        self.gamma_Ek_list=[]
+        self.gamma_px_list= gamma_energy["px/keV"].to_list()
+        self.gamma_py_list = gamma_energy["py/keV"].to_list()
+        self.gamma_pz_list = gamma_energy["pz/keV"].to_list()
+        for i in range(len(self.gamma_px_list)):
+            self.gamma_Ek_list.append((self.gamma_px_list[i]**2+self.gamma_py_list[i]**2+self.gamma_pz_list[i]**2)**0.5)
         print("gamma in 1E6 ", len(self.gamma_Ek_list))
 
         with open(self.false_3_path, 'w', newline='') as myfile:
