ed9e8d98b3b43cc5f68ae1017859fde99e3bac75
RunzZhang <52052572+RunzZhang@users.noreply.github.com>
2025-08-16 18:18:13 -0700

    Update readroot.py

--- readroot.py
+++ readroot.py
@@ -305,6 +305,7 @@ class ReadRoot():
         self.N_check.to_csv(self.false_2_path_mid, index=False)
 
         n_list_inela = self.LAr_n_merged_inela["Event"].to_list()
+        print("Ncheck_ inel", n_list_inela)
         self.N_check_inela = self.df[self.df["Event"].isin(n_list_inela) & (
                 (self.df["name"] == 'neutron') | (self.df["name"] == 'Ar40') | (self.df["name"] == 'Ar36'))]
         self.N_check_inela.to_csv(self.false_3_path_mid, index=False)
