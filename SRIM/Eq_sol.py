bab827ebf30ab6d937a89eb80f79ff6e02e294d9
RunzZhang <52052572+RunzZhang@users.noreply.github.com>
2025-08-17 17:51:50 -0700

    Update Eq_sol.py

--- SRIM/Eq_sol.py
+++ SRIM/Eq_sol.py
@@ -279,9 +279,9 @@ class E_loss_solve():
         # self.t_crit = 1343 * 10 ** (-6) * np.log(self.ini_E / 0.1)
         self.t_crit = 7.24432998e-04
         self.e_list =[]
-        self.bins = 100000
+        self.bins = 50
         for i in range(self.bins):
-            self.e_list.append(i*self.last_t/1000)
+            self.e_list.append(i*self.last_t/10)
         print(self.t_crit)
         # solve = solve_ivp(self.E_loss_el_t_fun_ODE, [0, 2*self.last_t], [self.ini_E], t_eval=self.e_list) # check one point's value
         solve = solve_ivp(self.E_loss_t_fun_ODE_new_D, [0, self.last_t], [self.ini_E])
