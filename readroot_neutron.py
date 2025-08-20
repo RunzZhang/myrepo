da686897302d819aeb642b8cd4760d90ca35c596
RunzZhang <52052572+RunzZhang@users.noreply.github.com>
2025-08-20 16:58:01 -0700

    Update readroot_neutron.py

--- readroot_neutron.py
+++ readroot_neutron.py
@@ -963,6 +963,14 @@ class ReadRoot():
                     self.df["Parent ID"] == 0)&(self.df["Volume"] == "physSap")][
             ['Event', 'Volume', 'Track ID', 'X/mm', 'Y/mm', 'Z/mm', 'px/MeV', 'py/MeV', 'pz/MeV', "Kinetic/keV",
              'Parent ID']]
+
+
+        # test neutron just hit sapphire
+        self.df_neutron_cross = self.df[
+            (self.df["name"] == 'neutron') & (
+                    self.df["Parent ID"] == 0) & (self.df["Volume"] == "physSap")][
+            ['Event', 'Volume', 'Track ID', 'X/mm', 'Y/mm', 'Z/mm', 'px/MeV', 'py/MeV', 'pz/MeV', "Kinetic/keV",
+             'Parent ID']]
         print("first iincome", self.df_neutron_income.head(100))
         self.df_neutron_income = self.keep_1st(self.df_neutron_income)
 
