080d559787008e16a6313866b79bdde693d6fac2
RunzZhang <52052572+RunzZhang@users.noreply.github.com>
2025-08-21 18:47:03 -0700

    Update readroot_neutron.py

--- readroot_neutron.py
+++ readroot_neutron.py
@@ -131,7 +131,8 @@ class ReadRoot():
         # self.df = self.file.arrays(self.selected_columns, library="pd").head(self.rows)
         # # process data so that it is easier to read
         # first 1000 rows
-        self.df = self.file.arrays(self.selected_columns, library="pd")
+        # self.df = self.file.arrays(self.selected_columns, library="pd")
+        self.df = self.file.arrays(library="pd")
         print("df",self.df.head(self.rows))
         # self.df = self.file.arrays(self.selected_columns, library="pd").head(self.rows)
         # only valid for ncrystal
