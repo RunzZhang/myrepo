6d9696c1e09e71599165ccea2d3b3efc683de648
RunzZhang <52052572+RunzZhang@users.noreply.github.com>
2025-09-08 10:32:47 -0700

    Update SN_plotCf_gamma.py

--- SN_plotCf_gamma.py
+++ SN_plotCf_gamma.py
@@ -28,7 +28,8 @@ class SN():
 
 
         #982 statics false 1
-        for i in range(1,101):
+        # for i in range(1,101):
+        for i in range(1, 2):
             self.main_body(i)
         self.plot_G()
         # self.plot_gamma()
