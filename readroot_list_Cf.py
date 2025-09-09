d41b9ed87aa803af36a86a31632b5efb31f2f6ad
RunzZhang <52052572+RunzZhang@users.noreply.github.com>
2025-09-08 17:10:55 -0700

    Update readroot_list_Cf.py

--- readroot_list_Cf.py
+++ readroot_list_Cf.py
@@ -577,7 +577,7 @@ class ReadRoot():
         self.df_gamma_rw = pd.read_csv(self.false_3_path_mid)
         print(self.df_gamma_rw[["Kinetic/keV"]].head(20))
 
-        self.gamma_Scint = self.df_gamma_rw[(self.gamma_Scint['name'] == 'gamma') & (self.gamma_Scint['Volume'] == 'LAr_phys')&
+        self.gamma_Scint = self.df_gamma_rw[(self.df_gamma_rw['name'] == 'gamma') & (self.df_gamma_rw['Volume'] == 'LAr_phys')&
             (self.df_gamma_rw['Volume'] == 'LAr_phys')]
         gamma_list = self.gamma_Scint["Event"].unique()
 
