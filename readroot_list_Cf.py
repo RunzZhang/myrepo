6597927f0360f7890f9c485f5fd6d8ede490def4
RunzZhang <52052572+RunzZhang@users.noreply.github.com>
2025-09-09 16:29:20 -0700

    Update readroot_list_Cf.py

--- readroot_list_Cf.py
+++ readroot_list_Cf.py
@@ -581,9 +581,9 @@ class ReadRoot():
         self.df_gamma_rw = pd.read_csv(self.false_3_path_mid)
         print(self.df_gamma_rw[["Kinetic/keV"]].head(20))
 
-        self.gamma_Scint = self.df_gamma_rw[(self.df_gamma_rw['name'] == 'gamma') & (self.df_gamma_rw['Volume'] == 'LAr_phys')(self.df_gamma_rw['Kinetic/keV'] >0)]
+        self.gamma_Scint = self.df_gamma_rw[(self.df_gamma_rw['name'] == 'gamma') & (self.df_gamma_rw['Volume'] == 'LAr_phys')&(self.df_gamma_rw['Kinetic/keV'] >0)]
         # >0 to rule out no contribution gammas
-        
+
         gamma_list = self.gamma_Scint["Event"].unique()
 
         # print out the file of inelastic scattering
