68df772b37a98deb244988d214dc26546324a6b4
RunzZhang <52052572+RunzZhang@users.noreply.github.com>
2025-09-08 20:58:01 -0700

    Update readroot_list_Cf.py

--- readroot_list_Cf.py
+++ readroot_list_Cf.py
@@ -140,8 +140,8 @@ class ReadRoot():
 
         # self.filepath = self.base_path +"dmx_lr.root"
         # self.main_body(1)
-        for i in range(1,101):
-        # for i in range(1, 2):
+        # for i in range(1,101):
+        for i in range(1, 2):
             self.main_body(i)
     def main_body(self,i):
         print(i)
