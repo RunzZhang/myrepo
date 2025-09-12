d10c2b737e65d64a2d114fcf9ead90579822c5d6
RunzZhang <52052572+RunzZhang@users.noreply.github.com>
2025-09-12 16:17:55 -0700

    Update chunk_root_format_Amli.py

--- chunk_root_format_Amli.py
+++ chunk_root_format_Amli.py
@@ -22,17 +22,17 @@ class ReadRoot:
         # self.signal_path_mid = self.base_path + self.signal_mid
         # self.signal_path = self.base_path + self.signal
 
-        self.filepath = self.base_path + "dmx_AmLi_1E7.root"
+        self.filepath = self.base_path + "dmx_AmLi_neutron_1E7.root"
         # self.filepath = self.base_path + "dmx_AmLi.root" # test
         self.tree_name = "tree"  # Assuming your TTree is named "tree"
 
         # Define the columns you want to read and write
-        self.selected_columns = ["Event", "name", "Parent ID", "Track ID", "Step ID", "X/mm", "Kinetic/keV",
-                                 "Recoiled/keV", "Volume", "Process"]
+        self.selected_columns = ["Event", "name", "Parent ID", "Track ID", "Step ID", "X/mm", "PreKinetic/MeV","PostKinetic/MeV"
+                                 "Recoiled/MeV", "Volume", "Process"]
 
     def chunk_and_write_root(self, num_chunks=20, output_dir=None):
         if output_dir is None:
-            output_dir = os.path.join(self.base_path, "chunked_root_files_AmLi")
+            output_dir = os.path.join(self.base_path, "chunked_root_files_AmLi_2")
         os.makedirs(output_dir, exist_ok=True)
 
         with uproot.open(self.filepath) as file:
