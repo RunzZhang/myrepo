ec04aa641397f218d796d8d38a21b6f56f319a52
RunzZhang <52052572+RunzZhang@users.noreply.github.com>
2025-09-12 15:08:39 -0700

    Update readroot_Ar_inelastic_box.py

--- readroot_Ar_inelastic_box.py
+++ readroot_Ar_inelastic_box.py
@@ -465,7 +465,7 @@ class ReadRoot():
         self.N_check.to_csv(self.false_2_path_mid, index=False)
 
         n_list_inela = self.LAr_n_merged_inela["Event"].to_list()
-        print("Ncheck_ inel", n_list_inela)
+        # print("Ncheck_ inel", n_list_inela)
         self.N_check_inela = self.df[(self.df["Event"].isin(n_list_inela)) & (
                 (self.df["name"] == 'neutron') | (self.df["name"] == 'Ar40') | (self.df["name"] == 'Ar36') | (
                 self.df["name"] == 'gamma') | (self.df["name"] == 'e-'))]
