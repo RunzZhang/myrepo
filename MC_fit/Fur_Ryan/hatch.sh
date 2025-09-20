5e2d45a9049e943f4f6817e530ff6c015d6099b7
RunzZhang <52052572+RunzZhang@users.noreply.github.com>
2025-09-20 16:50:43 -0700

    hi precition LZ

--- MC_fit/Fur_Ryan/hatch.sh
+++ MC_fit/Fur_Ryan/hatch.sh
@@ -1,6 +1,6 @@
-nameJob="TN_sims_2E5_pn_low_LZ"
+nameJob="TN_sims_2E5_pn_high_LZ"
 
-SUBDIR="/home/runze/Documents/results/TN_sims_2E5_pn_low_LZ"
+SUBDIR="/home/runze/Documents/results/TN_sims_2E5_pn_high_LZ"
 
 if [ ! -d $SUBDIR ]; then
     mkdir $SUBDIR
