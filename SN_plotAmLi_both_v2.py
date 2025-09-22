376f0521d593f057d4641570767b351336d98a84
RunzZhang <52052572+RunzZhang@users.noreply.github.com>
2025-09-21 21:19:03 -0700

    caculate intial rate and tagged SNR

---
+++ SN_plotAmLi_both_v2.py
@@ -0,0 +1,320 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+import csv
+import numpy as np
+class SN():
+    def __init__(self):
+        # after generate new files, you need to select the capture ratio/source for different configs in read_files function.
+        # then choose the correct signal/noise of with clause in read files.
+        # at last change the self.name and plot_name in plot function
+        # v2: change back to 2 backgrounds but with finer definitions
+        self.base_path = "/data/runzezhang/result/TN_sims_D/chunked_root_files_AmLi_LZ_v2/"
+        self.base_path2 = "/data/runzezhang/result/TN_sims_D/chunked_root_files_AmLi_LZ_v2/"
+        self.plot_path = '/data/runzezhang/result/TN_sims_D/plot/'
+        self.false_1 = "AmLi_false1.csv"
+        self.false_2 = "AmLi_false2.csv"
+        self.signal = "AmLi_sig.csv"
+        self.name1 = "NR ER Background"
+        self.name2 = "Hard Scatter Background"
+        self.name = "Backgrounds"
+        self.plot_name = self.name+"AmLi_1E7_total_LZ_updated_v2.pdf"
+        self.pho_threshold = 100
+        self.signal_final_list = []
+        self.noise1_final_list =[]
+        self.noise2_final_list = []
+        self.untagged_bubble_list =[]
+
+
+        #982 statics false 1
+        for i in range(1,101):
+            self.main_body(i)
+        self.untagged_bubble_rate()
+        (result1, result2)=self.combine_data()
+        self.plot_sn(result1[0],result2[0], result1[1],result2[1],result1[2],result2[2],result1[3],result2[3])
+
+
+    def main_body(self,i):
+        print(i)
+        self.false_1 = f"AmLi_1E7_false1_part{i}.csv"
+        self.false_2 = f"AmLi_1E7_false2_part{i}.csv"
+        self.false_3 = f"AmLi_1E7_false3_part{i}.csv"
+        self.signal = f"AmLi_1E7_sig_part{i}.csv"
+
+        self.false_1_path = self.base_path + self.false_1
+        self.false_2_path = self.base_path + self.false_2
+        self.false_3_path = self.base_path + self.false_3
+        self.signal_path = self.base_path + self.signal
+
+
+        self.read_files()
+
+        # self.read_files_s_to_N1()
+
+# main funtion we use
+    def read_files(self):
+        self.original_Activity = 2.135 # original activity in the paper
+        self.Activity = 2.135  # source practical activity in mivro curie for 50 bubbles/hour
+        # self.Activity = 0.0416  # source activity in mivro curie
+        # self.capture_ratio = 1.164E-3 # 1125eV 1.4g/cm Ar
+        # self.capture_ratio = 0.121 # 400 eV 1.4g/cm3 Ar
+        # self.capture_ratio = 0.267  # 350 eV
+        self.capture_ratio = 0.116  # 400 eV
+        # self.capture_ratio = 6.52E-3  # 700 eV
+        # self.capture_ratio = 1.158E-3  # 1125 eV
+        # self.rate = 435.6 #/s # CF neutron rate 9 mucurie
+        # self.rate = 0.1968 #AmLi neutron rate /s
+        # self.rate = 2.52e4  # AmLi neutron rate /s PNNL
+        self.rate = 21  # AmLi neutron rate /s LZ
+        # self.G4_events= 1E7
+        self.G4_events = 2E7
+        self.G4_sig_time=(self.G4_events / self.rate)
+        with open(self.signal_path, 'r') as file:
+            reader = csv.reader(file)
+            # Read the first row (assuming single row for simplicity)
+            number_list = next(reader)
+            # Convert the strings to floats
+            self.sig_raw_list = [float(value) for value in number_list]
+        self.signal_final_list = self.signal_final_list + self.sig_raw_list
+
+        print("capture event number", len(self.sig_raw_list))
+        self.G4_noise_time = self.G4_events / self.rate
+        # with open("/data/runzezhang/result/TN_e_sims/scatter_spectrum_CF.csv", 'r') as file:
+        # Noise 1,
+        with open(self.false_1_path, 'r') as file:
+            reader = csv.reader(file)
+            # Read the first row (assuming single row for simplicity)
+            number_list = next(reader)
+            # Convert the strings to floats
+            self.noise1_raw_list = [float(value) for value in number_list[1:]]
+            bubble_num =  number_list[0]
+
+            # the [0] is NR number and [1:] is the photon numbers
+        self.noise1_final_list = self.noise1_final_list + self.noise1_raw_list
+        self.untagged_bubble_list.append(bubble_num)
+        # Noise 2
+        with open(self.false_2_path, 'r') as file:
+            reader = csv.reader(file)
+            # Read the first row (assuming single row for simplicity)
+            number_list = next(reader)
+            # Convert the strings to floats
+            self.noise2_raw_list = [float(value) for value in number_list]
+            # self.noise_raw_list = [float(value)  for value in number_list]
+        self.noise2_final_list = self.noise2_final_list + self.noise2_raw_list
+
+
+    def combine_data(self):
+        # get rate vs diff threshold
+        print(len(self.noise1_final_list))
+        max_noise1_photon = round(max(self.noise1_final_list))
+        print("max",max_noise1_photon)
+        # form the threshold function
+        threshold1_list  = []
+        bin_size = round(max_noise1_photon/10)# if the max noise photon is too large, then we need to modity this bc of RAM
+        for i in range(0,max_noise1_photon):
+        # for i in range(0,round(max_noise_photon*0.1)):
+            if i%bin_size==0:
+                percentage = (i / max_noise1_photon) * 100
+                print(f"Noise 1 Progress: {percentage:.0f}%")
+            threshold1_list.append(i)
+
+        print(len(self.noise2_final_list))
+        max_noise2_photon = round(max(self.noise2_final_list))
+        print("max", max_noise2_photon)
+        # form the threshold function
+        threshold2_list = []
+        bin_size = round(
+            max_noise2_photon / 10)  # if the max noise photon is too large, then we need to modity this bc of RAM
+        for i in range(0, max_noise2_photon):
+            # for i in range(0,round(max_noise_photon*0.1)):
+            if i % bin_size == 0:
+                percentage = (i / max_noise2_photon) * 100
+                print(f"Noise 2 Progress: {percentage:.0f}%")
+            threshold2_list.append(i)
+
+
+
+
+        # plot the S/N ratio picture
+        result1 = self.unit_transfer( self.noise1_final_list,threshold1_list)
+        result2 = self.unit_transfer(self.noise2_final_list, threshold2_list)
+        return (result1, result2)
+
+    def hist_info(self):
+        sig_counts, sig_bin_edges = np.histogram(self.signal_final_list, bins=100)
+        sig_normalized_counts = sig_counts*self.capture_ratio/self.G4_sig_time
+        sig_bin_centers = (sig_bin_edges[:-1] + sig_bin_edges[1:]) / 2
+        plt.bar(sig_bin_centers, sig_normalized_counts, width=sig_bin_edges[1] - sig_bin_edges[0], color='red',label='signal')
+
+        noise_counts, noise_bin_edges = np.histogram(self.noise_final_list, bins=100)
+        noise_normalized_counts = noise_counts / self.G4_noise_time
+        noise_bin_centers = (noise_bin_edges[:-1] + noise_bin_edges[1:]) / 2
+        plt.bar(noise_bin_centers, noise_normalized_counts, width=noise_bin_edges[1] - noise_bin_edges[0], color='blue',label='noise')
+
+        # Set x-label and y-label with font size
+        # plt.xlabel('Value', fontsize=14)
+        # plt.ylabel('Frequency (normalized)', fontsize=14)
+        # plt.hist(self.signal_final_list, color="red", label='signal')
+        # plt.hist(self.noise_final_list,color='blue',label='noise')
+
+        plt.xlabel("photon detected by SiPM #", fontsize=16)
+        plt.ylabel("signal/noise rate #/s", fontsize=16)
+        plt.yscale('log')
+        plt.legend()
+        plot_name = "sn1_1E7"
+        plt.savefig(self.plot_path+plot_name)
+        # plt.show()
+
+    def prepare(self,noise_list, threshold): # filter the value above the threshold
+        self.sig = [value for value in self.signal_final_list if value >= threshold]
+        self.noise = [value for value in noise_list if value >= threshold]
+        sig_len = len(self.sig)
+        noise_len = len(self.noise)
+        return (sig_len,noise_len)
+    def unit_transfer(self, noise_list, threshold_list):
+        signal_rate_list = []
+        noise_rate_list =[]
+        signal_num_list = []
+        noise_num_list = []
+        SN_ratio = []
+        photon_n_list = []
+
+        print("cut",max(noise_list))
+        length = round(max(self.signal_final_list))
+        point = [] # threshold cut?
+        for i in range(length):
+            photon_n_list.append(i)
+            (sig_num,noise_num)= self.prepare(noise_list,i)
+            # change signal_number form /s to /h
+            signal_rate_list.append(self.Activity*3600*sig_num*self.capture_ratio/(2.135*self.G4_sig_time))
+            noise_rate_list.append(3600*self.Activity*noise_num/(2.135*self.G4_noise_time))
+            signal_num_list.append(sig_num)
+            noise_num_list.append(noise_num)
+            if noise_num !=0:
+                SN_ratio.append((sig_num*self.capture_ratio/self.G4_sig_time)/(noise_num/self.G4_noise_time))
+            else:
+
+                point.append(i)
+                # print("point", point)
+                # SN_ratio.append(max(SN_ratio)) # append line in the graph
+                SN_ratio.append(max(SN_ratio)*1E5)  # append inf line in the graph
+        for j in range(len(signal_rate_list)):
+            if photon_n_list[j]>self.pho_threshold:
+                print("output",j,signal_rate_list[j],noise_rate_list[j])
+                print("stat num", noise_num_list[j])
+                break
+        print("sig rate",max(signal_rate_list))
+        if point != []:
+            print("sig rate after cut", signal_rate_list[point[0]])
+        print("noise stat N", len(noise_list))
+        print("noise rate",max(noise_rate_list))
+
+        print("SN",max(SN_ratio),SN_ratio[:20])
+        print("noise uncetainty", 1.29*max(noise_rate_list)/len(noise_list))
+        print("sig_stats", signal_num_list[0], "noise_stats", noise_num_list[0])
+
+        return(signal_rate_list, photon_n_list, noise_rate_list,  SN_ratio)
+    def untagged_bubble_rate(self):
+        summed_bubble_num = sum(self.untagged_bubble_list)
+
+        summed_bubble_rate = self.Activity * 3600 * summed_bubble_num / (2.135 * self.G4_sig_time)
+        # untagged total bubble rate in /h
+        print("untagged total bubble rate /h", summed_bubble_rate)
+
+    def plot_sn(self, sig1, sig2,sig3, pho1, pho2, pho3,noise1, noise2, noise3, sn1, sn2, sn3):
+        fig, (ax1, ax3) = plt.subplots(1, 2, figsize=(12, 5))  # ax1 for first plot, ax3 for second plot
+        left_axis_range=[4e-2,3e1]
+        right_axis_range=[1e-1,1e3]
+
+        # left_axis_range = [1e-1, 3e4]
+        # right_axis_range = [1e-1, 1e5]
+
+        # ======== FIRST PLOT (your original one) ========
+        # Plot dataset 1 and dataset 2 on the left y-axis
+        line1, = ax1.plot(pho1, sig1, 'g-', label='Neutron Capture Signal')
+        line2, = ax1.plot(pho1, noise1, 'b-', label='Background')
+        ax1.ticklabel_format(style='sci', scilimits=(-2, 3), axis='y')
+        ax1.set_xlim([0, 600])
+        ax1.set_ylim(left_axis_range)
+        # print("pho",pho1)
+        # print("noise1", noise1)
+
+        ax1.set_xlabel('Photon Number Threshold (number)', fontsize=16)
+        ax1.set_ylabel('Rate (event/hr)', color='black', fontsize=16)
+        ax1.axvline(x=self.pho_threshold, color='black', linestyle='dotted')
+        ax1.set_yscale('log')
+        # ax1.set_aspect('equal', adjustable="datalim")
+
+
+        # Create another y-axis for SNR
+        ax2 = ax1.twinx()
+        line3, = ax2.plot(pho1, sn1, 'r-', label='SNR')
+        ax2.set_ylabel('Signal to noise ratio', color='black', fontsize=16)
+        # ax2.set_ylim([0, 120])
+        ax2.set_ylim(right_axis_range)
+        ax2.set_yscale('log')
+
+        # Legend for first plot
+        lines_group1 = [line1, line2, line3]
+        labels_group1 = [line.get_label() for line in lines_group1]
+        ax1.legend(lines_group1, labels_group1, loc='upper right')
+
+        ax1.set_title(self.name1, fontsize=16)
+
+        # ======== SECOND PLOT (side-by-side) ========
+        # Example plot — replace with your own data
+        line4, = ax3.plot(pho2, sig2, 'g-', label='Neutron Capture Signal')
+        line5, = ax3.plot(pho2, noise2, 'b-', label='Background')
+        ax3.ticklabel_format(style='sci', scilimits=(-2, 3), axis='y')
+        ax3.set_xlim([0, 600])
+        ax3.set_ylim(left_axis_range)
+
+        ax3.set_xlabel('Photon Number Threshold (number)', fontsize=16)
+        ax3.set_ylabel('Rate (event/hr)', color='black', fontsize=16)
+        ax3.axvline(x=self.pho_threshold, color='black', linestyle='dotted')
+        ax3.set_yscale('log')
+        # ax3.set_aspect('equal')
+
+        # Create another y-axis for SNR
+        ax4 = ax3.twinx()
+        line6, = ax4.plot(pho2, sn2, 'r-', label='SNR')
+        ax4.set_ylabel('Signal to noise ratio', color='black', fontsize=16)
+        # ax4.set_ylim([0,120])
+        ax4.set_ylim(right_axis_range)
+        ax4.set_yscale('log')
+
+        # Legend for first plot
+        lines_group2 = [line4, line5, line6]
+        labels_group2 = [line.get_label() for line in lines_group2]
+        ax3.legend(lines_group2, labels_group2, loc='upper right')
+
+        ax3.set_title(self.name2, fontsize=16)
+
+
+        # Adjust spacing so plots don’t overlap
+        fig.set_size_inches(20, 6)
+        plt.tight_layout()
+
+        # Save or show
+        plt.savefig(self.plot_path + self.plot_name)
+        # plt.show()
+
+
+
+class test_csv():
+    def __init__(self):
+        list1 = [1,3,4.5,6.7,8.9]
+        with open("/data/runzezhang/test.csv", 'w', newline='') as myfile:
+            wr = csv.writer(myfile)
+            wr.writerow(list1)
+        with open("/data/runzezhang/test.csv", 'r') as file:
+            reader = csv.reader(file)
+            # Read the first row (assuming single row for simplicity)
+            number_list = next(reader)
+            # Convert the strings to floats
+            number_list = [float(value) for value in number_list]
+
+        print(number_list)
+
+if __name__=="__main__":
+    sn = SN()
+    # test = test_csv()
