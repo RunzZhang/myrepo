d322ae625ae44e0241c24801703a7cc7ae4d4708
RunzZhang <52052572+RunzZhang@users.noreply.github.com>
2025-09-24 18:59:33 -0700

    Update readroot_list_AmLi.py

--- readroot_list_AmLi.py
+++ readroot_list_AmLi.py
@@ -1463,7 +1463,9 @@ class ReadRoot():
         # double check gamma
 
         summed_values = self.tagged_gamma.groupby(['Event'])["Recoiled/MeV"].sum().reset_index()
+
         print(summed_values.head(20))
+        print(summed_values[summed_values["Event"]==105600])
 
         # add gamma up
         self.electron_recoiled_list = summed_values["Recoiled/MeV"].to_list()
