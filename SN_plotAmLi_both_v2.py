b4dcec249b5dcebba0328a62aa14e9eb483257bd
RunzZhang <52052572+RunzZhang@users.noreply.github.com>
2025-09-26 18:29:38 -0700

    Update SN_plotAmLi_both_v2.py

--- SN_plotAmLi_both_v2.py
+++ SN_plotAmLi_both_v2.py
@@ -9,8 +9,8 @@ class SN():
         # at last change the self.name and plot_name in plot function
         # v2: change back to 2 backgrounds but with finer definitions
         # v4 kill duplicated NRERs
-        self.base_path = "/data/runzezhang/result/TN_sims_D/chunked_root_files_AmLi_v4/"
-        self.base_path2 = "/data/runzezhang/result/TN_sims_D/chunked_root_files_AmLi_v4/"
+        self.base_path = "/data/runzezhang/result/TN_sims_D/chunked_root_files_AmLi_v2/"
+        self.base_path2 = "/data/runzezhang/result/TN_sims_D/chunked_root_files_AmLi_v2/"
         self.plot_path = '/data/runzezhang/result/TN_sims_D/plot/'
         self.false_1 = "AmLi_false1.csv"
         self.false_2 = "AmLi_false2.csv"
