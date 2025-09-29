04e6aa79d3d28162230904cfdee970266c4b91d8
RunzZhang <52052572+RunzZhang@users.noreply.github.com>
2025-09-29 15:48:30 -0700

    Update readroot_list_AmLi.py

--- readroot_list_AmLi.py
+++ readroot_list_AmLi.py
@@ -134,8 +134,8 @@ class RestructureRoot():
 
 class ReadRoot():
     def __init__(self):
-        self.base_path = "/data/runzezhang/result/TN_sims_D/chunked_root_files_AmLi_test_1E6/"
-        self.base_path2 = "/data/runzezhang/result/TN_sims_D/chunked_root_files_AmLi_test_1E6/"
+        self.base_path = "/data/runzezhang/result/TN_sims_D/chunked_root_files_LZ_bare_clean_point/"
+        self.base_path2 = "/data/runzezhang/result/TN_sims_D/chunked_root_filesLZ_bare_clean_point/"
         self.plot_path = '/data/runzezhang/result/TN_sims_D/plot/'
 
         # self.filepath = self.base_path +"dmx_lr.root"
