4bfc93b812cd861619959a1d6b0c87e3926665a1
RunzZhang <52052572+RunzZhang@users.noreply.github.com>
2025-09-30 15:14:19 -0700

    come back to initial sims

--- SN_plotAmLi_both_v2.py
+++ SN_plotAmLi_both_v2.py
@@ -9,8 +9,8 @@ class SN():
         # at last change the self.name and plot_name in plot function
         # v2: change back to 2 backgrounds but with finer definitions
         # v4 kill duplicated NRERs
-        self.base_path = "/data/runzezhang/result/TN_sims_D/chunked_root_files_AmLi_test_1E6/"
-        self.base_path2 = "/data/runzezhang/result/TN_sims_D/chunked_root_files_AmLi_test_1E6/"
+        self.base_path = "/data/runzezhang/result/TN_sims_D/chunked_root_files_AmLi_LZ_bare_clean_point_log/"
+        self.base_path2 = "/data/runzezhang/result/TN_sims_D/chunked_root_files_AmLi_LZ_bare_clean_point_log/"
         self.plot_path = '/data/runzezhang/result/TN_sims_D/plot/'
         self.false_1 = "AmLi_false1.csv"
         self.false_2 = "AmLi_false2.csv"
@@ -80,38 +80,38 @@ class SN():
         # self.G4_events= 1E7
         self.G4_events = 2E7
         self.G4_sig_time=(self.G4_events / self.rate)
-        # with open(self.signal_path, 'r') as file:
-        #     reader = csv.reader(file)
-        #     # Read the first row (assuming single row for simplicity)
-        #     number_list = next(reader)
-        #     # Convert the strings to floats
-        #     self.sig_raw_list = [float(value) for value in number_list]
-        # self.signal_final_list = self.signal_final_list + self.sig_raw_list
-        #
-        # print("capture event number", len(self.sig_raw_list))
-        # self.G4_noise_time = self.G4_events / self.rate
-        # # with open("/data/runzezhang/result/TN_e_sims/scatter_spectrum_CF.csv", 'r') as file:
-        # # Noise 1,
-        # with open(self.false_1_path, 'r') as file:
-        #     reader = csv.reader(file)
-        #     # Read the first row (assuming single row for simplicity)
-        #     number_list = next(reader)
-        #     # Convert the strings to floats
-        #     self.noise1_raw_list = [float(value) for value in number_list[1:]]
-        #     bubble_num =  number_list[0]
-        #
-        #     # the [0] is NR number and [1:] is the photon numbers
-        # self.noise1_final_list = self.noise1_final_list + self.noise1_raw_list
-        # self.untagged_bubble_list.append(float(bubble_num))
-        # # Noise 2
-        # with open(self.false_2_path, 'r') as file:
-        #     reader = csv.reader(file)
-        #     # Read the first row (assuming single row for simplicity)
-        #     number_list = next(reader)
-        #     # Convert the strings to floats
-        #     self.noise2_raw_list = [float(value) for value in number_list]
-        #     # self.noise_raw_list = [float(value)  for value in number_list]
-        # self.noise2_final_list = self.noise2_final_list + self.noise2_raw_list
+        with open(self.signal_path, 'r') as file:
+            reader = csv.reader(file)
+            # Read the first row (assuming single row for simplicity)
+            number_list = next(reader)
+            # Convert the strings to floats
+            self.sig_raw_list = [float(value) for value in number_list]
+        self.signal_final_list = self.signal_final_list + self.sig_raw_list
+
+        print("capture event number", len(self.sig_raw_list))
+        self.G4_noise_time = self.G4_events / self.rate
+        # with open("/data/runzezhang/result/TN_e_sims/scatter_spectrum_CF.csv", 'r') as file:
+        # Noise 1,
+        with open(self.false_1_path, 'r') as file:
+            reader = csv.reader(file)
+            # Read the first row (assuming single row for simplicity)
+            number_list = next(reader)
+            # Convert the strings to floats
+            self.noise1_raw_list = [float(value) for value in number_list[1:]]
+            bubble_num =  number_list[0]
+
+            # the [0] is NR number and [1:] is the photon numbers
+        self.noise1_final_list = self.noise1_final_list + self.noise1_raw_list
+        self.untagged_bubble_list.append(float(bubble_num))
+        # Noise 2
+        with open(self.false_2_path, 'r') as file:
+            reader = csv.reader(file)
+            # Read the first row (assuming single row for simplicity)
+            number_list = next(reader)
+            # Convert the strings to floats
+            self.noise2_raw_list = [float(value) for value in number_list]
+            # self.noise_raw_list = [float(value)  for value in number_list]
+        self.noise2_final_list = self.noise2_final_list + self.noise2_raw_list
 
         # Initial amli spectrm
         with open(self.ini_path, 'r') as file:
--- chunk_root_format_Amli.py
+++ chunk_root_format_Amli.py
@@ -25,7 +25,7 @@ class ReadRoot:
         # self.signal_path_mid = self.base_path + self.signal_mid
         # self.signal_path = self.base_path + self.signal
 
-        self.filepath = self.base_path + "dmx_AmLi_LZ_LZ_bare_clean_point_log_1E6.root"
+        self.filepath = self.base_path + "/data/runzezhang/result/TN_sims_D/dmx_AmLi_LZ_bare_clean_point_log_2E7.root"
         # self.filepath = self.base_path + "dmx_AmLi.root" # test
         self.tree_name = "tree"  # Assuming your TTree is named "tree"
 
@@ -38,7 +38,7 @@ class ReadRoot:
 
     def chunk_and_write_root(self, num_chunks=20, output_dir=None):
         if output_dir is None:
-            output_dir = os.path.join(self.base_path, "chunked_root_files_AmLi_test_1E6")
+            output_dir = os.path.join(self.base_path, "chunked_root_files_AmLi_LZ_bare_clean_point_log")
         os.makedirs(output_dir, exist_ok=True)
 
         with uproot.open(self.filepath) as file:
