3cf0022e14c3bfca3eceb8791cdeb0696efa9709
RunzZhang <52052572+RunzZhang@users.noreply.github.com>
2025-10-01 16:00:45 -0700

    Update SN_plotAmLi_both_v2.py

--- SN_plotAmLi_both_v2.py
+++ SN_plotAmLi_both_v2.py
@@ -78,9 +78,9 @@ class SN():
         # self.rate = 435.6 #/s # CF neutron rate 9 mucurie
         # self.rate = 0.1968 #AmLi neutron rate /s
         # self.rate = 2.52e4  # AmLi neutron rate /s PNNL
-        self.rate = 9  # AmLi neutron rate /s LZ
-        # self.G4_events= 1E7
-        self.G4_events = 2E7
+        self.rate = 21  # AmLi neutron rate /s LZ
+        self.G4_events= 1E7
+        # self.G4_events = 2E7
         self.G4_sig_time=(self.G4_events / self.rate)
         with open(self.signal_path, 'r') as file:
             reader = csv.reader(file)
