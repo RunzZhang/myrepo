0389141590ba6b9817baa56e4c2db6d2d31ddd41
RunzZhang <52052572+RunzZhang@users.noreply.github.com>
2025-10-01 12:49:20 -0700

    Update SN_plotAmLi_both_v2.py

--- SN_plotAmLi_both_v2.py
+++ SN_plotAmLi_both_v2.py
@@ -375,7 +375,7 @@ class SN():
         print("ini", bins_ini[:10])
         print("ke",
         bins_ke[:10])
-        print("before density bins_ini, rate ini, counts ini,counts ke", bins_ini[point:], "\n", Rate_ini[point:], "\n",
+        print("before density bins_ini, rate ini, counts ini,counts ke", Rate_ini[point:],"\n", Rate_ke[point:], "\n",
               counts_ini[point:], "\n", counts_ke[point:],"\n", bins_ini[point:],"\n", bins_ke[point:])  # print no-zero first bins
 
         # make the y value /h/eV
@@ -386,7 +386,7 @@ class SN():
 
         plt.plot(bins_ini, Rate_ini, drawstyle="steps-mid", label="LZ AmLi Escaping Neutron")
 
-        print("after density bins_ini, rate ini, counts ini,counts ke",bins_ini[point:],"\n", Rate_ini[point:],"\n", counts_ini[point:],"\n", counts_ke[point:],"\n", bins_ini[point:],"\n", bins_ke[point:]) # print no-zero first bins
+        print("after density bins_ini, rate ini, counts ini,counts ke",Rate_ini[point:],"\n", Rate_ke[point:],"\n", counts_ini[point:],"\n", counts_ke[point:],"\n", bins_ini[point:],"\n", bins_ke[point:]) # print no-zero first bins
         plt.plot(bins_ke, Rate_ke, drawstyle="steps-mid", label="First Enter LAr Neutron")
         plt.plot(ene_list, possibility_list, drawstyle="steps-mid", label="Original Spectrum dat")
         # plt.plot(bins_ini, Rate_ini, label="LZ AmLi Escaping Neutron")
