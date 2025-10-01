32984bccdd983e5b13620a8f9a833a8170d76ed0
RunzZhang <52052572+RunzZhang@users.noreply.github.com>
2025-09-30 20:59:03 -0700

    Update chunk_root_format_Amli.py

--- chunk_root_format_Amli.py
+++ chunk_root_format_Amli.py
@@ -25,7 +25,7 @@ class ReadRoot:
         # self.signal_path_mid = self.base_path + self.signal_mid
         # self.signal_path = self.base_path + self.signal
 
-        self.filepath = self.base_path + "/data/runzezhang/result/TN_sims_D/dmx_AmLi_LZ_bare_clean_point_log_2E7.root"
+        self.filepath = self.base_path + "dmx_AmLi_LZ_bare_clean_point_log_2E7.root"
         # self.filepath = self.base_path + "dmx_AmLi.root" # test
         self.tree_name = "tree"  # Assuming your TTree is named "tree"
 
