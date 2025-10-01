34a2065944f97334f63659dd1664be521431ebff
RunzZhang <52052572+RunzZhang@users.noreply.github.com>
2025-10-01 08:49:32 -0700

    Update SN_plotAmLi_both_v2.py

--- SN_plotAmLi_both_v2.py
+++ SN_plotAmLi_both_v2.py
@@ -345,10 +345,20 @@ class SN():
         counts_ini, bin_edges_ini, patches_ini = plt.hist(self.neutron_ini_list, bins=log_bins, density= True)
         counts_ke, bin_edges_ke, patches_ke = plt.hist(self.neutron_ar_ke_list, bins=log_bins, density= True)
         plt.clf()
-        normalized  = sum(counts_ini)
-        print("norma fact", normalized)
+
+        normalized_ini  = 0
+        normalized_ke =0
+        for i in range(len(counts_ini)):
+            possibility_ini = counts_ini[i]*(bin_edges_ini[i+1]-bin_edges_ini[i])
+            possibility_ke = counts_ke[i] * (bin_edges_ke[i + 1] - bin_edges_ke[i])
+            normalized_ini+= possibility_ini
+            normalized_ke += possibility_ke
+
+        print("norma fact", normalized_ini, normalized_ke)
+
         Rate_ini = counts_ini*self.rate # rate in /h
-        Rate_ke = counts_ke * self.rate  # rate in /h
+        print("total rate", sum(Rate_ini))
+        Rate_ke = counts_ke * self.rate*normalized_ke/normalized_ini  # rate in /h
         bin_factor = bin_edges_ke[1]/bin_edges_ke[0]
         bins_ini = bin_edges_ini[:-1]*bin_factor**0.5
         # bins_ini = bin_edges_ini[:-1] * bin_factor
@@ -363,7 +373,7 @@ class SN():
                 break
         print(bins_ini[point:],"\n", Rate_ini[point:]) # print no-zero first bins
         plt.plot(bins_ke, Rate_ke, drawstyle="steps-mid", label="First Enter LAr Neutron")
-        plt.plot(ene_list, possibility_list, drawstyle="steps-mid", label="Original Spectrum dat")
+        # plt.plot(ene_list, possibility_list, drawstyle="steps-mid", label="Original Spectrum dat")
         # plt.plot(bins_ini, Rate_ini, label="LZ AmLi Escaping Neutron")
         # plt.plot(bins_ke, Rate_ke,  label="First Enter LAr Neutron")
         plt.xscale("log")
