36dda1598ad9b6cbf94e8d1d0b76a0eb0efb664a
RunzZhang <52052572+RunzZhang@users.noreply.github.com>
2025-10-02 14:41:35 -0700

    Update SN_plotAmLi_both_v2.py

--- SN_plotAmLi_both_v2.py
+++ SN_plotAmLi_both_v2.py
@@ -458,8 +458,8 @@ class SN():
         print("ini", bins_ini[:10])
         print("ke",
         bins_ke[:10])
-        print("before density bins_ini, rate ini, counts ini,counts ke", Rate_ini[point:],"\n", Rate_ke[point:], "\n",
-              counts_ini[point:], "\n", counts_ke[point:],"\n", bins_ini[point:],"\n", bins_ke[point:])  # print no-zero first bins
+        # print("before density bins_ini, rate ini, counts ini,counts ke", Rate_ini[point:],"\n", Rate_ke[point:], "\n",
+        #       counts_ini[point:], "\n", counts_ke[point:],"\n", bins_ini[point:],"\n", bins_ke[point:])  # print no-zero first bins
 
         # make the y value /h/eV
         # for i in range(len(Rate_ini)):
@@ -469,9 +469,9 @@ class SN():
 
         plt.plot(bins_ini, Rate_ini, drawstyle="steps-mid", label="LZ AmLi Escaping Neutron")
 
-        print("after density bins_ini, rate ini, counts ini,counts ke",Rate_ini[point:],"\n", Rate_ke[point:],"\n", counts_ini[point:],"\n", counts_ke[point:],"\n", bins_ini[point:],"\n", bins_ke[point:]) # print no-zero first bins
-        plt.plot(bins_ke, Rate_ke, drawstyle="steps-mid", label="First Enter LAr Neutron log", lw=4)
-        plt.plot(bins_ke, Rate_ke_alter, drawstyle="steps-mid", label="First Enter LAr Neutron lin")
+        # print("after density bins_ini, rate ini, counts ini,counts ke",Rate_ini[point:],"\n", Rate_ke[point:],"\n", counts_ini[point:],"\n", counts_ke[point:],"\n", bins_ini[point:],"\n", bins_ke[point:]) # print no-zero first bins
+        # plt.plot(bins_ke, Rate_ke, drawstyle="steps-mid", label="First Enter LAr Neutron log", lw=4)
+        # plt.plot(bins_ke, Rate_ke_alter, drawstyle="steps-mid", label="First Enter LAr Neutron lin")
         # plt.plot(ene_list, possibility_list, drawstyle="steps-mid", label="Original Spectrum dat")
         # plt.plot(bins_ini, Rate_ini, label="LZ AmLi Escaping Neutron")
         # plt.plot(bins_ke, Rate_ke,  label="First Enter LAr Neutron")
@@ -479,7 +479,7 @@ class SN():
         plt.yscale("log")
         plt.xlabel("Energy (eV)", fontsize=16)
         plt.ylabel(r"Rate (event/hr)", fontsize=16)
-        plt.gca().xaxis.set_major_locator(LogLocator(base=10.0, numticks=15))
+        # plt.gca().xaxis.set_major_locator(LogLocator(base=10.0, numticks=15))
         plt.xlim([0, 1e7])
         # plt.ylim([1e-1, 1e4])
         plt.legend()
