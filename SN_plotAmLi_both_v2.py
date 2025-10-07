406b6b9db477069a33ac0fa398ce008a7183bb33
RunzZhang <52052572+RunzZhang@users.noreply.github.com>
2025-10-07 15:30:08 -0700

    Update SN_plotAmLi_both_v2.py

--- SN_plotAmLi_both_v2.py
+++ SN_plotAmLi_both_v2.py
@@ -354,7 +354,7 @@ class SN():
         # plot sn in same graph and no SNR
         fig, ax1 = plt.subplots(1, 1, figsize=(6, 5))  # ax1 for first plot, ax3 for second plot
         x_range = [0,430]
-        left_axis_range=[1e-2,1e2]
+        left_axis_range=[1e-2,2e1]
 
         # left_axis_range = [1e-1, 3e4]
         # right_axis_range = [1e-1, 1e5]
