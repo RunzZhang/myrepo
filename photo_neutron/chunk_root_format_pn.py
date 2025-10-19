a858b9acf09383fc24745bbc71d63d21c2995dca
RunzZhang <52052572+RunzZhang@users.noreply.github.com>
2025-10-19 09:01:13 -0700

    Update chunk_root_format_pn.py

--- photo_neutron/chunk_root_format_pn.py
+++ photo_neutron/chunk_root_format_pn.py
@@ -26,7 +26,7 @@ class ReadRoot:
         # self.signal_path_mid = self.base_path + self.signal_mid
         # self.signal_path = self.base_path + self.signal
 
-        self.filepath = self.base_path + "dmx_pn_tube_1E7.root"
+        self.filepath = self.base_path + "dmx_pn_1E7_tube.root"
         # self.filepath = self.base_path + "dmx_AmLi.root" # test
         self.tree_name = "tree"  # Assuming your TTree is named "tree"
 
