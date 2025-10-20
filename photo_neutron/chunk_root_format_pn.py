3416aaec3d313dc8e6718d0c6e9ed5cfcf0c5678
RunzZhang <52052572+RunzZhang@users.noreply.github.com>
2025-10-20 15:38:46 -0700

    tube output

--- photo_neutron/chunk_root_format_pn.py
+++ photo_neutron/chunk_root_format_pn.py
@@ -26,7 +26,7 @@ class ReadRoot:
         # self.signal_path_mid = self.base_path + self.signal_mid
         # self.signal_path = self.base_path + self.signal
 
-        self.filepath = self.base_path + "dmx_pn_1E7_outside.root"
+        self.filepath = self.base_path + "dmx_pn_1E7_tube.root"
         # self.filepath = self.base_path + "dmx_AmLi.root" # test
         self.tree_name = "tree"  # Assuming your TTree is named "tree"
 
@@ -39,7 +39,7 @@ class ReadRoot:
 
     def chunk_and_write_root(self, num_chunks=20, output_dir=None):
         if output_dir is None:
-            output_dir = os.path.join(self.base_path, "chunked_root_files_pn_outside_1E7")
+            output_dir = os.path.join(self.base_path, "chunked_root_files_pn_tube_1E7")
         os.makedirs(output_dir, exist_ok=True)
 
         with uproot.open(self.filepath) as file:
