8be8a78cae1607bfb6eccd4f4d251b5f2367e789
RunzZhang <52052572+RunzZhang@users.noreply.github.com>
2025-10-22 13:37:07 -0700

    Update chunk_root_format_pn.py

--- photo_neutron/chunk_root_format_pn.py
+++ photo_neutron/chunk_root_format_pn.py
@@ -2,7 +2,7 @@ import uproot
 import pandas as pd
 import os, time
 
-# cp /data/runzezhang/Geant4Simulaions/g411_TN/TN_source_AmLi_LZ.mac /data/runzezhang/result/TN_sims_D/chunked_root_files_pn_outside_1E7
+# cp /data/runzezhang/Geant4Simulaions/g411_TN/TN_source_AmLi_LZ.mac /data/runzezhang/result/TN_sims_D/chunked_root_files_pn_1E7_tube_gamma/
 # change the mac name and chunk folder to save the macro configuration
 class ReadRoot:
     def __init__(self):
