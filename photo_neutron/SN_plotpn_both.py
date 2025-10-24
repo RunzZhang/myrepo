57dafc109b53e85d30a41c9212fb2635a4ff51d6
RunzZhang <52052572+RunzZhang@users.noreply.github.com>
2025-10-24 09:52:57 -0700

    Update SN_plotpn_both.py

--- photo_neutron/SN_plotpn_both.py
+++ photo_neutron/SN_plotpn_both.py
@@ -22,7 +22,7 @@ class SN():
         self.name1 = "Correlated ER Background"
         self.name2 = "Hard Scatter Background"
         self.name = "Backgrounds"
-        self.plot_name = self.name+"PN_1E7_wt_gamma_outside_lead.pdf"
+        self.plot_name = self.name+"PN_1E7_wt_gamma_outside.pdf"
         self.pho_threshold = 200
         self.signal_final_list = []
         self.noise1_final_list =[]
