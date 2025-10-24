b84fe56b21763238b94ecb3c8256b69914be6e31
RunzZhang <52052572+RunzZhang@users.noreply.github.com>
2025-10-24 09:55:16 -0700

    Update SN_plotpn_both.py

--- photo_neutron/SN_plotpn_both.py
+++ photo_neutron/SN_plotpn_both.py
@@ -9,11 +9,11 @@ class SN():
         # at last change the self.name and plot_name in plot function
         # v2: change back to 2 backgrounds but with finer definitions
         # v4 kill duplicated NRERs
-        # self.base_path = "/data/runzezhang/result/TN_sims_D/chunked_root_files_pn_1E7_outside_lead/"
-        # self.base_path2 = "/data/runzezhang/result/TN_sims_D/chunked_root_files_pn_1E7_outside_lead_gamma/" # for gamma path
+        self.base_path = "/data/runzezhang/result/TN_sims_D/chunked_root_files_pn_1E7_outside_lead/"
+        self.base_path2 = "/data/runzezhang/result/TN_sims_D/chunked_root_files_pn_1E7_outside_lead_gamma/" # for gamma path
 
-        self.base_path = "/data/runzezhang/result/TN_sims_D/chunked_root_files_pn_outside_1E7/"
-        self.base_path2 = "/data/runzezhang/result/TN_sims_D/chunked_root_files_pn_1E7_outside_gamma/"  # for gamma path
+        # self.base_path = "/data/runzezhang/result/TN_sims_D/chunked_root_files_pn_outside_1E7/"
+        # self.base_path2 = "/data/runzezhang/result/TN_sims_D/chunked_root_files_pn_1E7_outside_gamma/"  # without lead
 
         self.plot_path = '/data/runzezhang/result/TN_sims_D/plot/'
         self.false_1 = "PN_false1.csv"
