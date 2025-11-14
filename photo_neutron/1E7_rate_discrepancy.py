7f4230e4a5b3cc64000861ad1e4c09e39d90594a
RunzZhang <52052572+RunzZhang@users.noreply.github.com>
2025-11-13 16:08:33 -0800

    Update 1E7_rate_discrepancy.py

--- photo_neutron/1E7_rate_discrepancy.py
+++ photo_neutron/1E7_rate_discrepancy.py
@@ -299,7 +299,7 @@ class SN():
         plt.plot(bins_ini, Rate_ini, drawstyle="steps-mid", label="Abnorml Rate")
         plt.plot(bins_ini2, Rate_ini2, drawstyle="steps-mid", label="'Normal' Rate")
 
-
+        print(min(Rate_ini), max(Rate_ini))
         # plt.yscale("log")
         plt.xlabel("x (cm)", fontsize=16)
         plt.ylabel(r"Rate (event/hr)", fontsize=16)
