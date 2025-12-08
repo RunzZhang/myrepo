adfd5243abd7e37a8b800426629aaa2af8e9bc2e
RunzZhang <52052572+RunzZhang@users.noreply.github.com>
2025-12-08 14:51:00 -0800

    photo neutron source with lead high resolution

--- MC_fit/Fur_Ryan/MCMC.py
+++ MC_fit/Fur_Ryan/MCMC.py
@@ -601,7 +601,8 @@ class multi_MC():
         # t = 2E5 / 2.22  # updated 2E5 events capture rate LZ bare is 4.23 , the capture rate is 2.22
         # AmLi source
         t = 2E5 / 1.18  # updated 2E5 events capture rate LZ bare is 4.17 , the capture rate is 0.94
-
+        # pn source with lead
+        t = 2E5/1.97 # updated 2E5 events capture rate ,pn capture rate after tagging is 1.97
         # veto analyze
         # t equals the event number in G4 devided by the signal rate after vetoing
 
@@ -1187,7 +1188,7 @@ class multi_MC():
         modeErrPN = 0.05
         binsize = 1
         sourceErr = .01 # change to 0.12 ~ 0.02
-        background = 49
+        background = 23
         backErr = np.round(background ** (1 / 2))
         # energies=[75,100,115,120,140]
         # efficiencies=[0,.2,.50,.8,1]
--- MC_fit/Fur_Ryan/hatch.sh
+++ MC_fit/Fur_Ryan/hatch.sh
@@ -1,6 +1,6 @@
-nameJob="TN_sims_2E5_pn_high_LZ_v2"
+nameJob="TN_sims_2E5_pn_lead_high"
 
-SUBDIR="/home/runze/Documents/results/TN_sims_2E5_pn_high_LZ_v2"
+SUBDIR="/home/runze/Documents/results/TN_sims_2E5_pn_lead_high"
 
 if [ ! -d $SUBDIR ]; then
     mkdir $SUBDIR
