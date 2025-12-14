a165386f4b2648dfa35cacdb25c355b669216933
RunzZhang <52052572+RunzZhang@users.noreply.github.com>
2025-12-13 16:29:35 -0800

    Update readroot_list_Cs_gamma.py

--- Cs/readroot_list_Cs_gamma.py
+++ Cs/readroot_list_Cs_gamma.py
@@ -134,8 +134,8 @@ class RestructureRoot():
 
 class ReadRoot():
     def __init__(self):
-        self.base_path = "/data/runzezhang/result/TN_sims_D/chunked_root_files_Cs_1E6/"
-        self.base_path2 = "/data/runzezhang/result/TN_sims_D/chunked_root_files_Cs_1E6/"
+        self.base_path = "/data/runzezhang/result/TN_sims_D/chunked_root_files_Cs_1E5/"
+        self.base_path2 = "/data/runzezhang/result/TN_sims_D/chunked_root_files_Cs_1E5/"
         self.plot_path = '/data/runzezhang/result/TN_sims_D/plot/'
 
         # self.filepath = self.base_path +"dmx_lr.root"
