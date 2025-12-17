d5c8ec146a1ea058e43d12632620d2505b14df3c
RunzZhang <52052572+RunzZhang@users.noreply.github.com>
2025-12-16 19:30:50 -0800

    Update SN_plot_Cs_both.py

--- Cs/SN_plot_Cs_both.py
+++ Cs/SN_plot_Cs_both.py
@@ -155,29 +155,29 @@ class SN():
         plt.savefig(self.plot_path+"Cs_1E5_multi.pdf")
 
     def read_ER_Ar(self):
-        ER = self.merged_df[self.merged_df["Volume"]=="LAr_phys"]["ER_near/eV"]
-        ER_sum = self.merged_df.groupby("Event")["ER_near/eV"].sum()
+        ER = self.merged_df[self.merged_df["Volume"]=="LAr_phys"]["ER_near/eV"]/1000
+        ER_sum = self.merged_df.groupby("Event")["ER_near/eV"].sum()/1000
         fig, ax = plt.subplots(1,2, figsize=(10, 4))
         ax[0].hist(ER, align="left", rwidth=0.9)
         ax[0].set_xlabel("ER/eV per scattering")
         ax[0].set_ylabel("Counts")
 
         ax[1].hist(ER_sum, align="left", rwidth=0.9)
-        ax[1].set_xlabel("ER/eV per event")
+        ax[1].set_xlabel("ER/keV per event")
         ax[1].set_ylabel("Counts")
         plt.savefig(self.plot_path + "Cs_1E5_ER_Ar.pdf")
 
 
     def read_ER_CF4(self):
-        ER = self.merged_df[self.merged_df["Volume"]=="hydraulic_fluid_phys"]["ER_near/eV"]
-        ER_sum = self.merged_df.groupby("Event")["ER_near/eV"].sum()
+        ER = self.merged_df[self.merged_df["Volume"]=="hydraulic_fluid_phys"]["ER_near/eV"]/1000
+        ER_sum = self.merged_df.groupby("Event")["ER_near/eV"].sum()/1000
         fig, ax = plt.subplots(1, 2, figsize=(10, 4))
         ax[0].hist(ER, align="left", rwidth=0.9)
         ax[0].set_xlabel("ER/eV per scattering")
         ax[0].set_ylabel("Counts")
 
         ax[1].hist(ER_sum, align="left", rwidth=0.9)
-        ax[1].set_xlabel("ER/eV per event")
+        ax[1].set_xlabel("ER/keV per event")
         ax[1].set_ylabel("Counts")
         plt.savefig(self.plot_path + "Cs_1E5_ER_CF4.pdf")
 
