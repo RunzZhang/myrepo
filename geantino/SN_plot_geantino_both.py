7691d3eb3e8bd0065b4d8cbdbfd873a6d4dcc846
RunzZhang <52052572+RunzZhang@users.noreply.github.com>
2025-12-16 17:57:07 -0800

    Update SN_plot_geantino_both.py

--- geantino/SN_plot_geantino_both.py
+++ geantino/SN_plot_geantino_both.py
@@ -144,7 +144,7 @@ class SN():
         print("ceiling_2nd celing", self.merged_df.loc[ceiling_2nd_idxmax, ["R/mm", "Z/mm"]],self.merged_df.loc[ceiling_2nd_idxmin, ["R/mm", "Z/mm"]] )
 
         ceiling_3rd = self.merged_df[
-            (self.merged_df["Volume"] == 'hydraulic_fluid_phys') & (self.merged_df["Z/mm"] < 550) & (self.merged_df["Z/mm"] > 450)& (self.merged_df["R/mm"] < 110)]
+            (self.merged_df["Volume"] == 'hydraulic_fluid_phys') & (self.merged_df["Z/mm"] < 450) & (self.merged_df["Z/mm"] > 350)& (self.merged_df["R/mm"] < 110)]
         ceiling_3rd_idxmax = ceiling_3rd["R/mm"].idxmax()
         ceiling_3rd_idxmin = ceiling_3rd["R/mm"].idxmin()
         print("ceiling_3rd celing", self.merged_df.loc[ceiling_3rd_idxmax, ["R/mm", "Z/mm"]], self.merged_df.loc[ceiling_3rd_idxmin, ["R/mm", "Z/mm"]])
