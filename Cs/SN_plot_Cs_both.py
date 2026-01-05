42c0552f1099f872e34078e528301c15fefe3590
RunzZhang <52052572+RunzZhang@users.noreply.github.com>
2026-01-05 15:08:44 -0800

    Update SN_plot_Cs_both.py

--- Cs/SN_plot_Cs_both.py
+++ Cs/SN_plot_Cs_both.py
@@ -72,7 +72,7 @@ class SN():
         self.original_Activity = 5 # original activity in the paper
         self.Activity = 5  # source practical activity in mivro curie for 50 bubbles/hour
 
-        self.gamma_rate = 18e6 # /s
+        self.gamma_rate = 2.44e6 # /s
 
 
         self.G4_events_gamma =  1E5
@@ -337,7 +337,7 @@ class SN():
 
 
 
-        plt.savefig(self.plot_path + "Cs_1E5_ER_hotrate_cumulative.pdf")
+        plt.savefig(self.plot_path + "Cs_1E5_ER_coldrate_cumulative.pdf")
 
     def read_ER_Ar_CF_2d_sum(self):
         # calcualte sum of ER classified in Ar and CF4
