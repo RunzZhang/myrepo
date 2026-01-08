12e23186ecc2eed22253e8fc3b6bb41b544d9c6e
RunzZhang <52052572+RunzZhang@users.noreply.github.com>
2026-01-07 16:30:21 -0800

    Update SN_plot_Cs_both.py

--- Cs/SN_plot_Cs_both.py
+++ Cs/SN_plot_Cs_both.py
@@ -90,8 +90,8 @@ class SN():
     def data_analysis(self):
         #position distributions histogram, dependisng on step number
         # self.read_positions()
-        self.read_positions_2d_hist()
-        # self.read_positions_zslice()
+        # self.read_positions_2d_hist()
+        self.read_positions_zslice()
         #mulitipliciy distribtuion depending on events
         # self.read_multiplicity()
         # ER distribution per row
