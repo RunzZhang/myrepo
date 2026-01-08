1c96b1e23c01d8cfc6ce46f221f5b4319e01d6db
RunzZhang <52052572+RunzZhang@users.noreply.github.com>
2026-01-07 17:34:14 -0800

    Update SN_plot_Cs_both.py

--- Cs/SN_plot_Cs_both.py
+++ Cs/SN_plot_Cs_both.py
@@ -354,6 +354,7 @@ class SN():
         bin0_len = int(hist_array[0][1][1] - hist_array[0][1][0])
         ax.set_xlabel("ER/keV threshold per deposition in LAr")
         ax.set_ylabel(" Rate mHz")
+        ax.set_yscale("log")
         ax.ticklabel_format(axis="y", style="sci", scilimits=(0, 0))
         ax.minorticks_on()
         # ax[0].grid(which="major", linestyle="-", linewidth=0.8, alpha=0.7)
