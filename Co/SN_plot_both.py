bc2b1d724b106cb2fcc6ab35680ca4c65f9c9091
RunzZhang <52052572+RunzZhang@users.noreply.github.com>
2026-01-20 17:32:16 -0800

    Update SN_plot_both.py

--- Co/SN_plot_both.py
+++ Co/SN_plot_both.py
@@ -9,8 +9,8 @@ class SN():
         # at last change the self.name and plot_name in plot function
         # v2: change back to 2 backgrounds but with finer definitions
         # v4 kill duplicated NRERs
-        self.base_path = "/data/runzezhang/result/TN_sims_D/chunked_root_files_Cs_1E7/"
-        self.base_path2 = "/data/runzezhang/result/TN_sims_D/chunked_root_files_Cs_1E7/" # for gamma path
+        self.base_path = "/data/runzezhang/result/TN_sims_D/chunked_root_files_Co_5E6/"
+        self.base_path2 = "/data/runzezhang/result/TN_sims_D/chunked_root_files_Co_5E6/" # for gamma path
 
         # self.base_path = "/data/runzezhang/result/TN_sims_D/chunked_root_files_pn_outside_1E7/"
         # self.base_path2 = "/data/runzezhang/result/TN_sims_D/chunked_root_files_pn_1E7_outside_gamma/"  # without lead
@@ -23,7 +23,7 @@ class SN():
         self.name2 = "Hard Scatter Background"
         self.name = "Backgrounds"
         self.plot_name = self.name+"PN_1E6_wt_gamma_outside.pdf"
-        self.gamma_bkg_path = self.plot_path+"Cs_gamma position_distribution.pdf"
+        self.gamma_bkg_path = self.plot_path+"Co_gamma position_distribution.pdf"
         self.pho_threshold = 100
 
         cols = ["Event","name", "R/mm", "Z/mm", "Volume", "Process", "ER_near/eV", "Multiplicity"]
@@ -33,9 +33,12 @@ class SN():
 
 
         #982 statics false 1
-        for i in range(1,51):
+        for i in range(1,54):
+            try:
         # for i in range(1, 11):
-            self.main_body(i)
+                self.main_body(i)
+            except:
+                continue
         # self.main_body(1)
         self.combine_df()
         self.data_analysis()
@@ -49,7 +52,7 @@ class SN():
         self.false_gamma_1 = f"PN_gamma_1E7_false1_part{i}.csv"
         self.signal = f"PN_1E7_sig_part{i}.csv"
 
-        self.info_path = self.base_path + f"Cs_gamma_1E6_info_scube_part{i}.csv"
+        self.info_path = self.base_path + f"Co_gamma_1E6_info_scube_part{i}.csv"
 
         self.false_1_path = self.base_path + self.false_1
         self.false_2_path = self.base_path + self.false_2
@@ -89,7 +92,7 @@ class SN():
 
     def data_analysis(self):
         #position distributions histogram, dependisng on step number
-        # self.read_positions()
+        self.read_positions()
         # self.read_positions_2d_hist()
         # self.read_positions_zslice()
         #mulitipliciy distribtuion depending on events
@@ -98,7 +101,7 @@ class SN():
         # ER distribution per row
         # self.read_ER_Ar_CF()
         # self.read_ER_Ar_CF_per_deposit_rate()
-        self.read_ER_Ar_CF_per_deposit_rate_cumulative()
+        # self.read_ER_Ar_CF_per_deposit_rate_cumulative()
         # self.read_ER_CF_per_deposit_rate_cumulative()
         # self.read_ER_Ar_CF_1d_sum()
         # self.read_ER_Ar_CF_2d_sum()
@@ -133,7 +136,7 @@ class SN():
         ax.set_ylabel("Z [mm]")
         cbar = plt.colorbar(sc, ax=ax)
         cbar.set_label("Scatter Order")
-        plt.savefig(self.plot_path+"Cs_1E5_position.pdf")
+        plt.savefig(self.plot_path+"Co_1E5_position.pdf")
 
     def read_positions_zslice(self):
         z_slice = self.merged_df[(self.merged_df["Z/mm"]>=450)&(self.merged_df["Z/mm"]<=550)]
@@ -150,7 +153,7 @@ class SN():
 
         cbar = plt.colorbar(sc[3], ax=ax)
         cbar.set_label("Counts(log)")
-        plt.savefig(self.plot_path + "Cs_1E5_zslice_position_density.pdf")
+        plt.savefig(self.plot_path + "Co_1E5_zslice_position_density.pdf")
 
     def read_positions_2d_hist(self):
         max_multi_num = self.merged_df["Multiplicity"].max()
@@ -177,7 +180,7 @@ class SN():
         ax.set_ylim(-100,800)
         cbar = plt.colorbar(sc[3], ax=ax)
         cbar.set_label("Counts(log)")
-        plt.savefig(self.plot_path+"Cs_1E5_position_density.pdf")
+        plt.savefig(self.plot_path+"Co_1E5_position_density.pdf")
 
 
     def read_multiplicity(self):
@@ -190,7 +193,7 @@ class SN():
 
         ax.set_xlabel("Multiplicity")
         ax.set_ylabel("Probability")
-        plt.savefig(self.plot_path+"Cs_1E7_multi.pdf")
+        plt.savefig(self.plot_path+"Co_1E7_multi.pdf")
 
     def read_Ar_multiplicity(self):
         multiplicity = self.merged_df[self.merged_df["Volume"]=="LAr_phys"].groupby("Event")["Multiplicity"].max()
@@ -202,7 +205,7 @@ class SN():
 
         ax.set_xlabel("Argon Multiplicity")
         ax.set_ylabel("Probability")
-        plt.savefig(self.plot_path + "Cs_1E7_argon_multi.pdf")
+        plt.savefig(self.plot_path + "Co_1E7_argon_multi.pdf")
     def read_ER_Ar_CF(self):
         # per energy deposition
         # the sum is per event
@@ -229,7 +232,7 @@ class SN():
         ax[2].set_xlabel("ER/keV per event")
         ax[2].set_ylabel("Counts")
 
-        plt.savefig(self.plot_path + "Cs_1E5_ER_all.pdf")
+        plt.savefig(self.plot_path + "Co_1E5_ER_all.pdf")
 
     def read_ER_Ar_CF_per_deposit_rate(self):
         # per energy deposition and total
@@ -291,7 +294,7 @@ class SN():
         # ax[2].grid(which="major", linestyle="-", linewidth=0.8, alpha=0.7)
         # ax[2].grid(which="minor", linestyle=":", linewidth=0.5, alpha=0.4)
 
-        plt.savefig(self.plot_path + "Cs_1E7_ER_perdepostion_coldrate.pdf")
+        plt.savefig(self.plot_path + "Co_1E7_ER_perdepostion_coldrate.pdf")
 
 
     def read_ER_Ar_CF_per_deposit_rate_cumulative(self):
@@ -363,7 +366,7 @@ class SN():
         # ax[2].grid(which="major", linestyle="-", linewidth=0.8, alpha=0.7)
         # ax[2].grid(which="minor", linestyle=":", linewidth=0.5, alpha=0.4)
 
-        plt.savefig(self.plot_path + "Cs_1E7_ER_perdepostion_cumulative_coldrate.pdf")
+        plt.savefig(self.plot_path + "Co_1E7_ER_perdepostion_cumulative_coldrate.pdf")
     def read_ER_CF_per_deposit_rate_cumulative(self):
         # rate factor in mHz
         Rate_factor = self.gamma_rate*1000 / (self.G4_events_gamma)
@@ -405,7 +408,7 @@ class SN():
         # ax[0].grid(which="minor", linestyle=":", linewidth=0.5, alpha=0.4)
 
 
-        plt.savefig(self.plot_path + "Cs_1E5_CF4_ER_perdepostion_cumulative_coldrate.pdf")
+        plt.savefig(self.plot_path + "Co_1E5_CF4_ER_perdepostion_cumulative_coldrate.pdf")
     def read_ER_Ar_CF_1d_sum_rate(self):
         # calcualte sum of ER classified in Ar and CF4 per event
         # and sum rate is over both volume in Ar and CF4
@@ -453,7 +456,7 @@ class SN():
 
 
 
-        plt.savefig(self.plot_path + "Cs_1E5_ER_coldrate.pdf")
+        plt.savefig(self.plot_path + "Co_1E5_ER_coldrate.pdf")
 
     def read_ER_Ar_CF_1d_sum_rate_cummulative(self):
         # calcualte sum of ER classified in Ar and CF4 per event
@@ -509,7 +512,7 @@ class SN():
 
 
 
-        plt.savefig(self.plot_path + "Cs_1E5_ER_coldrate_cumulative.pdf")
+        plt.savefig(self.plot_path + "Co_1E5_ER_coldrate_cumulative.pdf")
 
     def read_ER_Ar_CF_2d_sum(self):
         # calcualte sum of ER classified in Ar and CF4 per event
@@ -534,7 +537,7 @@ class SN():
         # ax.set_ylim(-100, 800)
         cbar = plt.colorbar(sc[3], ax=ax)
         cbar.set_label("Counts(log)")
-        plt.savefig(self.plot_path + "Cs_1E5_ER_density.pdf")
+        plt.savefig(self.plot_path + "Co_1E5_ER_density.pdf")
 
 
 
