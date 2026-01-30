5ed7c491616a848708207ad68fe8a2f4a35fb4b1
RunzZhang <52052572+RunzZhang@users.noreply.github.com>
2026-01-29 16:40:55 -0800

    Update SN_plotCf_both.py

--- Cf_coffin/SN_plotCf_both.py
+++ Cf_coffin/SN_plotCf_both.py
@@ -40,19 +40,19 @@ class SN():
 
         #982 statics false 1
         # for i in range(1,101):
-        for i in range(1, 11):
-            self.main_body(i)
-        # self.main_body(1)
+        # for i in range(1, 11):
+        #     self.main_body(i)
+        self.main_body(1)
         # for ploting PN background tagging and SNR
-        self.untagged_bubble_rate()
-        if self.gamma:
-            (result1, result2, resultgamma1) = self.combine_data(self.gamma)
-            self.plot_sn_gamma_v2(result1[0], result2[0], resultgamma1[0],result1[1], result2[1], resultgamma1[1],result1[2], result2[2], resultgamma1[2], result1[3],
-                            result2[3],resultgamma1[3])
-        else:
-            (result1, result2) = self.combine_data(self.gamma)
-            self.plot_sn_v2(result1[0], result2[0], result1[1], result2[1], result1[2], result2[2], result1[3],
-                            result2[3])
+        # self.untagged_bubble_rate()
+        # if self.gamma:
+        #     (result1, result2, resultgamma1) = self.combine_data(self.gamma)
+        #     self.plot_sn_gamma_v2(result1[0], result2[0], resultgamma1[0],result1[1], result2[1], resultgamma1[1],result1[2], result2[2], resultgamma1[2], result1[3],
+        #                     result2[3],resultgamma1[3])
+        # else:
+        #     (result1, result2) = self.combine_data(self.gamma)
+        #     self.plot_sn_v2(result1[0], result2[0], result1[1], result2[1], result1[2], result2[2], result1[3],
+        #                     result2[3])
 
         # for ploting PN spectrum and SBC detector thermalizing effect
         # self.plot_neutron_spectrum()
