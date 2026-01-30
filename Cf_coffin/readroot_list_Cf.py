5ce2b31eecddbd78d23f61b4b2c3c44624640f88
RunzZhang <52052572+RunzZhang@users.noreply.github.com>
2026-01-29 16:55:17 -0800

    Update readroot_list_Cf.py

--- Cf_coffin/readroot_list_Cf.py
+++ Cf_coffin/readroot_list_Cf.py
@@ -304,7 +304,7 @@ class ReadRoot():
         # also include initial energy
         self.initial_position = self.df[(self.df["name"]=="neutron")&(self.df["Volume"]=="cf_source_phys")&(self.df['Step ID'] == 1)][["X/mm", "Y/mm","Z/mm", "Volume"]]
         self.argon_position = self.df[(self.df["name"]=="neutron")&(self.df["Volume"]=="LAr_phys")][["X/mm", "Y/mm","Z/mm", "Volume"]]
-        self.geometry = self.initial_position+self.argon_position
+        self.geometry =  pd.concat([self.initial_position, self.argon_position], axis=0)
         print(self.geometry)
         print(self.initial_position)
         self.geometry.to_csv(self.geometry_path, index = False)
