1e6f11e4d10e0f926248de4761f44e5e36a776c5
RunzZhang <52052572+RunzZhang@users.noreply.github.com>
2026-01-30 16:30:55 -0800

    group calculaton

--- Cf_coffin/SN_plotCf_both.py
+++ Cf_coffin/SN_plotCf_both.py
@@ -43,9 +43,9 @@ class SN():
 
         #982 statics false 1
         # for i in range(1,101):
-        # for i in range(1, 11):
-        #     self.main_body(i)
-        self.main_body(1)
+        for i in range(1, 11):
+            self.main_body(i)
+        # self.main_body(1)
         # self.check_geometry()
         self.NR_spectrum()
         # for ploting PN background tagging and SNR
