1266589ecd1dc921a4f05a090bd722973f54f3b1
RunzZhang <52052572+RunzZhang@users.noreply.github.com>
2026-02-19 09:57:35 -0800

    Update SN_plotCf_both.py

--- Cf_coffin/SN_plotCf_both.py
+++ Cf_coffin/SN_plotCf_both.py
@@ -847,7 +847,7 @@ class SN():
         bin_n = int((max_edge-min_edge)/100)
 
         multiplicity_list = []
-        for i in range(0,bin_n-2):
+        for i in range(0,bin_n):
             energy_threshold = min_edge+ i*100
             energy_mask = self.df_energy["Recoiled/eV"]>= energy_threshold
             df = self.df_energy.loc[energy_mask,:]
@@ -857,7 +857,7 @@ class SN():
             # all bins are int
             multiplicity_min = min(multiplicity)
             multiplicity_max = max(multiplicity)
-            bin_num = multiplicity_max-multiplicity_min
+            bin_num = multiplicity_max-multiplicity_min+1
             bin_range = (multiplicity_min, multiplicity_max)
             print("energy threshold", energy_threshold, "min", multiplicity_min, "max", multiplicity_max, "bin_num", bin_num, "bin_range", bin_range)
             (multiplicity_counts, multiplicity_edges)= np.histogram(multiplicity, bins=bin_num, range=bin_range)
