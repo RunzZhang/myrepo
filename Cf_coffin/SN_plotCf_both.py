50f15fa9f5fae99b83e492dfc2ad141fdcb3fb8f
RunzZhang <52052572+RunzZhang@users.noreply.github.com>
2026-02-19 09:58:31 -0800

    Update SN_plotCf_both.py

--- Cf_coffin/SN_plotCf_both.py
+++ Cf_coffin/SN_plotCf_both.py
@@ -870,7 +870,7 @@ class SN():
 
         fig, ax = plt.subplots()
         for i in range(len(multiplicity_list)):
-            ax.bar(multiplicity_list[i][1], multiplicity_list[i][0], width=multiplicity_list[i][2], align="edge", label="threshold "+str(multiplicity_list[i][3]+" eV") )
+            ax.bar(multiplicity_list[i][1], multiplicity_list[i][0], width=multiplicity_list[i][2], align="edge", label="threshold "+str(multiplicity_list[i][3])+" eV" )
         ax.set_xlabel("Multiplicity")
         ax.set_ylabel("Rate [mHz]")
         ax.set_yscale("log")
