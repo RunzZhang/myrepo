c36bbed3a3e6e130c46764d7b78f549ae581d83e
RunzZhang <52052572+RunzZhang@users.noreply.github.com>
2026-02-25 11:02:42 -0800

    Update SN_plot_both.py

--- Co/SN_plot_both.py
+++ Co/SN_plot_both.py
@@ -401,7 +401,7 @@ class SN():
             threshold = Setiz[j]
             for i in range(len(hist_array[0][1])):
                 if threshold>= hist_array[0][1][i]:
-                    rate = (threshold-hist_array[0][1][i])*Rate_factor*(cumulative_threshold_array[0][i]-cumulative_threshold_array[0][i-1])/(hist_array[0][1][i]-hist_array[0][1][i-1])
+                    rate = Rate_factor*(hist_array[0][1][i]+(threshold-hist_array[0][1][i])*(cumulative_threshold_array[0][i+1]-cumulative_threshold_array[0][i])/(hist_array[0][1][i+1]-hist_array[0][1][i]))
                     rejection = exp_rate[j]/rate
                     rejection_list.append(rejection)
                     break
@@ -444,7 +444,7 @@ class SN():
             threshold = Setiz[j]
             for i in range(len(hist_array[0][1])):
                 if threshold>= hist_array[0][1][i]:
-                    rate = (threshold-hist_array[0][1][i])*Rate_factor*(cumulative_threshold_array[0][i]-cumulative_threshold_array[0][i-1])/(hist_array[0][1][i]-hist_array[0][1][i-1])
+                    rate = Rate_factor*(hist_array[0][1][i]+(threshold-hist_array[0][1][i])*(cumulative_threshold_array[0][i+1]-cumulative_threshold_array[0][i])/(hist_array[0][1][i+1]-hist_array[0][1][i]))
                     rejection = exp_rate[j]/(rate*threshold)
                     rejection_list.append(rejection)
                     break
