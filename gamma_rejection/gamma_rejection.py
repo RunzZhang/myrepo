bc886086a0486295c37c689b8bb9ce24698d8b2d
RunzZhang <52052572+RunzZhang@users.noreply.github.com>
2026-02-28 11:21:27 -0800

    Update gamma_rejection.py

--- gamma_rejection/gamma_rejection.py
+++ gamma_rejection/gamma_rejection.py
@@ -17,21 +17,22 @@ fig, ax = plt.subplots(1,3)
 # 3 graphs, one for original plot(Gray) about rate, 2 and 3 for rejections
 for i in range(len(df_list)):
     path = os.path.join(plot_path, df_list[i] + "_output.txt")
-    df_list = pd.read_csv(path,index_col=0)
+    df = pd.read_csv(path,index_col=0)
     doc_label = df_list[i].rstrip("_exposures")
     # signal
-    ax[0].errorbar(df_list['Updated Setiz [keV]'],df_list["Exp Rate [mHz]"],
-                   yerr = df_list["Exp Sigma [mHz]"],label=doc_label+"signal",fmt=fmt_list[i],color = colors[i][0])
-    ax[0].errorbar(df_list[ 'Updated Setiz [keV]'], df_list[ "Bkg Rate [mHz]"],
-                   yerr=df_list[ "Bkg Sigma [mHz]"], label=doc_label + "bkg",fmt=fmt_list[i],color = colors[i][1])
-    ax[0].errorbar(df_list[ 'Updated Setiz [keV]'], df_list[ "Clean Rate [mHz]"],
-                   yerr=df_list[ "Clean Sigma [mHz]"], label=doc_label + "clean", fmt=fmt_list[i],color = colors[i][2])
+    print(df_list)
+    ax[0].errorbar(df['Updated Setiz [keV]'],df["Exp Rate [mHz]"],
+                   yerr = df["Exp Sigma [mHz]"],label=doc_label+"signal",fmt=fmt_list[i],color = colors[i][0])
+    ax[0].errorbar(df[ 'Updated Setiz [keV]'], df[ "Bkg Rate [mHz]"],
+                   yerr=df[ "Bkg Sigma [mHz]"], label=doc_label + "bkg",fmt=fmt_list[i],color = colors[i][1])
+    ax[0].errorbar(df[ 'Updated Setiz [keV]'], df[ "Clean Rate [mHz]"],
+                   yerr=df[ "Clean Sigma [mHz]"], label=doc_label + "clean", fmt=fmt_list[i],color = colors[i][2])
 
-    ax[1].errorbar(df_list[ 'Updated Setiz [keV]'], df_list[ "Rejection Rate Scattering[mHz]"],
-                   yerr=df_list[ "Rejection Sigma Scattering[mHz]"], label=doc_label,fmt=fmt_list[i])
+    ax[1].errorbar(df[ 'Updated Setiz [keV]'], df[ "Rejection Rate Scattering[mHz]"],
+                   yerr=df[ "Rejection Sigma Scattering[mHz]"], label=doc_label,fmt=fmt_list[i])
 
-    ax[2].errorbar(df_list[ 'Updated Setiz [keV]'], df_list[ "Rejection Rate KeV[mHz]"],
-                   yerr=df_list[ "Rejection Sigma KeV[mHz]"], label=doc_label,fmt=fmt_list[i])
+    ax[2].errorbar(df[ 'Updated Setiz [keV]'], df[ "Rejection Rate KeV[mHz]"],
+                   yerr=df[ "Rejection Sigma KeV[mHz]"], label=doc_label,fmt=fmt_list[i])
 
 ax[0].xlabel("Setiz [keV]")
 ax[0].ylabel("Rate [mHz]")
