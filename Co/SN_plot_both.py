8efc0bfee637330ceeff3d2c6263fe291647a924
RunzZhang <52052572+RunzZhang@users.noreply.github.com>
2026-02-28 11:41:47 -0800

    Update SN_plot_both.py

--- Co/SN_plot_both.py
+++ Co/SN_plot_both.py
@@ -402,8 +402,10 @@ class SN():
             [sum(energy_deposit_list[i:]) for i in range(len(energy_deposit_list))])
 
         source_exposure_df = self.read_exposure(expfile_name + ".txt")
-        print(source_exposure_df.loc[:, 0])
+        # print(source_exposure_df.loc[:, 0])
+        print(source_exposure_df)
         background_exposure_df = self.read_exposure(bkgfile_name + ".txt")
+        print(background_exposure_df)
         Seitz_pressure_list = np.arange(2.25, 6.5, 0.25)
         print(Seitz_pressure_list)
         Setiz = [np.float64(1.3445287166423177), np.float64(1.4677096307281403), np.float64(1.6077252261931916),
