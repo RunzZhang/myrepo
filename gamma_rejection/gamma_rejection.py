6bfa4dc7fa93b8b6032021b0544fdc2a07317b1b
RunzZhang <52052572+RunzZhang@users.noreply.github.com>
2026-02-28 11:15:29 -0800

    Create gamma_rejection.py

---
+++ gamma_rejection/gamma_rejection.py
@@ -0,0 +1,49 @@
+import os
+
+import matplotlib.pyplot as plt
+import numpy as np
+
+import pandas as pd
+
+plot_path = '/data/runzezhang/result/TN_sims_D/plot/'
+df_list = ["Cold-Cs-11_17-18_exposures","Cold-Cs-12_01_exposures","Cold-Cs-12_10-11_exposures","60Co-12_15-16_exposures"]
+fmt_list = ['o','-s','--^',':d']
+colors = [
+    ['#8B0000', '#FF0000', '#FF7F7F'],         #'red'
+     ['#006400', '#228B22', '#90EE90'],        #'green'
+     ['#00008B', '#0000FF', '#ADD8E6'],        #'blue'
+     ['#3E2723', '#795548', '#D7CCC8']]        #'brown'
+fig, ax = plt.subplots(1,3)
+# 3 graphs, one for original plot(Gray) about rate, 2 and 3 for rejections
+for i in range(len(df_list)):
+    path = os.path.join(plot_path, df_list[i] + "_output.txt")
+    df_list = pd.read_csv(df_list[i],index_col=0)
+    doc_label = df_list[i].rstrip("_exposures")
+    # signal
+    ax[0].errorbar(df_list.loc[:,'Updated Setiz [keV]'],df_list.loc[:,"Exp Rate [mHz]"],
+                   yerr = df_list.loc[:,"Exp Sigma [mHz]"],label=doc_label+"signal",fmt=fmt_list[i],color = colors[i][0])
+    ax[0].errorbar(df_list.loc[:, 'Updated Setiz [keV]'], df_list.loc[:, "Bkg Rate [mHz]"],
+                   yerr=df_list.loc[:, "Bkg Sigma [mHz]"], label=doc_label + "bkg",fmt=fmt_list[i],color = colors[i][1])
+    ax[0].errorbar(df_list.loc[:, 'Updated Setiz [keV]'], df_list.loc[:, "Clean Rate [mHz]"],
+                   yerr=df_list.loc[:, "Clean Sigma [mHz]"], label=doc_label + "clean", fmt=fmt_list[i],color = colors[i][2])
+
+    ax[1].errorbar(df_list.loc[:, 'Updated Setiz [keV]'], df_list.loc[:, "Rejection Rate Scattering[mHz]"],
+                   yerr=df_list.loc[:, "Rejection Sigma Scattering[mHz]"], label=doc_label,fmt=fmt_list[i])
+
+    ax[2].errorbar(df_list.loc[:, 'Updated Setiz [keV]'], df_list.loc[:, "Rejection Rate KeV[mHz]"],
+                   yerr=df_list.loc[:, "Rejection Sigma KeV[mHz]"], label=doc_label,fmt=fmt_list[i])
+
+ax[0].xlabel("Setiz [keV]")
+ax[0].ylabel("Rate [mHz]")
+ax[0].title("Signal/BKG Rates ")
+
+ax[1].xlabel("Setiz [keV]")
+ax[1].ylabel("Gamma Rejection Per Scattering []")
+ax[1].title("Gamma Rejection Per Scattering ")
+
+
+ax[1].xlabel("Setiz [keV]")
+ax[1].ylabel("Gamma Rejection Per keV [/keV]")
+ax[1].title("Gamma Rejection Per keV ")
+
+plt.legend()
