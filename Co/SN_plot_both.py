1140c6958523530d8044a9486814e2afb9c97af1
RunzZhang <52052572+RunzZhang@users.noreply.github.com>
2026-02-27 21:00:49 -0800

    Update SN_plot_both.py

--- Co/SN_plot_both.py
+++ Co/SN_plot_both.py
@@ -579,7 +579,7 @@ class SN():
         output_dict = {
             'Src Pressure [bara]': source_pressure_list,
             'Bkg Pressure [bara]': bkg_pressure_recon_list,
-            'Src Setiz [keV]': Setiz,
+            'Updated Setiz [keV]': updated_Setiz_list,
             "Exp Rate [mHz]": exp_rate_list,
             "Bkg Rate [mHz]": background_rate_list,
             "Clean Rate [mHz]": clean_rate_list,
