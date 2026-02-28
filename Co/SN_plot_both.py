619a8200b28b5f4d45721df85b0f89e31eae4dcd
RunzZhang <52052572+RunzZhang@users.noreply.github.com>
2026-02-27 20:29:20 -0800

    Update SN_plot_both.py

--- Co/SN_plot_both.py
+++ Co/SN_plot_both.py
@@ -814,7 +814,7 @@ class SN():
 
         # Read the file
         # sep='\s+' handles any number of spaces or tabs as delimiters
-        df = pd.read_csv(file_path, sep='\s+', skiprows=1, header=None,index=False)
+        df = pd.read_csv(file_path, sep='\s+', skiprows=1, header=None,index_col=0)
 
         return df
 
