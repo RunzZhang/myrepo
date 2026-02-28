d621b807f260e03c4f7b3e7f79b79797aafc0ae1
RunzZhang <52052572+RunzZhang@users.noreply.github.com>
2026-02-28 12:07:35 -0800

    Update gamma_rejection.py

--- gamma_rejection/gamma_rejection.py
+++ gamma_rejection/gamma_rejection.py
@@ -23,8 +23,9 @@ for i in range(len(df_list)):
     # signal
     # drop 2.75,3.25, 3.75 bara pressure
     pressure_drop_list = [2.75,3.25,3.75]
-    for j in pressure_drop_list:
-        df.drop(df[df['Pressure [bara]'] == j].index)
+    df = df[~df['Pressure [bara]'].isin(pressure_drop_list )]
+    # for j in pressure_drop_list:
+    #     df.drop(df[df['Pressure [bara]'] == j].index)
     print(df['Pressure [bara]'])
 
     # ax[0].errorbar(df['Pressure [bara]'],df["Exp Rate [mHz]"],
