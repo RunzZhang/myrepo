5b63af79fe89ed4f740b52c2c578fcaec784992e
RunzZhang <52052572+RunzZhang@users.noreply.github.com>
2026-02-28 11:20:30 -0800

    Update gamma_rejection.py

--- gamma_rejection/gamma_rejection.py
+++ gamma_rejection/gamma_rejection.py
@@ -20,18 +20,18 @@ for i in range(len(df_list)):
     df_list = pd.read_csv(path,index_col=0)
     doc_label = df_list[i].rstrip("_exposures")
     # signal
-    ax[0].errorbar(df_list.loc[:,'Updated Setiz [keV]'],df_list.loc[:,"Exp Rate [mHz]"],
-                   yerr = df_list.loc[:,"Exp Sigma [mHz]"],label=doc_label+"signal",fmt=fmt_list[i],color = colors[i][0])
-    ax[0].errorbar(df_list.loc[:, 'Updated Setiz [keV]'], df_list.loc[:, "Bkg Rate [mHz]"],
-                   yerr=df_list.loc[:, "Bkg Sigma [mHz]"], label=doc_label + "bkg",fmt=fmt_list[i],color = colors[i][1])
-    ax[0].errorbar(df_list.loc[:, 'Updated Setiz [keV]'], df_list.loc[:, "Clean Rate [mHz]"],
-                   yerr=df_list.loc[:, "Clean Sigma [mHz]"], label=doc_label + "clean", fmt=fmt_list[i],color = colors[i][2])
+    ax[0].errorbar(df_list['Updated Setiz [keV]'],df_list["Exp Rate [mHz]"],
+                   yerr = df_list["Exp Sigma [mHz]"],label=doc_label+"signal",fmt=fmt_list[i],color = colors[i][0])
+    ax[0].errorbar(df_list[ 'Updated Setiz [keV]'], df_list[ "Bkg Rate [mHz]"],
+                   yerr=df_list[ "Bkg Sigma [mHz]"], label=doc_label + "bkg",fmt=fmt_list[i],color = colors[i][1])
+    ax[0].errorbar(df_list[ 'Updated Setiz [keV]'], df_list[ "Clean Rate [mHz]"],
+                   yerr=df_list[ "Clean Sigma [mHz]"], label=doc_label + "clean", fmt=fmt_list[i],color = colors[i][2])
 
-    ax[1].errorbar(df_list.loc[:, 'Updated Setiz [keV]'], df_list.loc[:, "Rejection Rate Scattering[mHz]"],
-                   yerr=df_list.loc[:, "Rejection Sigma Scattering[mHz]"], label=doc_label,fmt=fmt_list[i])
+    ax[1].errorbar(df_list[ 'Updated Setiz [keV]'], df_list[ "Rejection Rate Scattering[mHz]"],
+                   yerr=df_list[ "Rejection Sigma Scattering[mHz]"], label=doc_label,fmt=fmt_list[i])
 
-    ax[2].errorbar(df_list.loc[:, 'Updated Setiz [keV]'], df_list.loc[:, "Rejection Rate KeV[mHz]"],
-                   yerr=df_list.loc[:, "Rejection Sigma KeV[mHz]"], label=doc_label,fmt=fmt_list[i])
+    ax[2].errorbar(df_list[ 'Updated Setiz [keV]'], df_list[ "Rejection Rate KeV[mHz]"],
+                   yerr=df_list[ "Rejection Sigma KeV[mHz]"], label=doc_label,fmt=fmt_list[i])
 
 ax[0].xlabel("Setiz [keV]")
 ax[0].ylabel("Rate [mHz]")
