0067d81509090eb522e906163d9ecd15c8d3f717
RunzZhang <52052572+RunzZhang@users.noreply.github.com>
2026-03-01 10:19:27 -0800

    Update gamma_rejection_check.py

--- gamma_rejection/gamma_rejection_check.py
+++ gamma_rejection/gamma_rejection_check.py
@@ -33,11 +33,11 @@ for i in range(len(df_list)):
     #     df.drop(df[df['Pressure [bara]'] == j].index)
     print(df['Pressure [bara]'])
 
-    ax[0].errorbar(df['Pressure [bara]'],df["Exp Rate [mHz]"],
+    ax.errorbar(df['Pressure [bara]'],df["Exp Rate [mHz]"],
                    yerr = df["Exp Sigma [mHz]"],label=doc_label+"signal",fmt=fmt_list[i],color = colors[i][0])
     # ax.errorbar(df['Pressure [bara]'], df[ "Bkg Rate [mHz]"],
     #                yerr=df[ "Bkg Sigma [mHz]"], label=doc_label + "bkg",fmt=fmt_list[i],color = colors[i][1])
-    # ax[0].errorbar(df[ 'Pressure [bara]'], df[ "Clean Rate [mHz]"],
+    # ax.errorbar(df[ 'Pressure [bara]'], df[ "Clean Rate [mHz]"],
     #                yerr=df[ "Clean Sigma [mHz]"], label=doc_label + "clean", fmt=fmt_list[i],color = colors[i][2])
 
 
