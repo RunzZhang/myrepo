034d05b8c136e8a516b524b98353a8a8189537ee
RunzZhang <52052572+RunzZhang@users.noreply.github.com>
2026-03-01 10:05:15 -0800

    Update gamma_rejection.py

--- gamma_rejection/gamma_rejection.py
+++ gamma_rejection/gamma_rejection.py
@@ -30,10 +30,10 @@ for i in range(len(df_list)):
 
     ax[0].errorbar(df['Pressure [bara]'],df["Exp Rate [mHz]"],
                    yerr = df["Exp Sigma [mHz]"],label=doc_label+"signal",fmt=fmt_list[i],color = colors[i][0])
-    ax[0].errorbar(df['Pressure [bara]'], df[ "Bkg Rate [mHz]"],
-                   yerr=df[ "Bkg Sigma [mHz]"], label=doc_label + "bkg",fmt=fmt_list[i],color = colors[i][1])
-    ax[0].errorbar(df[ 'Pressure [bara]'], df[ "Clean Rate [mHz]"],
-                   yerr=df[ "Clean Sigma [mHz]"], label=doc_label + "clean", fmt=fmt_list[i],color = colors[i][2])
+    # ax[0].errorbar(df['Pressure [bara]'], df[ "Bkg Rate [mHz]"],
+    #                yerr=df[ "Bkg Sigma [mHz]"], label=doc_label + "bkg",fmt=fmt_list[i],color = colors[i][1])
+    # ax[0].errorbar(df[ 'Pressure [bara]'], df[ "Clean Rate [mHz]"],
+    #                yerr=df[ "Clean Sigma [mHz]"], label=doc_label + "clean", fmt=fmt_list[i],color = colors[i][2])
 
     ax[1].errorbar(df[ 'Updated Setiz [keV]'], df[ "Rejection Rate Scattering[mHz]"],
                    yerr=df[ "Rejection Sigma Scattering[mHz]"], label=doc_label,fmt=fmt_list[i])
