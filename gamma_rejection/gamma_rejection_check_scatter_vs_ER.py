67d354e0be7e41f48c6581e96c99642383225c9d
RunzZhang <52052572+RunzZhang@users.noreply.github.com>
2026-03-01 20:58:30 -0800

    Update gamma_rejection_check_scatter_vs_ER.py

--- gamma_rejection/gamma_rejection_check_scatter_vs_ER.py
+++ gamma_rejection/gamma_rejection_check_scatter_vs_ER.py
@@ -32,6 +32,7 @@ for i in range(len(df_list)):
     # for j in pressure_drop_list:
     #     df.drop(df[df['Pressure [bara]'] == j].index)
     print(df['Pressure [bara]'])
+    print(df["Rejection Rate KeV[mHz]"])
 
     ax.errorbar(df['Updated Setiz [keV]'], df["Rejection Rate Scattering[mHz]"],
                    yerr=df["Rejection Sigma Scattering[mHz]"], label=doc_label+ "per scattering",fmt=fmt_list[i])
