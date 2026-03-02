1da1b2fdd2dab71ec044832ca32a6590206de5a0
RunzZhang <52052572+RunzZhang@users.noreply.github.com>
2026-03-02 09:12:03 -0800

    check why

--- gamma_rejection/gamma_rejection_check_gray.py
+++ gamma_rejection/gamma_rejection_check_gray.py
@@ -17,8 +17,8 @@ colors = [
      ['#3E2723', '#795548', '#D7CCC8']]        #'brown'
 fig, ax = plt.subplots()
 # 3 graphs, one for original plot(Gray) about rate, 2 and 3 for rejections
-# df_list = df_co_list
-df_list = df_cs_list
+df_list = df_co_list
+# df_list = df_cs_list
 # df_list = df_full_list
 for i in range(len(df_list)):
     path = os.path.join(plot_path, df_list[i] + "_output.txt")
@@ -33,12 +33,12 @@ for i in range(len(df_list)):
     #     df.drop(df[df['Pressure [bara]'] == j].index)
     print(df['Pressure [bara]'])
 
-    ax.errorbar(df['Pressure [bara]'],df["Exp Rate [mHz]"],
-                   yerr = df["Exp Sigma [mHz]"],label=doc_label+"signal",fmt=fmt_list[i],color = colors[i][0])
+    # ax.errorbar(df['Pressure [bara]'],df["Exp Rate [mHz]"],
+    #                yerr = df["Exp Sigma [mHz]"],label=doc_label+"signal",fmt=fmt_list[i],color = colors[i][0])
     # ax.errorbar(df['Pressure [bara]'], df[ "Bkg Rate [mHz]"],
     #                yerr=df[ "Bkg Sigma [mHz]"], label=doc_label + "bkg",fmt=fmt_list[i],color = colors[i][1])
-    # ax.errorbar(df[ 'Pressure [bara]'], df[ "Clean Rate [mHz]"],
-    #                yerr=df[ "Clean Sigma [mHz]"], label=doc_label + "clean", fmt=fmt_list[i],color = colors[i][2])
+    ax.errorbar(df[ 'Pressure [bara]'], df[ "Clean Rate [mHz]"],
+                   yerr=df[ "Clean Sigma [mHz]"], label=doc_label + "clean", fmt=fmt_list[i],color = colors[i][2])
 
 
 ax.set_xlabel("Pressure [bara]")
@@ -48,4 +48,4 @@ ax.legend()
 
 
 
-plt.savefig(plot_path + "gamma_rate_Cs.pdf")
+plt.savefig(plot_path + "gamma_rate_Co.pdf")
