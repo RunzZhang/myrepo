23a4576cae1fc5bdadd7d7b7e7af1c058ebf11b2
RunzZhang <52052572+RunzZhang@users.noreply.github.com>
2026-03-01 20:58:49 -0800

    Update gamma_rejection_check_scatter_vs_ER.py

--- gamma_rejection/gamma_rejection_check_scatter_vs_ER.py
+++ gamma_rejection/gamma_rejection_check_scatter_vs_ER.py
@@ -37,7 +37,7 @@ for i in range(len(df_list)):
     ax.errorbar(df['Updated Setiz [keV]'], df["Rejection Rate Scattering[mHz]"],
                    yerr=df["Rejection Sigma Scattering[mHz]"], label=doc_label+ "per scattering",fmt=fmt_list[i])
     ax2 = ax.twinx()
-    ax.errorbar(df['Updated Setiz [keV]'], df["Rejection Rate KeV[mHz]"],
+    ax2.errorbar(df['Updated Setiz [keV]'], df["Rejection Rate KeV[mHz]"],
                 yerr=df["Rejection Sigma KeV[mHz]"], label=doc_label+"per ER",fmt=fmt_list[i])
     # ax.errorbar(df['Pressure [bara]'], df[ "Bkg Rate [mHz]"],
     #                yerr=df[ "Bkg Sigma [mHz]"], label=doc_label + "bkg",fmt=fmt_list[i],color = colors[i][1])
