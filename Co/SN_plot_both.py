98f21e9cb486b06ea86f423dda13b467989d68f4
RunzZhang <52052572+RunzZhang@users.noreply.github.com>
2026-03-01 20:36:55 -0800

    Update SN_plot_both.py

--- Co/SN_plot_both.py
+++ Co/SN_plot_both.py
@@ -690,13 +690,15 @@ class SN():
         ax[0].set_xlabel("thershold [keV]")
         ax[0].set_ylabel("Counts")
         ax[0].set_title("Cumulative counts vs threshold")
-        ax[0].set_yscale("log")
+        ax[0].set_xlim(0, 5)
+        # ax[0].set_yscale("log")
 
         ax[1].plot(hist_array[0][1][:-1], cumulative_threshold_array[0])
         ax[1].set_xlabel("thershold [keV]")
         ax[1].set_ylabel("Counts*energy [KeV]")
         ax[1].set_title("Cumulative counts*energy vs threshold")
-        ax[1].set_yscale("log")
+        ax[1].set_xlim(0, 5)
+        # ax[1].set_yscale("log")
         print(hist_array[0][1][1]-hist_array[0][1][0],"keV width")
         plt.savefig(self.plot_path + "Co_cumulative_counts_function.pdf")
 
