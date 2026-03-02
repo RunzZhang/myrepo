1c52aafc06cff93d851cd0e16675b8a10a80fa0c
RunzZhang <52052572+RunzZhang@users.noreply.github.com>
2026-03-01 21:12:58 -0800

    Update SN_plot_both.py

--- Co/SN_plot_both.py
+++ Co/SN_plot_both.py
@@ -701,7 +701,7 @@ class SN():
         ax[1].set_xlim(0, 5)
 
         for k in range(10):
-            print(cumulative_threshold_per_scatter_array[0][i]/cumulative_threshold_array[0][i])
+            print(cumulative_threshold_array[0][i]/cumulative_threshold_per_scatter_array[0][i])
         # ax[1].set_yscale("log")
         print(hist_array[0][1][1]-hist_array[0][1][0],"keV width")
         plt.savefig(self.plot_path + "Co_cumulative_counts_function.pdf")
