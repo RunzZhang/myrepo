43dccb3cca7de4333d40d388c05a19746a13b8fb
RunzZhang <52052572+RunzZhang@users.noreply.github.com>
2026-03-04 09:04:24 -0800

    Update SN_plot_both.py

--- Co/SN_plot_both.py
+++ Co/SN_plot_both.py
@@ -542,9 +542,9 @@ class SN():
 
         hist_array = [None]
         # hist_array[0] = np.histogram(ER_Ar, bins=100, range=(0, 1200))
-        hist_array[0] = np.histogram(ER_Ar, bins=4800, range=(0, 1200))
+        hist_array[0] = np.histogram(ER_Ar, bins=12000, range=(0, 1200))
         # 12 keV -> 1 Setiz threshold there is no change for gamma rejection
-        # we need 0.25 keV, and this gives us 4800 bins
+        # we need 0.1 keV, and this gives us 4800 bins
 
         # transfer edge to mid point per bin
 
