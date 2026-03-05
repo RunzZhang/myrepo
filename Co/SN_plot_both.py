511779d72f7db455fae69ddf8878c840a6eb05a2
RunzZhang <52052572+RunzZhang@users.noreply.github.com>
2026-03-04 16:07:31 -0800

    replot in paper scale

--- Co/SN_plot_both.py
+++ Co/SN_plot_both.py
@@ -556,8 +556,10 @@ class SN():
         # histogram per scattering per keV
         cumulative_threshold_array = [None]
         energy_deposit_list = [hist_array[0][0][i] * hist_array[0][1][i] for i in range(len(hist_array[0][0]))]
+
         cumulative_threshold_array[0] = np.array(
             [sum(energy_deposit_list[i:]) for i in range(len(energy_deposit_list))])
+        print("total count* energy", cumulative_threshold_array[0][0])
 
         for expfile_name in exp_file_list:
             source_exposure_df = self.read_exposure(expfile_name + ".txt")
