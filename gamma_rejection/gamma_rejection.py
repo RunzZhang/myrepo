cf3e1c034cd616cdeec6d2b02b43c41000e9e940
RunzZhang <52052572+RunzZhang@users.noreply.github.com>
2026-03-10 14:59:20 -0700

    Update gamma_rejection.py

--- gamma_rejection/gamma_rejection.py
+++ gamma_rejection/gamma_rejection.py
@@ -8,9 +8,8 @@ import pandas as pd
 plot_path = '/data/runzezhang/result/TN_sims_D/plot/'
 df_co_list = ["60Co-12_15-16_exposures"]
 # df_cs_list = ["Cold-Cs-11_17-18_exposures_mix","Cold-Cs-12_01_exposures_mix","Cold-Cs-12_10-11_exposures_mix"]
-# df_cs_list = ["Cold-Cs-1_20-21_exposures",
-#                          "Cold-Cs-2_2-3_exposures"]
-df_cs_list = ["Cold-Cs-1_20-21_exposures"]
+df_cs_list = ["Cold-Cs-1_20-21_exposures_zoom",
+                         "Cold-Cs-2_2-3_exposures_mix"]
 df_full_list = df_co_list+df_cs_list
 # df_list = df_co_list
 df_list = df_cs_list
