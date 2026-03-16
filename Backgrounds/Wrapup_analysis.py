72706c287019fc1bbd67c727d535ecaead77f428
RunzZhang <52052572+RunzZhang@users.noreply.github.com>
2026-03-16 16:51:52 -0700

    Update Wrapup_analysis.py

--- Backgrounds/Wrapup_analysis.py
+++ Backgrounds/Wrapup_analysis.py
@@ -411,8 +411,10 @@ class integrated_analysis():
     def bkg_plot(self):
         self.df_bkg_116 = pd.read_csv(self.Bkg_average_116_path)
         self.df_bkg_116 = pd.merge(self.df_bkg_116, self.df_energy_116_tab, on='Pressure [bara]', how="inner")
+        print('self.df_bkg_116',self.df_bkg_116)
         self.df_bkg_119 = pd.read_csv(self.Bkg_average_119_path)
         self.df_bkg_119 = pd.merge(self.df_bkg_119, self.df_energy_119_tab, on='Pressure [bara]', how="inner")
+        print('self.df_bkg_119', self.df_bkg_119)
 
         fig, ax = plt.subplots()
         ax.errorbar(self.df_bkg_116['Setiz [keV]'],self.df_bkg_116["Bkg Rate [mHz]"],
