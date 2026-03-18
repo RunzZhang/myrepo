10ff6e63078b9f58a16df303d2838467e725f452
RunzZhang <52052572+RunzZhang@users.noreply.github.com>
2026-03-18 16:32:38 -0700

    Update Wrapup_analysis.py

--- Backgrounds/Wrapup_analysis.py
+++ Backgrounds/Wrapup_analysis.py
@@ -440,6 +440,7 @@ class integrated_analysis():
             df = pd.read_csv(self.Cs_exp_rejection_path[i])
             # print(df.columns)
             doc_label = self.Cs_exp_raw_path[i].rstrip("_exposures")
+            print('doc_label',doc_label)
             # signal
             # drop 2.75,3.25, 3.75 bara pressure
             # pressure_drop_list = [2.75,3.25,3.75]
