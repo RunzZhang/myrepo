fbf7246c55fe91486f43f3adf4907efe4e3897ef
RunzZhang <52052572+RunzZhang@users.noreply.github.com>
2026-03-20 16:13:40 -0700

    Update Wrapup_analysis.py

--- Backgrounds/Wrapup_analysis.py
+++ Backgrounds/Wrapup_analysis.py
@@ -583,7 +583,6 @@ class integrated_analysis():
 
         #gamma rejection up limit
         self.bkg_floor_plot(ax[0],"Seitz")
-        self.bkg_floor_plot(ax[0], "Eion")
 
         ax[0].set_xlabel("Seitz [keV]")
         ax[0].set_ylabel("Gamma Rejection Per Scattering []")
@@ -598,7 +597,7 @@ class integrated_analysis():
 
         ax[1].plot(x_fitted_keV, y_fitted_keV, label=f"a,b = {a_fit_keV:.2e} , {b_fit_keV:.2e}", color="black")
 
-        self.bkg_floor_plot(ax[1], "Seitz")
+        
         self.bkg_floor_plot(ax[1], "Eion")
         ax[1].set_xlabel("Eion_rl-1_rhol-1 [GeVcm**2 g-1]")
         ax[1].set_ylabel("Gamma Rejection Per keV [/keV]")
