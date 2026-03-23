13980f00073414ea14d7d0914b929db29fdcb251
RunzZhang <52052572+RunzZhang@users.noreply.github.com>
2026-03-23 14:46:05 -0700

    Update readroot_list_gamma.py

--- Co/readroot_list_gamma.py
+++ Co/readroot_list_gamma.py
@@ -140,7 +140,7 @@ class ReadRoot():
 
         # self.filepath = self.base_path +"dmx_lr.root"
         # self.main_body(1)
-        for i in range(1,54):
+        for i in range(1,56):
             try:
         # for i in range(1, 11):
                 self.main_body(i)
