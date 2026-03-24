e208d62fc9e50bd5e5dfc798e4cf734ba05c398c
RunzZhang <52052572+RunzZhang@users.noreply.github.com>
2026-03-24 09:50:15 -0700

    Update SN_plotCf_both.py

--- Cf_coffin/SN_plotCf_both.py
+++ Cf_coffin/SN_plotCf_both.py
@@ -763,6 +763,8 @@ class SN():
     def NR_spectrum(self):
         rate_factor = 1000*self.rate*self.Activity/(self.original_Activity*self.G4_events) # /ms
 
+        # filter NR = 0 events
+        self.df_energy = self.df_energy[self.df_energy["Recoiled/MeV"]>0]
         self.scatter = self.df_energy[self.df_energy["Process"].isin(['hadElastic', 'neutronInelastic'])]
         self.capture = self.df_energy[self.df_energy["Process"].isin(['nCapture'])]
         max_NR_limit = max(self.scatter["Recoiled/MeV"]*1e6)
