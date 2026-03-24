225a33a63f9aafdff7c648219eecb30408e1c3f7
RunzZhang <52052572+RunzZhang@users.noreply.github.com>
2026-03-24 09:47:19 -0700

    Update SN_plotCf_both.py

--- Cf_coffin/SN_plotCf_both.py
+++ Cf_coffin/SN_plotCf_both.py
@@ -849,7 +849,7 @@ class SN():
 
         multiplicity_list = []
         # for i in range(0,bin_n):
-        for i in range(1, 20,1):
+        for i in range(0, 20,1):
             # energy_threshold = min_edge+ i*100 # every 100 eV
             energy_threshold = min_edge + i * 1  # every 1 eV
             energy_mask = self.df_energy["Recoiled/eV"]>= energy_threshold
