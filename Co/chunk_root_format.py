e1f03a3cd4acdd383d0e1fdd781a3b04ea1ef6ed
RunzZhang <52052572+RunzZhang@users.noreply.github.com>
2026-03-24 09:43:46 -0700

    update

--- Co/chunk_root_format.py
+++ Co/chunk_root_format.py
@@ -95,4 +95,4 @@ class ReadRoot:
 # Example usage:
 if __name__ == "__main__":
     reader = ReadRoot()
-    reader.chunk_and_write_root(start_chunk_cum=27,num_chunks=25)
+    reader.chunk_and_write_root(start_chunk_cum=26,num_chunks=25)
