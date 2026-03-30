23962e8e7d6872cfab77b941b39a0b7ad157fe93
RunzZhang <52052572+RunzZhang@users.noreply.github.com>
2026-03-30 16:49:29 -0700

    Update Wrapup_analysis.py

--- Backgrounds/Wrapup_analysis.py
+++ Backgrounds/Wrapup_analysis.py
@@ -417,8 +417,8 @@ class integrated_analysis():
                    yerr = exp_df["Clean Rate Sigma [mHz]"], label=f"Cf config B {i}",fmt = 'o')
 
 
-        ax[1].plot(self.Cf_simsA[1][0][1][:-1],self.Cf_simsA[0]*self.Cf_simsA[2], label='Cf confiA spectrum')
-        ax[1].plot(self.Cf_simsB[1][0][1][:-1], self.Cf_simsB[0] * self.Cf_simsB[2], label='Cf confiA spectrum')
+        ax[1].plot(self.Cf_simsA[1][0][1][:-1],self.Cf_simsA[0]*self.Cf_simsA[2], label='Cf configA spectrum')
+        ax[1].plot(self.Cf_simsB[1][0][1][:-1], self.Cf_simsB[0] * self.Cf_simsB[2], label='Cf configB spectrum')
 
         ax[0].set_xlabel('Pressure [bara]')
         ax[0].set_ylabel("Exp Rate [mHz]")
