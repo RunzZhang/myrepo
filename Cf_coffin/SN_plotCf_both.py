00c33cd9586c6e449ee1825b372372d1bf347517
RunzZhang <52052572+RunzZhang@users.noreply.github.com>
2026-03-30 17:10:52 -0700

    Update SN_plotCf_both.py

--- Cf_coffin/SN_plotCf_both.py
+++ Cf_coffin/SN_plotCf_both.py
@@ -955,6 +955,7 @@ class SN():
 
 
         print("max(ER_Ar)", max(NR_Ar))
+        print('self.df_energy[self.df_energy["Recoiled/eV"]>1e5]',self.df_energy[self.df_energy["Recoiled/eV"]>1e5])
         max_Ar = max(NR_Ar)
         hist_array = [None]
         hist_array[0] = np.histogram(NR_Ar, bins=int(max_Ar/100), range=(0, max_Ar))
