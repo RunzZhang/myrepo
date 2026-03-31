33f71ac81ec54d6a20a0f81d0cb7ba0460add628
RunzZhang <52052572+RunzZhang@users.noreply.github.com>
2026-03-30 17:53:09 -0700

    Update Wrapup_analysis.py

--- Backgrounds/Wrapup_analysis.py
+++ Backgrounds/Wrapup_analysis.py
@@ -430,7 +430,7 @@ class integrated_analysis():
         ax[1].set_xlabel("Seitz [eV]")
         ax[1].set_ylabel("Clean Rate [mHz]")
         ax[1].set_title("Clean Rate Compare with sims")
-        ax[0].set_xlim(0, 3500)
+        ax[1].set_xlim(0, 3500)
         ax[1].set_ylim(0,600)
         ax[1].legend()
 
