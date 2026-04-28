fa95b872169d7195a8b11b1c15a39dcee886f3b0
RunzZhang <52052572+RunzZhang@users.noreply.github.com>
2026-04-28 10:05:03 -0700

    Update SN_plotCf_both.py

--- Cf_coffin/SN_plotCf_both.py
+++ Cf_coffin/SN_plotCf_both.py
@@ -14,12 +14,12 @@ class SN():
 
         ## change config A to B
         # replace sourcetube_A and config_A
-        self.base_path = "/data/runzezhang/result/TN_sims_D/chunked_root_files_Cf_1E7_sourcetube_A/"
-        self.base_path2 = "/data/runzezhang/result/TN_sims_D/chunked_root_files_Cf_1E7_sourcetube_A/" # for gamma path
+        self.base_path = "/data/runzezhang/result/TN_sims_D/chunked_root_files_Cf_1E7_sourcetube_B/"
+        self.base_path2 = "/data/runzezhang/result/TN_sims_D/chunked_root_files_Cf_1E7_sourcetube_B/" # for gamma path
 
         # self.base_path = "/data/runzezhang/result/TN_sims_D/chunked_root_files_pn_outside_1E7/"
         # self.base_path2 = "/data/runzezhang/result/TN_sims_D/chunked_root_files_pn_1E7_outside_gamma/"  # without lead
-        self.config_string = 'config_A'
+        self.config_string = 'config_B'
         self.plot_path = '/data/runzezhang/result/TN_sims_D/plot/'
         self.TN_spectrum_path = './MC_argon_full_20250701_LSS07_2E5'
         self.false_1 = "PN_false1.csv"
