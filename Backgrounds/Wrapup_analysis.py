70f5f14866f59b3f0960d0d2af1b0f20512540e6
RunzZhang <52052572+RunzZhang@users.noreply.github.com>
2026-05-07 20:55:17 -0700

    Update Wrapup_analysis.py

--- Backgrounds/Wrapup_analysis.py
+++ Backgrounds/Wrapup_analysis.py
@@ -470,7 +470,7 @@ class integrated_analysis():
         ax[3].set_xlabel("Seitz [keV]")
         ax[3].set_ylabel("Clean Rate [mHz] per bin")
         ax[3].set_title("Clean Rate Differential Spectrum")
-        # ax[3].set_yscale("log")
+        ax[3].set_yscale("log")
         ax[3].set_xlim(-1, 200)
         # ax[3].set_ylim(0, 220)
         ax[3].legend()
