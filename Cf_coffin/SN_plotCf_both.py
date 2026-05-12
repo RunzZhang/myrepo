d23d6f679cbbc3ade678c8e3f5938feb00e4ba5e
RunzZhang <52052572+RunzZhang@users.noreply.github.com>
2026-05-12 10:37:28 -0700

    Update SN_plotCf_both.py

--- Cf_coffin/SN_plotCf_both.py
+++ Cf_coffin/SN_plotCf_both.py
@@ -820,7 +820,7 @@ class SN():
 
         ax[0].set_xlabel("R- $R_{center}$ [mm]")
         ax[0].set_ylabel("Z [mm]")
-        ax[0].set_xlim(-77,-75)
+        # ax[0].set_xlim(-77,-75)
         # ax[0].set_ylim(-690,-680)
         cbar0 = plt.colorbar(sc0[3], ax=ax[0])
         cbar0.set_label("Counts(log)")
