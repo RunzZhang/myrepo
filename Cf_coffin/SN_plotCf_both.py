d608ae3ccf6ffd3631076ff4040ac62a97e8fc49
RunzZhang <52052572+RunzZhang@users.noreply.github.com>
2026-05-12 16:13:52 -0700

    Update SN_plotCf_both.py

--- Cf_coffin/SN_plotCf_both.py
+++ Cf_coffin/SN_plotCf_both.py
@@ -806,7 +806,7 @@ class SN():
 
         self.active["PreKinetic/keV"] = self.active["PreKinetic/MeV"] * 1000
 
-        print("total runs", len(self.active['Event'].tolist())
+        print("total runs", len(self.active['Event'].tolist()))
 
         self.leaving_source["PreKinetic/keV"] = self.leaving_source["PreKinetic/MeV"] * 1000
         self.leaving_source["PostKinetic/keV"] = self.leaving_source["PostKinetic/MeV"] * 1000
