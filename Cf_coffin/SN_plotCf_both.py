0bc6bd6b926c68a9649c8cb7444010c850510707
RunzZhang <52052572+RunzZhang@users.noreply.github.com>
2026-05-14 13:21:21 -0700

    Update SN_plotCf_both.py

--- Cf_coffin/SN_plotCf_both.py
+++ Cf_coffin/SN_plotCf_both.py
@@ -1152,18 +1152,21 @@ class SN():
         self.df_sstl_energy = self.df_energy[self.df_energy['Event'].isin(self.low_e_n_list)]
         self.scatter = self.df_sstl_energy[self.df_sstl_energy["Process"].isin(['hadElastic', 'neutronInelastic'])]
         self.capture = self.df_sstl_energy[self.df_sstl_energy["Process"].isin(['nCapture'])]
+
         max_NR_limit = max(self.scatter["Recoiled/MeV"]*1e3)
         print("maximum scatter recoil energy",max_NR_limit)
         # bin info and maybe same for both category
         # 100 ev per bin
         bin_num= int(max_NR_limit/100)+1
         max_bin_range= bin_num*100
+        print("bin_num",bin_num,"max_bin_range",max_bin_range)
 
         bin_range= (0,max_bin_range)
         (scatter_counts, scatter_edge) = np.histogram(self.scatter["Recoiled/MeV"]*1e3, bins=bin_num, range=bin_range)
         capture_counts = len(self.capture["Recoiled/MeV"])
         # read thermal neutron recoiled spectrum by MCMC
         self.TN_recoil_list = self.read_TN_R_spectrum()# in eV
+        self.TN_recoil_list = [i*1000 for i in self.TN_recoil_list] #kev
         (capture_counts, capture_edge) = np.histogram(self.TN_recoil_list, density=True,bins=bin_num, range=bin_range)
         width = capture_edge[1]-capture_edge[0]
         #0th order just a threshold
