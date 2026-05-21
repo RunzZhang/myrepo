7b97073d05ecd078a5da575873c5efe898da98b9
RunzZhang <52052572+RunzZhang@users.noreply.github.com>
2026-05-21 11:01:52 -0700

    Update Wrapup_analysis.py

--- Backgrounds/Wrapup_analysis.py
+++ Backgrounds/Wrapup_analysis.py
@@ -610,7 +610,7 @@ class integrated_analysis():
         ax[0].plot(self.Cf_Density104_energy[:-1], self.Cf_Density104_rate, label=f'Density 1.04 $g/cm^3$')
         ax[0].plot(self.Cf_density20_energy[:-1], self.Cf_density20_rate, label=f'Density 1.50 $g/cm^3$')
 
-        ax[1].plot(self.Cf_Boron00, self.Cf_Boron00_rate, label=f'Boron 0%')
+        ax[1].plot(self.Cf_Boron00[:-1], self.Cf_Boron00_rate, label=f'Boron 0%')
         ax[1].plot(self.Cf_Density104_energy[:-1], self.Cf_Density104_rate, label=f'Boron 5%')
         ax[1].plot(self.Cf_Boron10_energy[:-1], self.Cf_Boron10_rate, label=f'Boron 10%')
 
