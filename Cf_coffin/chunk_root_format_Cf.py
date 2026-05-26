9863ad27ded7dd8aaad0bfdc67885b3a885c4e97
RunzZhang <52052572+RunzZhang@users.noreply.github.com>
2026-05-26 11:54:42 -0700

    z =-10cm

--- Cf_coffin/chunk_root_format_Cf.py
+++ Cf_coffin/chunk_root_format_Cf.py
@@ -26,7 +26,7 @@ class ReadRoot:
         # self.signal_path_mid = self.base_path + self.signal_mid
         # self.signal_path = self.base_path + self.signal
 
-        self.filepath = self.base_path + "dmx_Cf_1E7_zp10_sourcetube_B.root"
+        self.filepath = self.base_path + "dmx_Cf_1E7_zm10_sourcetube_B.root"
         # self.filepath = self.base_path + "dmx_AmLi.root" # test
         self.tree_name = "tree"  # Assuming your TTree is named "tree"
 
@@ -39,7 +39,7 @@ class ReadRoot:
 
     def chunk_and_write_root(self, num_chunks=20, output_dir=None):
         if output_dir is None:
-            output_dir = os.path.join(self.base_path, "chunked_root_files_Cf_1E7_zp10_sourcetube_B")
+            output_dir = os.path.join(self.base_path, "chunked_root_files_Cf_1E7_zm10_sourcetube_B")
         os.makedirs(output_dir, exist_ok=True)
 
         with uproot.open(self.filepath) as file:
--- Cf_coffin/readroot_list_Cf.py
+++ Cf_coffin/readroot_list_Cf.py
@@ -134,8 +134,8 @@ class RestructureRoot():
 
 class ReadRoot():
     def __init__(self):
-        self.base_path = "/data/runzezhang/result/TN_sims_D/chunked_root_files_Cf_1E7_zp10_sourcetube_B/"
-        self.base_path2 = "/data/runzezhang/result/TN_sims_D/chunked_root_files_Cf_1E7_zp10_sourcetube_B/"
+        self.base_path = "/data/runzezhang/result/TN_sims_D/chunked_root_files_Cf_1E7_zm10_sourcetube_B/"
+        self.base_path2 = "/data/runzezhang/result/TN_sims_D/chunked_root_files_Cf_1E7_zm10_sourcetube_B/"
         self.plot_path = '/data/runzezhang/result/TN_sims_D/plot/'
 
         # self.filepath = self.base_path +"dmx_lr.root"
