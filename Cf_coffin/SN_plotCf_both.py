80c09ab43501342b321e5ef5c0c67415cac1c20c
RunzZhang <52052572+RunzZhang@users.noreply.github.com>
2026-05-30 11:16:58 -0700

    Update SN_plotCf_both.py

--- Cf_coffin/SN_plotCf_both.py
+++ Cf_coffin/SN_plotCf_both.py
@@ -913,10 +913,10 @@ class SN():
             return 0
         ffig, ax = plt.subplots(1,3,figsize=(24,4))
         # X and Y
-        # sc0=ax[0].hist2d(self.leaving_coffin_Y_slice["X/mm"],self.leaving_coffin_Y_slice["Z/mm"],bins=50,
-        # cmap="plasma",norm="log",alpha=0.7)
-        sc0 = ax[0].hist2d(self.leaving_coffin_Y_slice["X/mm"], self.leaving_coffin_Y_slice["Z/mm"], bins=50,
-                           cmap="plasma", alpha=0.7,norm=colors.LogNorm(vmin=1))
+        sc0=ax[0].hist2d(self.leaving_coffin_Y_slice["X/mm"],self.leaving_coffin_Y_slice["Z/mm"],bins=50,
+        cmap="plasma",norm="log",alpha=0.7)
+        # sc0 = ax[0].hist2d(self.leaving_coffin_Y_slice["X/mm"], self.leaving_coffin_Y_slice["Z/mm"], bins=50,
+        #                    cmap="plasma", alpha=0.7,norm=colors.LogNorm(vmin=1))
 
         # ax.plot([189.95,189.95, 0.8485], [0,663.22, 714.03], color="red")
         # ax.plot([114.98,114.98, 0.75575], [0,587.01, 617.78], color="blue")
@@ -929,10 +929,10 @@ class SN():
         cbar0 = plt.colorbar(sc0[3], ax=ax[0])
         cbar0.set_label("Counts(log)")
 
-        # sc1 = ax[1].hist2d(self.leaving_coffin_X_slice["Y/mm"], self.leaving_coffin_X_slice["Z/mm"], bins=50,
-        #                    cmap="plasma", norm="log", alpha=0.7)
         sc1 = ax[1].hist2d(self.leaving_coffin_X_slice["Y/mm"], self.leaving_coffin_X_slice["Z/mm"], bins=50,
-                           cmap="plasma", alpha=0.7,norm=colors.LogNorm(vmin=1))
+                           cmap="plasma", norm="log", alpha=0.7)
+        # sc1 = ax[1].hist2d(self.leaving_coffin_X_slice["Y/mm"], self.leaving_coffin_X_slice["Z/mm"], bins=50,
+        #                    cmap="plasma", alpha=0.7,norm=colors.LogNorm(vmin=1))
 
         # ax.plot([189.95,189.95, 0.8485], [0,663.22, 714.03], color="red")
         # ax.plot([114.98,114.98, 0.75575], [0,587.01, 617.78], color="blue")
