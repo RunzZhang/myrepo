7f3b462fb4a9b48ef52d4e57f08b0415fd9741da
RunzZhang <52052572+RunzZhang@users.noreply.github.com>
2026-05-30 16:14:10 -0700

    Update Wrapup_analysis.py

--- Backgrounds/Wrapup_analysis.py
+++ Backgrounds/Wrapup_analysis.py
@@ -857,7 +857,7 @@ class integrated_analysis():
         ax[1, 0].set_xlabel("Seitz [eV]")
         ax[1, 0].set_ylabel("Clean Rate [mHz/bin]")
         ax[1, 0].set_title("Diff Rate with Different PE Density")
-        # ax[1, 0].set_xlim(-1, 3500)
+        ax[1, 0].set_xlim(-1, 3500)
         # ax[1, 0].set_ylim(0, 220)
         ax[1, 0].set_yscale("log")
         ax[1, 0].legend()
