929b1d74f0d669d9fccbfa11c251b589f95c1a9c
RunzZhang <52052572+RunzZhang@users.noreply.github.com>
2026-05-31 20:39:56 -0700

    test 150

--- Cf_coffin/SN_plotCf_both.py
+++ Cf_coffin/SN_plotCf_both.py
@@ -1472,7 +1472,7 @@ class SN():
             self.argon = self.df_geo[(self.df_geo["Volume"] == "LAr_phys")&(self.df_geo["Event"].isin(lar_event_list))]
             argon_energy = self.argon["PreKinetic/MeV"]
             argon_energy["PreKinetic/keV"] = self.argon["PreKinetic/MeV"]*1000
-            # 50keV per bin
+            # 100keV per bin
             max_first_argon = max(self.argon["PreKinetic/MeV"])*1e6
             first_argon_ene_array[0] = np.histogram(argon_energy["PreKinetic/keV"],bins=int(max_first_argon/100000), range=(0, max_first_argon/1000))
         except:
