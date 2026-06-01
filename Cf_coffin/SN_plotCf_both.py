ff0bbe131f7da476cc285731534646ad5a7180c0
RunzZhang <52052572+RunzZhang@users.noreply.github.com>
2026-05-31 18:59:25 -0700

    Update SN_plotCf_both.py

--- Cf_coffin/SN_plotCf_both.py
+++ Cf_coffin/SN_plotCf_both.py
@@ -1471,7 +1471,7 @@ class SN():
             argon_energy["PreKinetic/keV"] = self.argon["PreKinetic/MeV"]*1000
             # 50keV per bin
             max_first_argon = max(self.argon["PreKinetic/MeV"])*1e6
-            first_argon_ene_array[0] = np.histogram(argon_energy["PreKinetic/keV"],bins=int(max_first_argon/50000), range=(0, max_first_argon/1000))
+            first_argon_ene_array[0] = np.histogram(argon_energy["PreKinetic/keV"],bins=int(max_first_argon/100000), range=(0, max_first_argon/1000))
         except:
             print("error in neutron entering argon")
         finally:
