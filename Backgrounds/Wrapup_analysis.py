ae34be8793c7e20d1570fcec9318dacfcec39752
RunzZhang <52052572+RunzZhang@users.noreply.github.com>
2026-06-01 12:01:36 -0700

    Update Wrapup_analysis.py

--- Backgrounds/Wrapup_analysis.py
+++ Backgrounds/Wrapup_analysis.py
@@ -862,7 +862,7 @@ class integrated_analysis():
         ax[0,0].set_ylabel("Clean Rate [mHz]")
         ax[0,0].set_title("Rate with Different PE Density Boron = 5%")
         # ax[0,0].set_xlim(-1, 3500)
-        ax[0, 0].set_xlim(0, 200000)
+        ax[0, 0].set_xlim(0, 300)
         # ax[0,0].set_ylim(0, 220)
 
         ax[0,0].legend()
