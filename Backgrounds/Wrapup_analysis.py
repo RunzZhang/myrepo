09ff96c1e0a4fd29d94da3696c65244e28115a8e
RunzZhang <52052572+RunzZhang@users.noreply.github.com>
2026-05-31 21:13:47 -0700

    Update Wrapup_analysis.py

--- Backgrounds/Wrapup_analysis.py
+++ Backgrounds/Wrapup_analysis.py
@@ -803,12 +803,12 @@ class integrated_analysis():
         #                    self.Cf_Density104_rate - self.Cf_Density104_error, color='red', alpha=0.2)
         # ax[0,3].fill_between(self.Cf_CF4temp_140K_energy[:-1],self.Cf_CF4temp_140K_rate+self.Cf_CF4temp_140K_error,self.Cf_CF4temp_140K_rate-self.Cf_CF4temp_140K_error,color='purple', alpha=0.2)
 
-        ax[1,0].plot(self.Cf_simsB[4][1][:-1],self.Cf_simsB[4][0]*self.Cf_simsB[0], label=f'Density 0.95 $g/cm^3$',color="black")
-        ax[1, 0].plot(self.Cf_Density104[4][1][:-1], self.Cf_Density104[4][0]*self.Cf_Density104[0], label=f'Density 1.04 $g/cm^3$',
-                      color="b")
+        # ax[1,0].plot(self.Cf_simsB[4][1][:-1],self.Cf_simsB[4][0]*self.Cf_simsB[0], label=f'Density 0.95 $g/cm^3$',color="black")
+        # ax[1, 0].plot(self.Cf_Density104[4][1][:-1], self.Cf_Density104[4][0]*self.Cf_Density104[0], label=f'Density 1.04 $g/cm^3$',
+        #               color="b")
         # ax[1, 0].plot(self.Cf_Density125_energy[:-1], self.Cf_Density125_diff_rate, label=f'Density 1.25 $g/cm^3$',
         #               color="orange")
-        print(self.Cf_Density150[4][0]*self.Cf_Density150[0])
+
         ax[1, 0].plot(self.Cf_Density150[4][1][:-1]/1000, self.Cf_Density150[4][0]*self.Cf_Density150[0], label=f'Density 1.50 $g/cm^3$',
                       color="green")
         # ax[1, 0].plot(self.Cf_Density175_energy[:-1]/1000, self.Cf_Density175_diff_rate, label=f'Density 1.75 $g/cm^3$',
@@ -876,9 +876,9 @@ class integrated_analysis():
         ax[1, 0].set_xlabel("Kinetic Energy [keV]")
         ax[1, 0].set_ylabel("Clean Rate [mHz/bin]")
         ax[1, 0].set_title("Diff Kinetic Spectrum For neutron first entering Ar")
-        ax[1, 0].set_xlim(100, 2500)
+        # ax[1, 0].set_xlim(100, 2500)
         # ax[1, 0].set_xlim(480, 500) #487
-        ax[1, 0].set_ylim(0, 5)
+        # ax[1, 0].set_ylim(0, 5)
         # ax[1, 0].set_yscale("log")
         ax[1, 0].legend()
 
