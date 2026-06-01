10ad5b72382eec00254d0e5f2c7bbc332159c689
RunzZhang <52052572+RunzZhang@users.noreply.github.com>
2026-05-31 18:09:04 -0700

    Update SN_plotCf_both.py

--- Cf_coffin/SN_plotCf_both.py
+++ Cf_coffin/SN_plotCf_both.py
@@ -1495,7 +1495,7 @@ class SN():
         lar_event_list = self.df_energy["Event"].tolist()
         self.argon = self.df_geo[(self.df_geo["Volume"] == "LAr_phys")&(self.df_geo["Event"].isin(lar_event_list))]
         argon_energy = self.argon
-        argon_energy["PreKinetic/keV"] = self.argon["PreKinetic/MeV"]*1000
+        argon_energy["PreKinetic/keV"] = argon_energy["PreKinetic/MeV"]*1000
         # tagged1_event_list =argon_energy[(argon_energy["PreKinetic/keV"].values>486)&(argon_energy["PreKinetic/keV"].values<487.5)]["Event"].tolist()
         #
         # Explicitly use .loc for label/boolean alignment
