9797a3fa43fa339c1cee687749a44904af82ac88
RunzZhang <52052572+RunzZhang@users.noreply.github.com>
2026-06-05 09:27:36 -0700

    Update Wrapup_analysis.py

--- Backgrounds/Wrapup_analysis.py
+++ Backgrounds/Wrapup_analysis.py
@@ -913,7 +913,7 @@ class integrated_analysis():
         ax[1, 1].set_xlabel("Kinetic Energy [keV]")
         ax[1, 1].set_ylabel("Clean Rate [mHz]")
         ax[1, 1].set_title("Cumulative Kinetic Spectrum For neutron first entering Ar")
-        ax[1, 1].set_xlim(0, 2500)
+        ax[1, 1].set_xlim(0, 3000)
         ax[1, 1].set_ylim(0, 220)
         # ax[1, 1].set_yscale("log")
         ax[1, 1].legend()
