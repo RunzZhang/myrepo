f6e85a2796aa0edd37e57f66b63cb24ff0db2214
RunzZhang <52052572+RunzZhang@users.noreply.github.com>
2026-06-07 15:52:07 -0700

    Update Wrapup_analysis.py

--- Backgrounds/Wrapup_analysis.py
+++ Backgrounds/Wrapup_analysis.py
@@ -910,7 +910,7 @@ class integrated_analysis():
         ax[1, 0].set_title("Diff Kinetic Spectrum For neutron first entering Ar")
         ax[1, 0].set_xlim(100, 3000)
         # ax[1, 0].set_xlim(480, 500) #487
-        ax[1, 0].set_ylim(0, 5)
+        # ax[1, 0].set_ylim(0, 5)
         ax[1, 0].set_yscale("log")
         ax[1, 0].legend()
 
