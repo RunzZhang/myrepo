94057c07bffc1d8c66a30047fcb7f469cdfb5853
RunzZhang <52052572+RunzZhang@users.noreply.github.com>
2026-06-10 11:11:10 -0700

    Update SN_plotCf_both.py

--- Cf_coffin/SN_plotCf_both.py
+++ Cf_coffin/SN_plotCf_both.py
@@ -893,13 +893,17 @@ class SN():
         self.leaving_coffin = self.leaving_coffin_init.loc[self.leaving_coffin_init.groupby('Event')['Step ID'].idxmax()]
         print("leaving coffin",self.leaving_coffin[:20])
 
+        # self.test_volume = self.df_phys[
+        #     (self.df_phys["name"] == "neutron") & (self.df_phys["Event"].isin(lar_event_list))]
+        coffin_horizontal_list = ["block1_phys", "block2_phys", "block3_phys",
+                       "block4_phys"]
         self.test_volume = self.df_phys[
-            (self.df_phys["name"] == "neutron") & (self.df_phys["Event"].isin(lar_event_list))]
+            (self.df_phys["name"] == "neutron") & (self.df_phys["Volume"].isin(coffin_horizontal_list))]
 
 
         # pd.set_option('display.max_columns', None)
-        # print("test", self.test_volume[self.test_volume["Event"]==20388])
-        # print("test", self.df_phys[self.df_phys["Event"] == 20388])
+        print("test", self.test_volume)
+        print("test", self.df_phys[self.df_phys["Event"] == 20388])
 
 
 
