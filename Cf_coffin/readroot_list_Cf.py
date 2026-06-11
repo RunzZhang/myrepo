b6f326f1d8d305b6358f9c2c6577244ef11de714
RunzZhang <52052572+RunzZhang@users.noreply.github.com>
2026-06-10 18:48:16 -0700

    Update readroot_list_Cf.py

--- Cf_coffin/readroot_list_Cf.py
+++ Cf_coffin/readroot_list_Cf.py
@@ -141,8 +141,8 @@ class ReadRoot():
 
         # self.filepath = self.base_path +"dmx_lr.root"
         # self.main_body(1)
-        # for i in range(1,101):
-        for i in range(1, 11):
+        for i in range(1,101):
+        # for i in range(1, 11):
             self.main_body(i)
     def main_body(self,i):
         print(i)
@@ -286,9 +286,9 @@ class ReadRoot():
                     (self.df["Volume"].isin(volume_list))]
         # print(self.phys)
 
-        print("test check df", self.df[self.df["Event"]==6624])
+        # print("test check df", self.df[self.df["Event"]==6624])
         # print("test check phys", self.phys[self.phys["Event"] == 20388])
-        sys.exit(0)
+        # sys.exit(0)
         self.phys.to_csv(self.phys_path, index = False)
 
     def source_geometry(self):
