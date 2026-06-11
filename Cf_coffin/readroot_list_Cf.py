c8519cd178785edb351e4a72e403b28ee3a1c245
RunzZhang <52052572+RunzZhang@users.noreply.github.com>
2026-06-11 11:14:38 -0700

    check density 150 curve

--- Cf_coffin/readroot_list_Cf.py
+++ Cf_coffin/readroot_list_Cf.py
@@ -141,8 +141,8 @@ class ReadRoot():
 
         # self.filepath = self.base_path +"dmx_lr.root"
         # self.main_body(1)
-        # for i in range(1,101):
-        for i in range(1, 11):
+        for i in range(1,101):
+        # for i in range(1, 11):
             self.main_body(i)
     def main_body(self,i):
         print(i)
