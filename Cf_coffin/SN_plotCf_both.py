5d7863bdda44f74b1b139aef0328f295e6f3a6ee
RunzZhang <52052572+RunzZhang@users.noreply.github.com>
2026-06-11 10:58:10 -0700

    Update SN_plotCf_both.py

--- Cf_coffin/SN_plotCf_both.py
+++ Cf_coffin/SN_plotCf_both.py
@@ -900,7 +900,7 @@ class SN():
         # energy and process mask
         # process_mask2 = (self.df_phys["Process"] == "neutronInelastic")
         mask2 = (self.leaving_coffin["Process"] == "hadElastic") &(self.leaving_coffin["PreKinetic/MeV"]
-            <=1)
+            >= 1)
         # mask2 =  True
         self.leaving_coffin =self.leaving_coffin[mask2]
 
