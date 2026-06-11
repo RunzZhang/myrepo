6c48125f8b502c2b88fa63862aec09b70070f492
RunzZhang <52052572+RunzZhang@users.noreply.github.com>
2026-06-10 18:50:51 -0700

    Update SN_plotCf_both.py

--- Cf_coffin/SN_plotCf_both.py
+++ Cf_coffin/SN_plotCf_both.py
@@ -52,8 +52,8 @@ class SN():
 
 
         #982 statics false 1
-        # for i in range(1,101):
-        for i in range(1, 11):
+        for i in range(1,101):
+        # for i in range(1, 11):
             self.main_body(i)
         # self.main_body(1)
         #check the intial neutron postions, argon volume and the intial neutron energy spectrum
