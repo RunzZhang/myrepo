d061c0531f0d838b9ad5dc465857fadcd4dadb1b
RunzZhang <52052572+RunzZhang@users.noreply.github.com>
2026-06-10 18:30:54 -0700

    Update SN_plotCf_both.py

--- Cf_coffin/SN_plotCf_both.py
+++ Cf_coffin/SN_plotCf_both.py
@@ -885,7 +885,8 @@ class SN():
 
 
         # process_mask = (self.df_phys["Process"] != "Transportation")
-        process_mask = (self.df_phys["Process"] == "neutronInelastic")
+        # process_mask = (self.df_phys["Process"] == "neutronInelastic")
+        process_mask = (self.df_phys["Process"] == "hadElastic")
 
         self.leaving_coffin_init = self.df_phys[(self.df_phys["name"] == "neutron") & (self.df_phys["Volume"].isin(coffin_volume_list))&process_mask]
         # also requires neutron entering argon
