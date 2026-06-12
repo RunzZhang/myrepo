340fb2e56fe77931e3a886c1f1bbb1d53976bfa5
RunzZhang <52052572+RunzZhang@users.noreply.github.com>
2026-06-11 20:00:25 -0700

    Update SN_plotCf_both.py

--- Cf_coffin/SN_plotCf_both.py
+++ Cf_coffin/SN_plotCf_both.py
@@ -873,9 +873,11 @@ class SN():
 
         rate_factor = 1000 * self.rate * self.Activity / (self.original_Activity * self.G4_events)
 
-        coffin_volume_list = ["block1_phys", "block2_phys", "block3_phys",
-                       "block4_phys", "block5_phys", "block6_phys", "block7_phys", "block8_phys", "block9_phys",
-                       "block10_phys", "block11_phys"]
+        # coffin_volume_list = ["block1_phys", "block2_phys", "block3_phys",
+        #                "block4_phys", "block5_phys", "block6_phys", "block7_phys", "block8_phys", "block9_phys",
+        #                "block10_phys", "block11_phys"]
+        coffin_volume_list = ["cf_active_phys", "cf_source_phys", "block1_phys", "block2_phys", "block3_phys",
+   "block4_phys","block5_phys","block6_phys","block7_phys","block8_phys","block9_phys","block10_phys","block11_phys"]
         self.active = self.df_phys[(self.df_phys["Volume"] == "cf_active_phys")&(self.df_phys["Step ID"] == 1)]
 
         # find center x and center Y locations
@@ -890,11 +892,11 @@ class SN():
         # process_mask = True
         # find high energy or low energy
         # high_energy_neutron_list= self.df_phys[(self.df_phys["name"] == "neutron")&(self.df_phys["Step ID"] == 1)&(self.df_phys["PreKinetic/MeV"] < 1)]["Event"].tolist()
-
-        self.leaving_coffin_init = self.df_phys[(self.df_phys["name"] == "neutron") & (self.df_phys["Volume"].isin(coffin_volume_list))&process_mask]
+        # no requirement
+        # self.leaving_coffin_init = self.df_phys[(self.df_phys["name"] == "neutron") & (self.df_phys["Volume"].isin(coffin_volume_list))&process_mask]
         # also requires neutron entering argon
-        # self.leaving_coffin_init = self.df_phys[
-        #     (self.df_phys["name"] == "neutron") & (self.df_phys["Volume"].isin(coffin_volume_list))&(self.df_phys["Event"].isin(lar_event_list))]
+        self.leaving_coffin_init = self.df_phys[
+            (self.df_phys["name"] == "neutron") & (self.df_phys["Volume"].isin(coffin_volume_list))&(self.df_phys["Event"].isin(lar_event_list))&process_mask]
 
         self.leaving_coffin = self.leaving_coffin_init.loc[self.leaving_coffin_init.groupby('Event')['Step ID'].idxmax()]
 
