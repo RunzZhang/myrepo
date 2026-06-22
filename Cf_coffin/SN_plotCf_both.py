57329e462890f00de7237e22c0b2baa36c67664c
RunzZhang <52052572+RunzZhang@users.noreply.github.com>
2026-06-22 08:38:01 -0700

    Update SN_plotCf_both.py

--- Cf_coffin/SN_plotCf_both.py
+++ Cf_coffin/SN_plotCf_both.py
@@ -1442,7 +1442,7 @@ class SN():
             ax.errorbar(energy_th, ratio_t[i],
                         yerr=ratio_error_t[i], label=f"multiplicity = {i+1}")
             print(f"multiplicity = {i+1}", energy_th,
-                  ratio_t[i],ratio_error_t[i])
+                  ratio_t[i],ratio_error_t[i], rate_t[i], rate_error_t[i])
 
         ax.set_xlabel("Energy Threshold [eV]")
         ax.set_ylabel("Ratio []")
