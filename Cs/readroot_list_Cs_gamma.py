856366824bfed648b22ff98a970a49656c64daa8
RunzZhang <52052572+RunzZhang@users.noreply.github.com>
2026-06-23 11:31:48 -0700

    Update readroot_list_Cs_gamma.py

--- Cs/readroot_list_Cs_gamma.py
+++ Cs/readroot_list_Cs_gamma.py
@@ -317,7 +317,7 @@ class ReadRoot():
 
 
         ## test Xenon doping
-        print(self.df['name'].unique())
+        print("particle name",self.df['name'].unique())
         self.gamma_Scint = self.df[
             ((self.df['Volume'] == 'LAr_phys')|(self.df['Volume'] == 'hydraulic_fluid_phys')) &( (self.df['Process'] == "compt")|(self.df['Process'] == "phot"))& (self.df['Parent ID'] == 0)]
         # record the positions and multiplicity
