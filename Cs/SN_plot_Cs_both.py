b802cd8e51e9cf8026a8e511d17e72d114a62dca
RunzZhang <52052572+RunzZhang@users.noreply.github.com>
2026-06-24 14:15:54 -0700

    Update SN_plot_Cs_both.py

--- Cs/SN_plot_Cs_both.py
+++ Cs/SN_plot_Cs_both.py
@@ -35,10 +35,10 @@ class SN():
 
 
         #982 statics false 1
-        for i in range(1,51):
+        # for i in range(1,51):
         # for i in range(1, 11):
-            self.main_body(i)
-        # self.main_body(1)
+        #     self.main_body(i)
+        self.main_body(1)
         self.combine_df()
         self.data_analysis()
 
