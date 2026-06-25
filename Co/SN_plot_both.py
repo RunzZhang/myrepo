ebc49c95c6faf0767ee189766c967a86a750a87b
RunzZhang <52052572+RunzZhang@users.noreply.github.com>
2026-06-25 13:58:06 -0700

    Update SN_plot_both.py

--- Co/SN_plot_both.py
+++ Co/SN_plot_both.py
@@ -215,7 +215,7 @@ class SN():
 
         ER_Ar = self.merged_df[self.merged_df["Volume"] == "LAr_phys"]["PreKinetic/MeV"] * 1000
         # the first peak
-        print(self.merged_df[(self.merged_df["Volume"] == "LAr_phys")&(self.merged_df["PreKinetic/MeV"].between(0.003,0.0035))]["PreKinetic/MeV"] * 1000)
+        print(self.merged_df[(self.merged_df["Volume"] == "LAr_phys")&(self.merged_df["PreKinetic/MeV"].between(0.003,0.0035))])
         fig, ax = plt.subplots(1, 3, figsize=(14, 4))
         # ax[0].hist(ER_Ar, bins=60,range= (0,600),align="left")
         array = ax[0].hist(ER_Ar, bins=1400, range=(0, 700), align="left")
