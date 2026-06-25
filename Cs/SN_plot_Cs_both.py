bd21e0a6d1fe744d58793a2edf792fcac5e84a38
RunzZhang <52052572+RunzZhang@users.noreply.github.com>
2026-06-25 10:22:20 -0700

    Update SN_plot_Cs_both.py

--- Cs/SN_plot_Cs_both.py
+++ Cs/SN_plot_Cs_both.py
@@ -254,6 +254,7 @@ class SN():
         #bins=12000, range=(0, 1200))
         ax[0].set_xlabel("photo absorption of Xe[keV] ")
         ax[0].set_ylabel("Counts")
+        ax[0].set_title("Cs source")
 
 
         plt.savefig(self.plot_path + "Cs_1E8_ER_all_doped.pdf")
