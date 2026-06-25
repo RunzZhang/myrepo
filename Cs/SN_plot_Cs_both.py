ed2ce4872f7e7f1fd04e52e27055446a926e4af6
RunzZhang <52052572+RunzZhang@users.noreply.github.com>
2026-06-25 14:00:59 -0700

    Update SN_plot_Cs_both.py

--- Cs/SN_plot_Cs_both.py
+++ Cs/SN_plot_Cs_both.py
@@ -245,6 +245,8 @@ class SN():
         # per energy deposition
         # the sum is per event
         ER_Ar = self.merged_df[self.merged_df["Volume"]=="LAr_phys"]["PreKinetic/MeV"]*1000
+        print(self.merged_df[(self.merged_df["Volume"] == "LAr_phys") & (
+            self.merged_df["PreKinetic/MeV"].between(0.0025, 0.0040))])
 
         fig, ax = plt.subplots(1,3, figsize=(14, 4))
         # ax[0].hist(ER_Ar, bins=60,range= (0,600),align="left")
