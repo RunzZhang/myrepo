34ddff1eb72b85c4d3db56ac65889a8492af1d3c
RunzZhang <52052572+RunzZhang@users.noreply.github.com>
2026-06-26 19:44:26 -0700

    Update SN_plot_Cs_both.py

--- Cs/SN_plot_Cs_both.py
+++ Cs/SN_plot_Cs_both.py
@@ -251,8 +251,9 @@ class SN():
         fig, ax = plt.subplots(1,3, figsize=(14, 4))
         # ax[0].hist(ER_Ar, bins=60,range= (0,600),align="left")
         array = ax[0].hist(ER_Ar, bins=1200, range=(0, 600), align="left")
-        print("counts",array[0][:20])
-        print("bins", array[1][:20])
+        for i in range(len(array[0])):
+            if array[0][i]>100:
+                print("energy bin keV",array[1][i],array[1][i+1])
         #bins=12000, range=(0, 1200))
         ax[0].set_xlabel("photo absorption of Xe[keV] ")
         ax[0].set_ylabel("Counts")
