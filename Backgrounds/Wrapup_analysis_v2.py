ef8dec0d90808978ff98b31b7e9b754c4c41a10e
RunzZhang <52052572+RunzZhang@users.noreply.github.com>
2026-06-30 17:10:47 -0700

    Update Wrapup_analysis_v2.py

--- Backgrounds/Wrapup_analysis_v2.py
+++ Backgrounds/Wrapup_analysis_v2.py
@@ -219,8 +219,8 @@ class integrated_analysis():
             exposure_df = exposure_df.iloc[:, :7]
             exposure_df.columns = ['Pressure [bara]', 'Lifetime [s]', 'Lifetime Error [s]','Exponential Fit 2xNLL','N.d.o.f.','Time Cut High [s]','Time Cut Low [s]']
             exposure_df = exposure_df[(exposure_df['Lifetime [s]'] <=6.92e-1) | (exposure_df['Lifetime [s]'] >= 6.94e-1)]
-            # exposure_df = exposure_df[
-            #     (exposure_df['Lifetime Error [s]']/exposure_df['Lifetime [s]'] <= 0.3)]
+            exposure_df = exposure_df[
+                (exposure_df['Lifetime Error [s]']/exposure_df['Lifetime [s]'] <= 0.5)]
             # add rate column
             exposure_df['Exp Rate [mHz]']= 1000/exposure_df['Lifetime [s]']
             exposure_df['Exp Rate Sigma [mHz]'] = exposure_df['Lifetime Error [s]'] * 1000 / (exposure_df['Lifetime [s]']) ** 2
@@ -234,8 +234,8 @@ class integrated_analysis():
             exposure_df = exposure_df.iloc[:, :7]
             exposure_df.columns = ['Pressure [bara]', 'Lifetime [s]', 'Lifetime Error [s]','Exponential Fit 2xNLL','N.d.o.f.','Time Cut High [s]','Time Cut Low [s]']
             exposure_df = exposure_df[(exposure_df['Lifetime [s]'] <=6.92e-1) | (exposure_df['Lifetime [s]'] >= 6.94e-1)]
-            # exposure_df = exposure_df[
-            #     (exposure_df['Lifetime Error [s]']/exposure_df['Lifetime [s]'] <= 0.3)]
+            exposure_df = exposure_df[
+                (exposure_df['Lifetime Error [s]']/exposure_df['Lifetime [s]'] <= 0.5)]
             exposure_df['Exp Rate [mHz]'] = 1000 / exposure_df['Lifetime [s]']
             exposure_df['Exp Rate Sigma [mHz]'] = exposure_df['Lifetime Error [s]'] * 1000 / (
             exposure_df['Lifetime [s]']) ** 2
