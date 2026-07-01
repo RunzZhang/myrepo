7d206b707cb8709ff7323f5949a6d90da2a8fb46
RunzZhang <52052572+RunzZhang@users.noreply.github.com>
2026-06-30 18:03:59 -0700

    Update Wrapup_analysis_v2.py

--- Backgrounds/Wrapup_analysis_v2.py
+++ Backgrounds/Wrapup_analysis_v2.py
@@ -1605,7 +1605,7 @@ class integrated_analysis():
                       color="black")
 
         ax[0].set_xlabel(r"$Q_{Seitz} r_l^{-1} \rho_l^{-1}$ [GeV cm$^2$ g$^{-1}$]")
-        ax[0].set_ylabel("Nucleation probability (per xenon photoabsorption) ")
+        ax[0].set_ylabel("Nucleation probability (per xenon K shell photoabsorption) ")
         ax[0].set_yscale("log")
         ax[0].legend(loc='upper right', fontsize=14)
 
