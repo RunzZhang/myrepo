98418cefc4eae6de03b1abe49ada2a9b3a608f61
RunzZhang <52052572+RunzZhang@users.noreply.github.com>
2026-07-01 20:56:11 -0700

    Update Wrapup_analysis_v2.py

--- Backgrounds/Wrapup_analysis_v2.py
+++ Backgrounds/Wrapup_analysis_v2.py
@@ -1817,7 +1817,6 @@ class integrated_analysis():
             df = df[df['Clean Rate [mHz]'] > 0]
             if i <= 3:
                 self.df_Cs_116_plot_list.append(df)
-                print("df",df.columns)
             else:
                 self.df_Cs_119_plot_list.append(df)
 
@@ -1829,7 +1828,6 @@ class integrated_analysis():
             self.Cs_fitting_list.append(df_fit)
         self.df_Cs_116_plot = pd.concat(self.df_Cs_116_plot_list, ignore_index=True)
         self.df_Cs_119_plot = pd.concat(self.df_Cs_119_plot_list, ignore_index=True)
-        print("self.df_Cs_116_plot",self.df_Cs_116_plot.columns)
 
         # make Cs 116 show just as one series
         self.df_Cs_116_plot = self.concat_PT_condition(self.df_Cs_116_plot)
@@ -2001,7 +1999,7 @@ class integrated_analysis():
         
     def concat_PT_condition(self, df):
 
-        df_combined = df.groupby(['Pressure [bara]', 'Seitz [keV]', 'Eion_rl-1_rhol-1 [GeVcm**2 g-1]', 'Q_rl-1_rhol-1 [GeVcm**2 g-1]'], as_index=False).agg({
+        df_combined = df.groupby(['Pressure [bara]', 'Seitz [keV]', 'Eion_rl-1_rhol-1 [GeVcm**2 g-1]', 'Q_rl-1_rhol-1 [GeVcm**2 g-1]' , 'Eion [keV]'], as_index=False).agg({
             "Clean Rate [mHz]": 'mean',
             'Clean Rate Sigma [mHz]': lambda x: np.sqrt(np.sum(x ** 2)),
             "Rejection Rate Scattering[]": 'mean',
