ee62ba3baa196478107abbd97e9db51a240eab93
RunzZhang <52052572+RunzZhang@users.noreply.github.com>
2026-07-01 21:04:06 -0700

    Update Wrapup_analysis_v2.py

--- Backgrounds/Wrapup_analysis_v2.py
+++ Backgrounds/Wrapup_analysis_v2.py
@@ -2317,8 +2317,8 @@ class integrated_analysis():
         result=np.zeros((4,3))
         for i in range(4):
             for j in range(3):
-                x = dataframe[[x_cfg[j]["x"]]].values
-                y = dataframe[[y_cfg[i]["y"]]].values
+                x = dataframe[[x_cfg[j]["x"]]].values.flatten()
+                y = dataframe[[y_cfg[i]["y"]]].values.flatten()
                 print("shape",type(x),x.shape)
                 # dealing with guess
                 x_min = min(x)
