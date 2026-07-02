841a44cc2b1416b8920deac3bd64150ba5e3f060
RunzZhang <52052572+RunzZhang@users.noreply.github.com>
2026-07-01 21:26:09 -0700

    Update Wrapup_analysis_v2.py

--- Backgrounds/Wrapup_analysis_v2.py
+++ Backgrounds/Wrapup_analysis_v2.py
@@ -468,7 +468,7 @@ class integrated_analysis():
             # clean rate!
             merged_df['Clean Rate [mHz]'] = merged_df['Exp Rate [mHz]'] - merged_df['Bkg Rate [mHz]']
             merged_df['Clean Rate Sigma [mHz]'] = np.sqrt(merged_df['Exp Rate Sigma [mHz]']**2 + merged_df['Bkg Rate Sigma [mHz]']**2)
-            print("Ba1", exposure_df)
+            print("Ba1", merged_df)
             # add Seitz and Eion unit
             merged_df = pd.merge(merged_df, self.df_energy_116_tab, on='Pressure [bara]', how="inner")
             # add sims analysis to get rejection
