545d41c0cc3f5955fe27110002952750a44860ad
RunzZhang <52052572+RunzZhang@users.noreply.github.com>
2026-07-02 10:47:32 -0700

    replace Barium data with 95 upper limit

--- Backgrounds/Wrapup_analysis_v2.py
+++ Backgrounds/Wrapup_analysis_v2.py
@@ -6,6 +6,7 @@ import os
 import pickle
 from scipy.optimize import curve_fit
 import math
+from scipy.stats import norm
 class integrated_analysis():
     def __init__(self):
 
@@ -460,15 +461,20 @@ class integrated_analysis():
             merged_df.to_csv(self.Co_exp_rate_path[i], index=False)
 
         for i in range(0,self.Ba_exp_116_raw_len):
-            print("Cs 116K", self.Ba_exp_rate_path[i])
+            print("Ba 116K", self.Ba_exp_rate_path[i])
             exposure_df = pd.read_csv(self.Ba_exp_sorted_path[i])
 
             # merge both has the pressure value, on pressure
             merged_df = pd.merge(self.df_bkg_116,exposure_df,on='Pressure [bara]', how="inner")
-            # clean rate!
-            merged_df['Clean Rate [mHz]'] = merged_df['Exp Rate [mHz]'] - merged_df['Bkg Rate [mHz]']
+            # clean rate! Upper limit for Barium
+            merged_df['Raw Clean Rate [mHz]'] = merged_df['Exp Rate [mHz]'] - merged_df['Bkg Rate [mHz]']
             merged_df['Clean Rate Sigma [mHz]'] = np.sqrt(merged_df['Exp Rate Sigma [mHz]']**2 + merged_df['Bkg Rate Sigma [mHz]']**2)
 
+            S = merged_df['Raw Clean Rate [mHz]']
+            E = merged_df['Clean Rate Sigma [mHz]']
+            inside_ppf = 1.0 - 0.05 * norm.cdf(S / E)
+            merged_df['Clean Rate [mHz]'] = S + E * norm.ppf(inside_ppf)
+            merged_df.drop(columns=['Raw Clean Rate [mHz]'])
             # add Seitz and Eion unit
             merged_df = pd.merge(merged_df, self.df_energy_116_tab, on='Pressure [bara]', how="inner")
             # add sims analysis to get rejection
@@ -1883,7 +1889,7 @@ class integrated_analysis():
             # pressure_drop_list = [2.75,3.25,3.75]
             pressure_drop_list = []
             df = df[~df['Pressure [bara]'].isin(pressure_drop_list)]
-            df = df[df['Clean Rate [mHz]'] > 0]
+            # df = df[df['Clean Rate [mHz]'] > 0]
 
             df_fit = df[['Seitz [keV]', "Rejection Rate Scattering[]", 'Eion_rl-1_rhol-1 [GeVcm**2 g-1]',
                          "Rejection Rate KeV[/keV]", 'Q_rl-1_rhol-1 [GeVcm**2 g-1]', "Rejection Rate Xenon Abs[]",
@@ -1930,8 +1936,10 @@ class integrated_analysis():
                            yerr=self.df_Co_116_plot[y_cfg["y_err"]], label="Co 116K", fmt='o')
                 ax_ij.errorbar(self.df_Co_119_plot[x_cfg["x"]], self.df_Co_119_plot[y_cfg["y"]],
                            yerr=self.df_Co_119_plot[y_cfg["y_err"]], label="Co 119K", fmt='o')
-                ax_ij.errorbar(self.df_Ba_116_plot[x_cfg["x"]], self.df_Ba_116_plot[y_cfg["y"]],
-                           yerr=self.df_Ba_116_plot[y_cfg["y_err"]], label="Ba 116K", fmt='o')
+                # ax_ij.errorbar(self.df_Ba_116_plot[x_cfg["x"]], self.df_Ba_116_plot[y_cfg["y"]],
+                #            yerr=self.df_Ba_116_plot[y_cfg["y_err"]], label="Ba 116K", fmt='o')
+                ax_ij.plot(self.df_Ba_116_plot[x_cfg["x"]], self.df_Ba_116_plot[y_cfg["y"]],
+                               label="Ba 116K", marker='v')
 
                 ax_ij.set_xlabel(x_cfg["xlabel"])
                 ax_ij.set_ylabel(y_cfg["ylabel"])
