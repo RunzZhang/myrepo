6067bc184499de1bf9cc2ce6c5aca00df995d12a
RunzZhang <52052572+RunzZhang@users.noreply.github.com>
2026-07-02 11:56:56 -0700

    Update SN_plot_both.py

--- Ba/SN_plot_both.py
+++ Ba/SN_plot_both.py
@@ -35,15 +35,15 @@ class SN():
 
 
         #982 statics false 1
-        for i in range(1,54):
-        # for i in range(1, 26):
-        # for i in range(26, 54):
-            try:
-        # for i in range(1, 11):
-                self.main_body(i)
-            except:
-                continue
-        # self.main_body(1)
+        # for i in range(1,54):
+        # # for i in range(1, 26):
+        # # for i in range(26, 54):
+        #     try:
+        # # for i in range(1, 11):
+        #         self.main_body(i)
+        #     except:
+        #         continue
+        self.main_body(1)
         self.combine_df()
         self.data_analysis()
 
