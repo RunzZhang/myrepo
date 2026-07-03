9e8274a0e9156e0d33abf2453c32129f5d3f5303
RunzZhang <52052572+RunzZhang@users.noreply.github.com>
2026-07-03 09:12:04 -0700

    Update Wrapup_analysis_v2.py

--- Backgrounds/Wrapup_analysis_v2.py
+++ Backgrounds/Wrapup_analysis_v2.py
@@ -2057,6 +2057,7 @@ class integrated_analysis():
         self.Ba_doped_energy_edges = self.Ba_sims_doped[1][0][1]
         self.Ba_doped_counts_cum_bin = self.Ba_sims_doped[2]
         self.Ba_doped_counts_energy_cum_bin = self.Ba_sims_doped[3]
+        print("Ba, ", self.Ba_counts_cum_bin[0], "counts," , self.Co_doped_counts_energy_cum_bin[0], "keV" )
 
 
 
