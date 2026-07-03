fb817606fc12bb6475d11c03378601c0c0387c79
RunzZhang <52052572+RunzZhang@users.noreply.github.com>
2026-07-02 19:22:56 -0700

    Update SN_plot_both.py

--- Ba/SN_plot_both.py
+++ Ba/SN_plot_both.py
@@ -233,7 +233,7 @@ class SN():
         ax[0].set_xlabel("photo absorption of Xe[keV] ")
         ax[0].set_ylabel("Counts")
         # ax[0].set_title("Co source - 1332 keV gamma")
-        ax[0].set_title("Co source - 1173 keV gamma")
+        ax[0].set_title("Ba source")
 
 
         plt.savefig(self.plot_path + "Ba_1E8_ER_all_doped.pdf")
