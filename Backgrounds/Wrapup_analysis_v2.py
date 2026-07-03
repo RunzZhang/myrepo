b197f11abca3da230356324bdd2692f038c67555
RunzZhang <52052572+RunzZhang@users.noreply.github.com>
2026-07-03 10:02:01 -0700

    Update Wrapup_analysis_v2.py

--- Backgrounds/Wrapup_analysis_v2.py
+++ Backgrounds/Wrapup_analysis_v2.py
@@ -314,7 +314,7 @@ class integrated_analysis():
             exposure_df['Exp Rate Sigma [mHz]'] = exposure_df['Lifetime Error [s]'] * 1000 / (exposure_df['Lifetime [s]']) ** 2
             exposure_df = exposure_df.drop(columns=['Exponential Fit 2xNLL',
                                                     'N.d.o.f.', 'Time Cut High [s]', 'Time Cut Low [s]'])
-            print("Ba Rate", exposure_df)
+
 
             exposure_df.to_csv(self.Ba_exp_sorted_path[i],index=False)
     def read_raw_backgrounds_exp(self):
@@ -1891,6 +1891,7 @@ class integrated_analysis():
             pressure_drop_list = []
             df = df[~df['Pressure [bara]'].isin(pressure_drop_list)]
             df = df[df['Clean Rate [mHz]'] > 0]
+            print("ba" ,df)
 
             df_fit = df[['Seitz [keV]', "Rejection Rate Scattering[]", 'Eion_rl-1_rhol-1 [GeVcm**2 g-1]',
                          "Rejection Rate KeV[/keV]", 'Q_rl-1_rhol-1 [GeVcm**2 g-1]', "Rejection Rate Xenon Abs[]",
