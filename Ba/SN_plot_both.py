8bdf49f55a35da997275b654118a6045b5752ab3
RunzZhang <52052572+RunzZhang@users.noreply.github.com>
2026-07-03 21:24:44 -0700

    Update SN_plot_both.py

--- Ba/SN_plot_both.py
+++ Ba/SN_plot_both.py
@@ -321,7 +321,7 @@ class SN():
         # MHz
         Rate_factor = self.gamma_rate*1000 / (self.G4_events_gamma)
         ER_Ar = self.merged_df[self.merged_df["Volume"]=="LAr_phys"]["ER_near/eV"]/1000
-        print("Ba level",self.merged_df[(self.merged_df["Volume"]=="LAr_phys")&(self.merged_df["ER_near/eV"]>360*1000)])
+        print("Ba level",self.merged_df[(self.merged_df["Volume"]=="LAr_phys")&(self.merged_df["ER_near/eV"]>350*1000)])
         ER_CF4 = self.merged_df[self.merged_df["Volume"] == "hydraulic_fluid_phys"]["ER_near/eV"] / 1000
         ER_CF4 = ER_Ar # only test argon
         print("ER CF4 counts", len(ER_CF4))
