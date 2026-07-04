66328aa8bfd15884f6808bfaf678caaae37f29b5
RunzZhang <52052572+RunzZhang@users.noreply.github.com>
2026-07-04 10:17:44 -0700

    1e6

--- Ba/SN_plot_both.py
+++ Ba/SN_plot_both.py
@@ -18,8 +18,8 @@ class SN():
         # self.base_path2 = "/data/runzezhang/result/TN_sims_D/chunked_root_files_Ba_1E5_ar_inside/"  # for gamma path
 
 
-        self.base_path = "/data/runzezhang/result/TN_sims_D/chunked_root_files_Ba_1E5_CF4_inside/"
-        self.base_path2 = "/data/runzezhang/result/TN_sims_D/chunked_root_files_Ba_1E5_CF4_inside/"
+        self.base_path = "/data/runzezhang/result/TN_sims_D/chunked_root_files_Ba_1E6_CF4_inside/"
+        self.base_path2 = "/data/runzezhang/result/TN_sims_D/chunked_root_files_Ba_1E6_CF4_inside/"
 
         # self.base_path = "/data/runzezhang/result/TN_sims_D/chunked_root_files_Ba_5E6/"
         # self.base_path2 = "/data/runzezhang/result/TN_sims_D/chunked_root_files_Ba_5E6/"  # for gamma path
