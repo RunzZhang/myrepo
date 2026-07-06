e1aec9e186b6f095cee04559e3e701115a5f5078
RunzZhang <52052572+RunzZhang@users.noreply.github.com>
2026-07-06 10:32:32 -0700

    Update Wrapup_analysis_v2.py

--- Backgrounds/Wrapup_analysis_v2.py
+++ Backgrounds/Wrapup_analysis_v2.py
@@ -2179,7 +2179,7 @@ class integrated_analysis():
                 # Plot line with markers and error bars
                 ax.errorbar(group_sorted['Run_Date'], group_sorted[y_column], yerr=group_sorted[y_error],
                             marker='o', linestyle='-', linewidth=2, elinewidth=1.5, capsize=4,
-                            label=r"$Q_{Seitz}  $"+f'{energy:.2f} keV')
+                            label=r"$Q_{Seitz}$ "+f'{energy:.2f} keV')
 
             # --- 3. Format the X-axis time presentation ---
             # Formats the dates on screen as MM/DD/YYYY
