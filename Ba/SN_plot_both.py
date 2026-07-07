140aba773a54c74b634898efdf6d699ff162cbf4
RunzZhang <52052572+RunzZhang@users.noreply.github.com>
2026-07-07 15:11:11 -0700

    update

--- Ba/SN_plot_both.py
+++ Ba/SN_plot_both.py
@@ -11,7 +11,7 @@ class SN():
         # at last change the self.name and plot_name in plot function
         # v2: change back to 2 backgrounds but with finer definitions
         # v4 kill duplicated NRERs
-        self.doped_path = "/data/runzezhang/result/TN_sims_D/chunked_root_files_Ba_1E8_100ppm/"
+        self.doped_path = "/data/runzezhang/result/TN_sims_D/chunked_root_files_Ba_ar_1E6_phot/"
 
         # self.base_path = "/data/runzezhang/result/TN_sims_D/chunked_root_files_Ba_1E5_ar_inside/"
         # self.base_path2 = "/data/runzezhang/result/TN_sims_D/chunked_root_files_Ba_1E5_ar_inside/"  # for gamma path
--- Ba/readroot_list_gamma.py
+++ Ba/readroot_list_gamma.py
@@ -137,8 +137,8 @@ class ReadRoot():
         # self.base_path = "/data/runzezhang/result/TN_sims_D/chunked_root_files_Co_5E6/"
         # self.base_path2 = "/data/runzezhang/result/TN_sims_D/chunked_root_files_Co_5E6/"
 
-        self.base_path = "/data/runzezhang/result/TN_sims_D/chunked_root_files_Ba_ar_1E6_phot/"
-        self.base_path2 = "/data/runzezhang/result/TN_sims_D/chunked_root_files_Ba_ar_1E6_phot/"
+        self.base_path = "/data/runzezhang/result/TN_sims_D/chunked_root_files_Ba_5E6/"
+        self.base_path2 = "/data/runzezhang/result/TN_sims_D/chunked_root_files_Ba_5E6/"
 
         self.plot_path = '/data/runzezhang/result/TN_sims_D/plot/'
 
@@ -225,15 +225,15 @@ class ReadRoot():
         self.modify_df()
 
         # find all ER and save ER into csv
-        # self.allER()
-        # self.ER_distribution_primary()
-        # self.ER_distribution_counts()
+        self.allER()
+        self.ER_distribution_primary()
+        self.ER_distribution_counts()
 
         # find all NR
         # self.allNR()
 
         #xenon doping
-        self.xenon_doped_phot()
+        # self.xenon_doped_phot()
 
 
 
