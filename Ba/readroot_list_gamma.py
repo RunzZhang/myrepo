0b6d2a379ca1e48a7b41817bf13adfada4eb552f
RunzZhang <52052572+RunzZhang@users.noreply.github.com>
2026-07-07 19:05:11 -0700

    Update readroot_list_gamma.py

--- Ba/readroot_list_gamma.py
+++ Ba/readroot_list_gamma.py
@@ -567,9 +567,9 @@ class ReadRoot():
 
         track_groups = self.df.groupby(['Event', 'Track ID'])
 
-        self.df['X_next/mm'] = track_groups['X/mm'].shift(-1)
-        self.df['Y_next/mm'] = track_groups['Y/mm'].shift(-1)
-        self.df['Z_next/mm'] = track_groups['Z/mm'].shift(-1)
+        self.df['X_next/mm'] = track_groups['X/mm'].shift(1)
+        self.df['Y_next/mm'] = track_groups['Y/mm'].shift(1)
+        self.df['Z_next/mm'] = track_groups['Z/mm'].shift(1)
 
         # If a process terminates the track (like 'phot'), there is no "next" step.
         # Fall back to the current coordinates for those terminal steps.
