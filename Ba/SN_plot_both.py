8f6b317b5f7f33b38b87d975f06b087d3fe2f0e6
RunzZhang <52052572+RunzZhang@users.noreply.github.com>
2026-07-08 16:54:00 -0700

    Update SN_plot_both.py

--- Ba/SN_plot_both.py
+++ Ba/SN_plot_both.py
@@ -348,6 +348,11 @@ class SN():
                 (self.merged_df_all["Volume"] == "LAr_phys") & (self.merged_df_all["Process"] == "compt")][
                 "ER_near/eV"] / 1000
 
+        ER_sum = \
+            self.merged_df_all[
+                (self.merged_df_all["Volume"] == "LAr_phys") ][
+                "ER_near/eV"] / 1000
+
 
 
         # estimate gamma rejction level
