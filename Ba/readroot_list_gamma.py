eebc976c2c394d7515e228a604524c0efc90fd29
RunzZhang <52052572+RunzZhang@users.noreply.github.com>
2026-07-07 17:05:09 -0700

    add phot process of special phot module

--- Ba/readroot_list_gamma.py
+++ Ba/readroot_list_gamma.py
@@ -134,8 +134,8 @@ class RestructureRoot():
 
 class ReadRoot():
     def __init__(self):
-        # self.base_path = "/data/runzezhang/result/TN_sims_D/chunked_root_files_Ba_ar_1E6_phot/"
-        # self.base_path2 = "/data/runzezhang/result/TN_sims_D/chunked_root_files_Ba_ar_1E6_phot/"
+        # self.base_path = "/data/runzezhang/result/TN_sims_D/chunked_root_files_dmx_Ba_1E6_phot_track/"
+        # self.base_path2 = "/data/runzezhang/result/TN_sims_D/chunked_root_files_dmx_Ba_1E6_phot_track/"
 
         self.base_path = "/data/runzezhang/result/TN_sims_D/chunked_root_files_Ba_1E6_normal/"
         self.base_path2 = "/data/runzezhang/result/TN_sims_D/chunked_root_files_Ba_1E6_normal/"
