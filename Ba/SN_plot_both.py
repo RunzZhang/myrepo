1ceb88997a5d008fb4aa441c1912246521b571ce
RunzZhang <52052572+RunzZhang@users.noreply.github.com>
2026-07-08 16:38:07 -0700

    Update SN_plot_both.py

--- Ba/SN_plot_both.py
+++ Ba/SN_plot_both.py
@@ -362,7 +362,7 @@ class SN():
         # ax[0].bar(hist_array[0][1][:-1], Rate_factor * hist_array[0][0], width=np.diff(hist_array[0][1]),
         #           align="edge",
         #           edgecolor="black")
-        hist_array[0][0].insert(0, 0)
+        hist_array[0][0]= np.insert(hist_array[0][0],0, 0)
         ax[0].plot(hist_array[0][1], Rate_factor * hist_array[0][0])
         bin0_len = int(hist_array[0][1][1] - hist_array[0][1][0])
         ax[0].set_xlabel("ER/keV per photo in LAr")
@@ -376,7 +376,7 @@ class SN():
         # ax[1].bar(hist_array[1][1][:-1], Rate_factor * hist_array[1][0], width=np.diff(hist_array[1][1]),
         #           align="edge",
         #           edgecolor="black")
-        hist_array[1][0].insert(0, 0)
+        hist_array[1][0]= np.insert(hist_array[1][0],0, 0)
         ax[1].plot(hist_array[1][1], Rate_factor * hist_array[1][0])
         ax[1].set_xlabel("ER/keV per photo in LAr")
         bin1_len = int(hist_array[1][1][1] - hist_array[1][1][0])
@@ -391,7 +391,7 @@ class SN():
         # ax[2].bar(hist_array[2][1][:-1], Rate_factor * hist_array[2][0], width=np.diff(hist_array[2][1]),
         #           align="edge",
         #           edgecolor="black")
-        hist_array[2][0].insert(0,0)
+        hist_array[2][0]= np.insert(hist_array[2][0],0, 0)
         ax[2].plot(hist_array[2][1], Rate_factor * hist_array[2][0])
         ax[2].set_xlabel("ER/keV per deposition ")
         bin2_len = int(hist_array[2][1][1] - hist_array[2][1][0])
