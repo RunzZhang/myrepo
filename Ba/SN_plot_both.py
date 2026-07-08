71d8d74c71598340d49f18765739d15e616a5d7f
RunzZhang <52052572+RunzZhang@users.noreply.github.com>
2026-07-08 16:59:05 -0700

    Update SN_plot_both.py

--- Ba/SN_plot_both.py
+++ Ba/SN_plot_both.py
@@ -350,10 +350,10 @@ class SN():
                 (self.merged_df_all["Volume"] == "LAr_phys") & (self.merged_df_all["Process"] == "compt")][
                 "ER_near/eV"] / 1000
 
-        ER_sum = \
-            self.merged_df_all[
-                (self.merged_df_all["Volume"] == "LAr_phys") ][
-                "ER_near/eV"] / 1000
+        # ER_sum = \
+        #     self.merged_df_all[
+        #         (self.merged_df_all["Volume"] == "LAr_phys") ][
+        #         "ER_near/eV"] / 1000
 
 
 
