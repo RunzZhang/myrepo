54e08256e040415f524a3648fd368fef948d1fb7
RunzZhang <52052572+RunzZhang@users.noreply.github.com>
2026-07-08 17:37:28 -0700

    write value easy to edit

--- Ba/readroot_list_gamma.py
+++ Ba/readroot_list_gamma.py
@@ -656,12 +656,13 @@ class ReadRoot():
 
         self.output_df.to_csv(self.info_primary_path, index=False)
 
-        self.df[self.df["Event"] == 6391].to_csv(self.base_path + "LAr_ER_6391.csv")
         # self.df[(self.df["Event"].isin(self.electron_recoiled_event_list))].to_csv(self.base_path+"LAr_ER_sample_preprocess_laststep_v2.csv")
 
         self.output_df.to_csv(self.info_all_path, index=False)
-
-        self.df[self.df["Event"] == 2930].to_csv(self.base_path + "LAr_ER_2930.csv")
+        file2930 = self.df[self.df["Event"] == 2930]
+        columns_to_round = ["X/mm","Y/mm","Z/mm","PreKinetic/MeV","PostKinetic/MeV","Recoiled/MeV"]
+        file2930[columns_to_round] = file2930[columns_to_round].round(1)
+        file2930.to_csv(self.base_path + "LAr_ER_2930.csv")
         # self.df[(self.df["Event"].isin(self.electron_recoiled_event_list))].to_csv(self.base_path+"LAr_ER_sample_preprocess_laststep_v2.csv")
 
     def delta_e_distribution(self):
