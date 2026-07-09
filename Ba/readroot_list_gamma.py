cdc41e3a4ea60f2b8d2bf0d71f720d9ba0a63f1b
RunzZhang <52052572+RunzZhang@users.noreply.github.com>
2026-07-08 17:39:42 -0700

    Update readroot_list_gamma.py

--- Ba/readroot_list_gamma.py
+++ Ba/readroot_list_gamma.py
@@ -660,8 +660,10 @@ class ReadRoot():
 
         self.output_df.to_csv(self.info_all_path, index=False)
         file2930 = self.df[self.df["Event"] == 2930]
-        columns_to_round = ["X/mm","Y/mm","Z/mm","PreKinetic/MeV","PostKinetic/MeV","Recoiled/MeV"]
-        file2930[columns_to_round] = file2930[columns_to_round].round(1)
+        columns_to_round1 = ["X/mm","Y/mm","Z/mm",]
+        columns_to_round6 = ["PreKinetic/MeV", "PostKinetic/MeV", "Recoiled/MeV"]
+        file2930[columns_to_round1] = file2930[columns_to_round1].round(1)
+        file2930[columns_to_round6] = file2930[columns_to_round6].round(6)
         file2930.to_csv(self.base_path + "LAr_ER_2930.csv")
         # self.df[(self.df["Event"].isin(self.electron_recoiled_event_list))].to_csv(self.base_path+"LAr_ER_sample_preprocess_laststep_v2.csv")
 
