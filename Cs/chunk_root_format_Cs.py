86b772c41e4cb161aa42bf7c269a0e395a090ee2
RunzZhang <52052572+RunzZhang@users.noreply.github.com>
2026-07-11 16:29:03 -0700

    Update chunk_root_format_Cs.py

--- Cs/chunk_root_format_Cs.py
+++ Cs/chunk_root_format_Cs.py
@@ -26,7 +26,7 @@ class ReadRoot:
         # self.signal_path_mid = self.base_path + self.signal_mid
         # self.signal_path = self.base_path + self.signal
 
-        self.filepath = self.base_path + " dmx_Cs_5E6.root"
+        self.filepath = self.base_path + "dmx_Cs_5E6.root"
         # self.filepath = self.base_path + "dmx_AmLi.root" # test
         self.tree_name = "tree"  # Assuming your TTree is named "tree"
 
