389eda0ef58622ff2363a33455bfc62b8f38fe4f
RunzZhang <52052572+RunzZhang@users.noreply.github.com>
2026-07-12 14:59:18 -0700

    Update SN_plot_both.py

--- Co/SN_plot_both.py
+++ Co/SN_plot_both.py
@@ -26,8 +26,8 @@ class SN():
         # self.base_path = "/data/runzezhang/result/GR_sims/chunked_root_files_Ba_1E6_normal/"
         # self.base_path2 = "/data/runzezhang/result/GR_sims/chunked_root_files_Ba_1E6_normal/"
 
-        self.base_path = "/data/runzezhang/result/GR_sims/chunked_root_files_Cs_5E6/"
-        self.base_path2 = "/data/runzezhang/result/GR_sims/chunked_root_files_Cs_5E6/"  # for gamma path
+        self.base_path = "/data/runzezhang/result/GR_sims/chunked_root_files_Co_5E6/"
+        self.base_path2 = "/data/runzezhang/result/GR_sims/chunked_root_files_Co_5E6/"  # for gamma path
 
         self.plot_path = '/data/runzezhang/result/GR_sims/plot/'
         self.false_1 = "PN_false1.csv"
