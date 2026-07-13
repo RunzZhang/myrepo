80320d114d3ba5248e7f7abba31bba4b1284c7af
RunzZhang <52052572+RunzZhang@users.noreply.github.com>
2026-07-13 15:22:39 -0700

    Update chunk_root_format.py

--- Ba/chunk_root_format.py
+++ Ba/chunk_root_format.py
@@ -41,7 +41,7 @@ class ReadRoot:
 
     def chunk_and_write_root(self, start_chunk_cum = 0,num_chunks=20, output_dir=None):
         if output_dir is None:
-            output_dir = os.path.join(self.base_path, "chunked_root_files_Ba_1E6_halfargon")
+            output_dir = os.path.join(self.base_path, "chunked_root_files_Ba_1E5_halfargon")
         os.makedirs(output_dir, exist_ok=True)
 
         with uproot.open(self.filepath) as file:
