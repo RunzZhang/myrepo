92b747eb870cbe734c00f309ee291d44e4ef1e8e
RunzZhang <52052572+RunzZhang@users.noreply.github.com>
2026-07-13 14:55:06 -0700

    Update readroot_list_gamma.py

--- Ba/readroot_list_gamma.py
+++ Ba/readroot_list_gamma.py
@@ -694,11 +694,11 @@ class ReadRoot():
         self.phot = self.mom_gamma[(self.mom_gamma['Process'] == "phot")]
         print("phot",self.phot.head(10))
         import crosssection_calculate
-        self.phot["Target_Post"] = self.phot["PreKinetic/MeV"].apply(
+        self.phot["Target_Post"] = self.phot["Pre_Target"].apply(
     lambda e: crosssection_calculate.calculate_doped_photoelectric_probabilities(e, 0.5, 0.5)["Xe_Interaction_Probability"])
         total_probability_sum = self.phot["Target_Post"].sum()
-        zero_count = len(self.phot[self.phot["Recoiled/MeV"] == 1.0])
-        print("Simulation", self.phot[self.phot["Recoiled/MeV"] == 1.0])
+        zero_count = len(self.phot[self.phot["Pre_Target"] == 1.0])
+        print("Simulation", self.phot[self.phot["Recoiled/MeVPre_Target"] == 1.0])
         print("Post analysis count", total_probability_sum, "Simulation result", zero_count, "total phot number",len(self.phot))
 
         self.output_df = self.mom_gamma[
