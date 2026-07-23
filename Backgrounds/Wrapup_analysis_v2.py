3bc212051fd92acb4052efbda0cc1cc51411dd5c
RunzZhang <52052572+RunzZhang@users.noreply.github.com>
2026-07-22 20:43:29 -0700

    Update Wrapup_analysis_v2.py

--- Backgrounds/Wrapup_analysis_v2.py
+++ Backgrounds/Wrapup_analysis_v2.py
@@ -1956,7 +1956,7 @@ class integrated_analysis():
                 ax_ij.set_xlabel(x_cfg["xlabel"],fontsize=16)
                 ax_ij.set_ylabel(y_cfg["ylabel"],fontsize=16)
                 ax_ij.set_yscale("log")
-                ax_ij.legend(loc='lower left', fontsize=16)
+                ax_ij.legend(loc='lower left', fontsize=13)
 
 
 
@@ -2031,13 +2031,13 @@ class integrated_analysis():
         a_val = result_Q2_xe[0]
         b_val = result_Q2_xe[1]
         label_text = f"A = {a_val:.2e},\nB = {b_val:.2e}"
-        ax[0].plot(result_Q2_xe[2], result_Q2_xe[3], label = label_text,
+        ax[0].plot(result_Q2_xe[2], result_Q2_xe[3],
                    color="black")
 
         ax[0].set_xlabel(r"$Q_{Seitz} r_l^{-1} \rho_l^{-1}$ [GeV cm$^2$ g$^{-1}$]",fontsize=16 )
         ax[0].set_ylabel("Nucleation probability \n (per xenon K shell photoabsorption) ",fontsize=16)
         ax[0].set_yscale("log")
-        ax[0].legend(loc='lower left', fontsize=14)
+        ax[0].legend(loc='lower left', fontsize=13)
 
 
 
