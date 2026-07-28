825815319968a67233a6ffc5e4f5dcc5c01577d4
RunzZhang <52052572+RunzZhang@users.noreply.github.com>
2026-07-28 16:52:43 -0700

    Update Wrapup_analysis_v2.py

--- Backgrounds/Wrapup_analysis_v2.py
+++ Backgrounds/Wrapup_analysis_v2.py
@@ -2422,7 +2422,13 @@ class integrated_analysis():
 
 
         ax.plot(SBC_Q_list, SBC_keV_list, label="SBC Fit", color="tab:blue")
-        secax.plot(Drex_Q_list, Drex_phot_list, label="Drexel Model", linestyle="--", color="tab:orange")
+        ax.plot(
+            Drex_Q_list,
+            Drex_phot_list / SCALE_FACTOR,
+            label="Drexel Model",
+            linestyle="--",
+            color="tab:orange"
+        )
         ax.plot(PICO_Q_list, PICO_keV_list, label="PICO Model", linestyle="--", color="tab:orange")
 
 
