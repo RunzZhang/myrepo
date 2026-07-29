e9167f76970182a5d0cb215aaacb0518ec50954f
RunzZhang <52052572+RunzZhang@users.noreply.github.com>
2026-07-29 16:36:50 -0700

    Update Wrapup_analysis_v2.py

--- Backgrounds/Wrapup_analysis_v2.py
+++ Backgrounds/Wrapup_analysis_v2.py
@@ -2556,7 +2556,7 @@ class integrated_analysis():
         # fitting parameter
 
         box_content0 = (f"$\\mathcal{{P}}_{{phot}} = A_{{phot}} e^{{-B_{{phot}} Q_{{Seitz}}}}$\n"
-                        f"\\mathcal{{A}}_{{phot}} = 0.13 MeV$^{{-1}}$\n"    f"\\mathcal{{B}}_{{phot}} = 8.75 GeV$^{{-1}}$cm$^{{-2}}$g")
+                        f"$\\mathcal{{A}}_{{phot}} = 0.13 MeV$^{{-1}}$\n"    f"$\\mathcal{{B}}_{{phot}} = 8.75 GeV$^{{-1}}$cm$^{{-2}}$g")
 
         ax.text(0.65, 0.95, box_content0,
                 transform=ax.transAxes,
@@ -2576,7 +2576,7 @@ class integrated_analysis():
 
 
         box_content1 = (f"$\\mathcal{{P}}_{{edep}} = A_{{edep}} e^{{-B_{{edep}} Q_{{Seitz}}}}$\n"
-                        f"\\mathcal{{A}}_{{edep}} = 0.13 MeV$^{{-1}}$\n"    f"\\mathcal{{B}}_{{edep}} = 8.75 GeV$^{{-1}}$cm$^{{-2}}$g")
+                        f"$\\mathcal{{A}}_{{edep}} = 0.13 MeV$^{{-1}}$\n"    f"$\\mathcal{{B}}_{{edep}} = 8.75 GeV$^{{-1}}$cm$^{{-2}}$g")
 
 
         ax.text(0.30, 0.95, box_content1,
