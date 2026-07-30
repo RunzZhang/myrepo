d5c541a3b8cfb51c1229f939c0f097068fc0cea8
RunzZhang <52052572+RunzZhang@users.noreply.github.com>
2026-07-30 11:23:59 -0700

    Update Wrapup_analysis_v2.py

--- Backgrounds/Wrapup_analysis_v2.py
+++ Backgrounds/Wrapup_analysis_v2.py
@@ -3091,8 +3091,8 @@ class integrated_analysis():
                 #            yerr=self.df_Ba_116_plot[y_cfg["y_err"]], label="Ba 116K", fmt='o')
                 ax_ij.errorbar(self.df_Hot_Cs_116_plot[x_cfg["x"]], self.df_Hot_Cs_116_plot[y_cfg["y"]],
                            yerr=self.df_Hot_Cs_116_plot[y_cfg["y_err"]], label="Hot Cs 116K", fmt='o',markersize=8,color = "purple")
-                ax_ij.errorbar(self.df_Th_116_plot[x_cfg["x"]], self.df_Th_116_plot[y_cfg["y"]],
-                           yerr=self.df_Th_116_plot[y_cfg["y_err"]], label="Th 116K", fmt='o',markersize=8,color = "brown")
+                # ax_ij.errorbar(self.df_Th_116_plot[x_cfg["x"]], self.df_Th_116_plot[y_cfg["y"]],
+                #            yerr=self.df_Th_116_plot[y_cfg["y_err"]], label="Th 116K", fmt='o',markersize=8,color = "brown")
                 # ax_ij.plot(self.df_Ba_116_plot[x_cfg["x"]], self.df_Ba_116_plot[y_cfg["y"]],
                 #                label="Ba 116K 95% CL \nUpper Limit", marker='v',linestyle='None')
 
