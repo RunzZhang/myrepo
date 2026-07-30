946e9fd2c97ea54621a5a5dcb1926f3acd085dda
RunzZhang <52052572+RunzZhang@users.noreply.github.com>
2026-07-30 10:39:04 -0700

    pause the Th and hot cs result, see if the bkg subtracted rate

--- Backgrounds/Wrapup_analysis_v2.py
+++ Backgrounds/Wrapup_analysis_v2.py
@@ -17,13 +17,15 @@ class integrated_analysis():
         # self.Cs_sim_path = '/lzdata/runzezhang/result/GR_sims/Cs_output_5E6.pkl'
         self.Co_sim_path = '/lzdata/runzezhang/result/GR_sims/Co_output_5E6_ERv2.pkl'
         self.Cs_sim_path = '/lzdata/runzezhang/result/GR_sims/Cs_output_5E6_ERv2.pkl'
-        self.Hot_Cs_sim_path = '/lzdata/runzezhang/result/GR_sims/Hot_Cs_output_5E6_ERv2.pkl'
+        # self.Hot_Cs_sim_path = '/lzdata/runzezhang/result/GR_sims/Hot_Cs_output_5E6_ERv2.pkl'
+        self.Hot_Cs_sim_path = '/lzdata/runzezhang/result/GR_sims/Cs_output_5E6_ERv2.pkl'
         self.Cf_simA_path = '/data/runzezhang/result/TN_sims_D/Cf_output_1E7_config_A.pkl'
         # self.Cf_simB_path = '/data/runzezhang/result/TN_sims_D/Cf_output_1E7_config_B.pkl'
         self.Cf_simB_path = '/data/runzezhang/result/TN_sims_D/Cf_output_1E7_density095_config_B.pkl'
         # self.Ba_sim_path = '/lzdata/runzezhang/result/GR_sims/Ba_output_5E6.pkl' # updated tracking, make poststep prestep matching, but still recording photon energy loss
         self.Ba_sim_path = '/lzdata/runzezhang/result/GR_sims/Ba_output_5E6_ERv2.pkl'  # updated ER calculation
-        self.Th_sim_path = '/lzdata/runzezhang/result/GR_sims/Th_output_5E6_ERv2.pkl'
+        # self.Th_sim_path = '/lzdata/runzezhang/result/GR_sims/Th_output_5E6_ERv2.pkl'
+        self.Th_sim_path = '/lzdata/runzezhang/result/GR_sims/Cs_output_5E6_ERv2.pkl'
 
         # doped
         # self.Co_sim_doped_path = '/data/runzezhang/result/GR_sims/Co_doped_output.pkl'
@@ -34,8 +36,10 @@ class integrated_analysis():
         self.Co_sim_doped_path = '/lzdata/runzezhang/result/GR_sims/Co_doped_output_full_track.pkl'
         self.Cs_sim_doped_path = '/lzdata/runzezhang/result/GR_sims/Cs_doped_output_full_track.pkl'
         self.Ba_sim_doped_path = '/lzdata/runzezhang/result/GR_sims/Ba_doped_output_full_track.pkl'
-        self.Th_sim_doped_path = '/lzdata/runzezhang/result/GR_sims/Th_doped_output_full_track.pkl'
-        self.Hot_Cs_sim_doped_path = '/lzdata/runzezhang/result/GR_sims/Hot_Cs_doped_output_full_track.pkl'
+        # self.Th_sim_doped_path = '/lzdata/runzezhang/result/GR_sims/Th_doped_output_full_track.pkl'
+        # self.Hot_Cs_sim_doped_path = '/lzdata/runzezhang/result/GR_sims/Hot_Cs_doped_output_full_track.pkl'
+        self.Th_sim_doped_path = '/lzdata/runzezhang/result/GR_sims/Ba_doped_output_full_track.pkl'
+        self.Hot_Cs_sim_doped_path = '/lzdata/runzezhang/result/GR_sims/Ba_doped_output_full_track.pkl'
 
         self.xe_shell_threshold = 0
 
