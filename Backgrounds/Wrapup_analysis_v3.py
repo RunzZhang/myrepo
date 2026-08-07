9d416f2e3dd654dcac78008aee7e906a3cb11b7e
RunzZhang <52052572+RunzZhang@users.noreply.github.com>
2026-08-07 09:47:26 -0700

    Update Wrapup_analysis_v3.py

--- Backgrounds/Wrapup_analysis_v3.py
+++ Backgrounds/Wrapup_analysis_v3.py
@@ -728,9 +728,8 @@ class integrated_analysis():
             self.sim_doped_list = self.gamma_source_group[source]["sim"]["doped_data"]
         except:
             print("NA sources")
-        if source =="Cs":
-            print(row,row.isna().any().any())
-        if not row.empty:
+        
+        if not row.isna().any().any():
             self.Rate_factor = self.sim_list[0]
 
             # print("self.Rate_factor",self.Rate_factor)
