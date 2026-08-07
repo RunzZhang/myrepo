a2f974741c5a2d8b9caa3df0efc2e7a20f021e9d
RunzZhang <52052572+RunzZhang@users.noreply.github.com>
2026-08-07 16:00:36 -0700

    Update Wrapup_analysis_v3.py

--- Backgrounds/Wrapup_analysis_v3.py
+++ Backgrounds/Wrapup_analysis_v3.py
@@ -133,7 +133,7 @@ class integrated_analysis():
         # self.bkg_subtracted_analysis()
         self.bkg_subtracted_analysis(plot=True)
 
-        self.gamma_rejection_plot_v3()
+        # self.gamma_rejection_plot_v3()
 
 
 
