14a3741b55e4624c9c10da3c801acef11ecb4fed
RunzZhang <52052572+RunzZhang@users.noreply.github.com>
2026-08-06 19:50:39 -0700

    Update Wrapup_analysis_v3.py

--- Backgrounds/Wrapup_analysis_v3.py
+++ Backgrounds/Wrapup_analysis_v3.py
@@ -96,7 +96,7 @@ class integrated_analysis():
                                         "Background-11_26-30_fiducial_mix", "Background-12_5-8_fiducial_mix",
                                         'Background-1_12-13_fiducial_mix','Background-1_17-20_fiducial_mix'],"sorted_path":[],"average_path":"", "full_info_path":""},
                                    "119K": { "raw_path": ["Background-1_30-2_2_fiducial_mix","Background-2_17-20-QUIET_fiducial_mix",
-                                               "Background-2_6-12_fiducial_mix","Background-2_28-3_2_fiducial_mix"],"sorted_path":[],"average_path":"", "full_info_path":""},
+                                               "Background-2_6-12_fiducial_mix"],"sorted_path":[],"average_path":"", "full_info_path":""},
                                      "total": {"raw_path": [],
                                                "sorted_path": [],"average_path":"", "full_info_path":""}}
         # if self.volume_option=="":
