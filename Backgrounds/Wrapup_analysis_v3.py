26bf9b6fa16035ec873912b6eae11b7f6c5ef88a
RunzZhang <52052572+RunzZhang@users.noreply.github.com>
2026-08-07 16:39:25 -0700

    Update Wrapup_analysis_v3.py

--- Backgrounds/Wrapup_analysis_v3.py
+++ Backgrounds/Wrapup_analysis_v3.py
@@ -229,7 +229,7 @@ class integrated_analysis():
             exposure_df = exposure_df[
                 (exposure_df['Lifetime [s]'] <= 6.92e-1) | (exposure_df['Lifetime [s]'] >= 6.94e-1)]
             exposure_df = exposure_df[
-                (exposure_df['Lifetime Error [s]'] / exposure_df['Lifetime [s]'] <= 0.3)]
+                (exposure_df['Lifetime Error [s]'] / exposure_df['Lifetime [s]'] <= 0.4)]
             exposure_df['Bkg Rate [mHz]'] = 1000 / exposure_df['Lifetime [s]']
             exposure_df['Bkg Rate Sigma [mHz]'] = exposure_df['Lifetime Error [s]'] * 1000 / (
                 exposure_df['Lifetime [s]']) ** 2
