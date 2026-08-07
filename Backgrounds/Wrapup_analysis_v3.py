7090a9ff866fea95fee1e92383dd3e40b9478463
RunzZhang <52052572+RunzZhang@users.noreply.github.com>
2026-08-07 09:15:38 -0700

    Update Wrapup_analysis_v3.py

--- Backgrounds/Wrapup_analysis_v3.py
+++ Backgrounds/Wrapup_analysis_v3.py
@@ -383,7 +383,7 @@ class integrated_analysis():
 
                         exp_df = pd.read_csv(temp_config["rate_path"][sorted_path_index])
                         columns_added = exp_df.apply(self.calculate_rejection_by_row_v2, axis=1, args=(source,))
-                        if source=="Cs" and sorted_path_index==3:
+                        if source=="Cs" and sorted_path_index==2:
                             print("export Rejection Cs3",exp_df,columns_added )
                         merged_df_rejection = pd.concat([exp_df, columns_added], axis=1)
                         # print('Cs print(merged_df)',self.Cs_exp_rate_path[i],'\n',merged_df)
