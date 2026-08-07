7c3490b291a4c3bc04755b541c1c35a2b3d580bc
RunzZhang <52052572+RunzZhang@users.noreply.github.com>
2026-08-07 16:26:40 -0700

    Update Wrapup_analysis_v3.py

--- Backgrounds/Wrapup_analysis_v3.py
+++ Backgrounds/Wrapup_analysis_v3.py
@@ -441,8 +441,8 @@ class integrated_analysis():
 
 
         if plot:
-            ax.set_xlim(0.4, 3.6)
-            ax.set_ylim(5, 55)
+            # ax.set_xlim(0.4, 3.6)
+            # ax.set_ylim(5, 55)
             ax.set_xlabel("Seitz [keV]")
             ax.set_ylabel("Bkg Rate [mHz]")
             ax.legend()
