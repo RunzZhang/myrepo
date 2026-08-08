d27c1d9ca353957d355b6de97a9cd894f788f1b7
RunzZhang <52052572+RunzZhang@users.noreply.github.com>
2026-08-07 21:02:11 -0700

    Update Wrapup_analysis_v3.py

--- Backgrounds/Wrapup_analysis_v3.py
+++ Backgrounds/Wrapup_analysis_v3.py
@@ -425,8 +425,7 @@ class integrated_analysis():
                                             fmt='o')
 
                             # cacluate rejection
-                            if source=="Co" and sorted_path_index==0:
-                                print("source Co", merged_df)
+
                             exp_df = pd.read_csv(temp_config["rate_path"][sorted_path_index])
 
                             columns_added = exp_df.apply(self.calculate_rejection_by_row_v2, axis=1, args=(source,))
@@ -435,6 +434,8 @@ class integrated_analysis():
 
                             merged_df_rejection = pd.concat([exp_df, columns_added], axis=1)
                             # print('Cs print(merged_df)',self.Cs_exp_rate_path[i],'\n',merged_df)
+                            if source=="Co" and sorted_path_index==0:
+                                print("source Co", merged_df_rejection)
                             merged_df_rejection.to_csv(temp_config["rejection_path"][sorted_path_index], index=False)
                         else:
                             exposure_df.to_csv(temp_config["rate_path"][sorted_path_index], index=False)
