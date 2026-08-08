a24ddd6bf05f85ae998478cac091959e5ca5d25b
RunzZhang <52052572+RunzZhang@users.noreply.github.com>
2026-08-07 20:06:10 -0700

    Update Wrapup_analysis_v3.py

--- Backgrounds/Wrapup_analysis_v3.py
+++ Backgrounds/Wrapup_analysis_v3.py
@@ -2764,6 +2764,7 @@ class integrated_analysis():
 
 
 if __name__=="__main__":
+    IA = integrated_analysis(volume="")
     IA =  integrated_analysis(volume="dome")
     # IA = integrated_analysis(volume="bulk")
     # test = test_csv()
