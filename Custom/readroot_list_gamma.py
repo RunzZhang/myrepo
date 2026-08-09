3bed49acfcb8aa77ec41b97326649c1fcfa99ba5
RunzZhang <52052572+RunzZhang@users.noreply.github.com>
2026-08-08 18:37:52 -0700

    Update readroot_list_gamma.py

--- Custom/readroot_list_gamma.py
+++ Custom/readroot_list_gamma.py
@@ -230,7 +230,14 @@ class ReadRoot():
         self.z_range = [0, 0]
 
         self.filepath = self.base_path + f"dmx_{self.source}_{i}.root"
-        self.file = uproot.open(self.filepath)["tree"]
+        self.filepath_backup = self.base_path + f"dmx_Co_{i}.root"
+        try:
+            self.file = uproot.open(self.filepath)["tree"]
+        except:
+            try:
+                self.file = uproot.open(self.filepath_backup)["tree"]
+            except:
+                print("no files")
 
         print("columns: ", self.file.keys())
         # ['Event', 'name', 'Parent ID', 'Track ID', 'Step ID', 'X/mm', 'Y/mm', 'Z/mm', 'Kinetic/MeV', 'Recoiled/MeV', 'Volume', 'Process']
