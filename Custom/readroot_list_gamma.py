4738624e7ba69c2a40ff27778fb75671d28e1c85
RunzZhang <52052572+RunzZhang@users.noreply.github.com>
2026-08-08 18:58:49 -0700

    Update readroot_list_gamma.py

--- Custom/readroot_list_gamma.py
+++ Custom/readroot_list_gamma.py
@@ -229,7 +229,7 @@ class ReadRoot():
         self.y_range = [0, 0]
         self.z_range = [0, 0]
 
-        # self.filepath = self.base_path + f"dmx_{self.source}_{i}.root"
+        self.filepath = self.base_path + f"dmx_{self.source}_{i}.root"
         self.filepath_backup = self.base_path + f"dmx_Co_1E7_{i}.root"
         try:
             self.file = uproot.open(self.filepath)["tree"]
