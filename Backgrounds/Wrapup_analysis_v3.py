cd8c05eafef0582715d7f89fdf5c04c7f5ad4f7f
RunzZhang <52052572+RunzZhang@users.noreply.github.com>
2026-08-17 11:24:46 -0700

    Update Wrapup_analysis_v3.py

--- Backgrounds/Wrapup_analysis_v3.py
+++ Backgrounds/Wrapup_analysis_v3.py
@@ -17,7 +17,7 @@ class integrated_analysis():
         # self.Cs_sim_path = '/lzdata/runzezhang/result/GR_sims/Cs_output_5E6.pkl'
         self.base_path = "/lzdata/runzezhang/result/GR_sims/"
         self.color_code = {"Cs":"green", "Co": "cyan", "Ba": "orange", "Th":"brown", "Hot_Cs":"gray"}
-        self.N_cut = 5
+        self.N_cut = 0
         self.bkg_uncertainty_cut = {"":0.3, "dome":0.15, "bulk":0.2}
         self.exp_uncertainty_cut = {"":0.3, "dome":0.3, "bulk":0.4}
         self.volume_option =  volume
@@ -200,11 +200,11 @@ class integrated_analysis():
                         # exposure_df.columns = ['Pressure [bara]', 'Lifetime [s]', 'Lifetime Error [s]',
                         #                        'Exponential Fit 2xNLL',
                         #                        'N.d.o.f.', 'Time Cut High [s]', 'Time Cut Low [s]']
-                        # exposure_df = exposure_df[
-                        #     (exposure_df['Lifetime [s]'] <= 6.92e-1) | (exposure_df['Lifetime [s]'] >= 6.94e-1)]
+                        exposure_df = exposure_df[
+                            (exposure_df['Lifetime [s]'] <= 6.92e-1) | (exposure_df['Lifetime [s]'] >= 6.94e-1)]
 
-                        # exposure_df = exposure_df[
-                        #     (exposure_df['Lifetime Error [s]'] / exposure_df['Lifetime [s]'] <= self.exp_uncertainty_cut[self.volume_option])]
+                        exposure_df = exposure_df[
+                            (exposure_df['Lifetime Error [s]'] / exposure_df['Lifetime [s]'] <= self.exp_uncertainty_cut[self.volume_option])]
                         # add rate column
                         exposure_df['Exp Rate [mHz]'] = 1000 / exposure_df['Lifetime [s]']
                         exposure_df['Exp Rate Sigma [mHz]'] = exposure_df['Lifetime Error [s]'] * 1000 / (
@@ -234,11 +234,11 @@ class integrated_analysis():
             exposure_df = self.read_exposure(self.background_group["total"]["raw_path"][i] + ".txt", volume=self.volume_option)
             # exposure_df.columns = ['Pressure [bara]', 'Lifetime [s]', 'Lifetime Error [s]', 'Exponential Fit 2xNLL',
             #                        'N.d.o.f.', 'Time Cut High [s]', 'Time Cut Low [s]']
-            # exposure_df = exposure_df[
-            #     (exposure_df['Lifetime [s]'] <= 6.92e-1) | (exposure_df['Lifetime [s]'] >= 6.94e-1)]
+            exposure_df = exposure_df[
+                (exposure_df['Lifetime [s]'] <= 6.92e-1) | (exposure_df['Lifetime [s]'] >= 6.94e-1)]
 
-            # exposure_df = exposure_df[
-            #     (exposure_df['Lifetime Error [s]'] / exposure_df['Lifetime [s]'] <= self.bkg_uncertainty_cut[self.volume_option])]
+            exposure_df = exposure_df[
+                (exposure_df['Lifetime Error [s]'] / exposure_df['Lifetime [s]'] <= self.bkg_uncertainty_cut[self.volume_option])]
 
             exposure_df['Bkg Rate [mHz]'] = 1000 / exposure_df['Lifetime [s]']
             exposure_df['Bkg Rate Sigma [mHz]'] = exposure_df['Lifetime Error [s]'] * 1000 / (
