bc91436bbbc0e928206f5b33dfabe6adbd4ade3f
RunzZhang <52052572+RunzZhang@users.noreply.github.com>
2026-08-17 13:44:29 -0700

    Update Wrapup_analysis_v3.py

--- Backgrounds/Wrapup_analysis_v3.py
+++ Backgrounds/Wrapup_analysis_v3.py
@@ -1708,6 +1708,7 @@ class integrated_analysis():
 
         # plot the fitting lines
         self.Cs_df = pd.concat(self.df_Cs_116_plot, ignore_index=True)
+        self.Cs_df = self.df_Cs_116_plot
         [result_Q_scatter, result_Q_keV, result_Q_xe, result_Eion_scatter, result_Eion_keV, result_Eion_xe,
          result_Q2_xe, result_Q_rate] = self.fitting_gamma_rejection_v2(self.Cs_df)
 
