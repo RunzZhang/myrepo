7baae0d8c11fd1f2fab70fb7a5e52e69026ab5e2
RunzZhang <52052572+RunzZhang@users.noreply.github.com>
2026-08-17 12:16:45 -0700

    Update Wrapup_analysis_v3.py

--- Backgrounds/Wrapup_analysis_v3.py
+++ Backgrounds/Wrapup_analysis_v3.py
@@ -3548,7 +3548,7 @@ class cross_plot_fiducial_volumes():
 
 if __name__=="__main__":
     # IA = integrated_analysis(volume="")
-    IA =  integrated_analysis(volume="dome")
+    # IA =  integrated_analysis(volume="dome")
     IA = integrated_analysis(volume="bulk")
     # test = test_csv()
     # plot = cross_plot_fiducial_volumes(pressure_plot=True)
