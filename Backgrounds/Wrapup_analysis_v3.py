de388c0efb1b4c886b0705807a911e79521ad518
RunzZhang <52052572+RunzZhang@users.noreply.github.com>
2026-08-17 13:34:14 -0700

    Update Wrapup_analysis_v3.py

--- Backgrounds/Wrapup_analysis_v3.py
+++ Backgrounds/Wrapup_analysis_v3.py
@@ -2698,6 +2698,8 @@ class integrated_analysis():
 
                     # Update rate values where condition is met
                     df_combined.loc[cond, rate_col] = upper_limit
+        else:
+            df_combined = df[df["Clean Rate [mHz]"]>0]
         return df_combined
 
     def doped_gamma_rejection_plot(self):
