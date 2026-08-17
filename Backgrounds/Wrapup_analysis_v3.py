1c344a5c42d98f63db0543100901dc5f6daee70f
RunzZhang <52052572+RunzZhang@users.noreply.github.com>
2026-08-17 12:59:07 -0700

    Update Wrapup_analysis_v3.py

--- Backgrounds/Wrapup_analysis_v3.py
+++ Backgrounds/Wrapup_analysis_v3.py
@@ -1649,6 +1649,7 @@ class integrated_analysis():
                         self.fitting_list.append(df_fit)
                         self.Cs_fitting_list.append(df_fit)
         self.df_Cs_116_plot = pd.concat(self.df_Cs_116_plot_list, ignore_index=True)
+        print("sum Cs 116", self.df_Cs_116_plot)
         self.df_Cs_119_plot = pd.concat(self.df_Cs_119_plot_list, ignore_index=True)
         self.df_Cs_116_plot = self.concat_PT_condition(self.df_Cs_116_plot)
 
