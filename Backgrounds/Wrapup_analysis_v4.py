c72ecea590ff37489a7db4d261effca447bfd342
RunzZhang <52052572+RunzZhang@users.noreply.github.com>
2026-08-21 07:59:34 -0700

    Update Wrapup_analysis_v4.py

--- Backgrounds/Wrapup_analysis_v4.py
+++ Backgrounds/Wrapup_analysis_v4.py
@@ -2920,7 +2920,7 @@ class integrated_analysis():
                 # 4. Final filter to ensure no remaining values are <= 0 (e.g. if sigma was 0)
                 df_combined = df_combined[df_combined["Clean Rate [mHz]"] > 0]
         else:
-            cond = (df_combined["Clean Rate [mHz]"] > 0) and (
+            cond = (df_combined["Clean Rate [mHz]"] > 0) & (
                     (df_combined["Clean Rate [mHz]"] - df_combined["Clean Rate Sigma [mHz]"]) > 0
             )
             df_combined = df_combined[cond]
