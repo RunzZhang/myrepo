5e5ddd75ca5d8bb783ed8d9c6cf4467dbcb4cc68
RunzZhang <52052572+RunzZhang@users.noreply.github.com>
2026-08-21 09:20:43 -0700

    Update Wrapup_analysis_v4.py

--- Backgrounds/Wrapup_analysis_v4.py
+++ Backgrounds/Wrapup_analysis_v4.py
@@ -2920,12 +2920,12 @@ class integrated_analysis():
                 # 4. Final filter to ensure no remaining values are <= 0 (e.g. if sigma was 0)
                 df_combined = df_combined[df_combined["Clean Rate [mHz]"] > 0]
         else:
-            print("PT concat upperlimit faLse", df_combined[["Clean Rate [mHz]","Clean Rate Sigma [mHz]"]] )
+            print("PT concat upperlimit faLse", df_combined[['Seitz Threshold [keV]',"Clean Rate [mHz]","Clean Rate Sigma [mHz]"]] )
             cond = (df_combined["Clean Rate [mHz]"] > 0) & (
                     (df_combined["Clean Rate [mHz]"] - df_combined["Clean Rate Sigma [mHz]"]) > 0
             )
             df_combined = df_combined[cond]
-            print("PT concat upperlimit faLse after", df_combined[["Clean Rate [mHz]", "Clean Rate Sigma [mHz]"]])
+            print("PT concat upperlimit faLse after", df_combined[['Seitz Threshold [keV]',"Clean Rate [mHz]", "Clean Rate Sigma [mHz]"]])
         return df_combined
 
     def doped_gamma_rejection_plot(self):
