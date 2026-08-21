60171d6222f34ac45cf6af67d7eab7892e4c4c00
RunzZhang <52052572+RunzZhang@users.noreply.github.com>
2026-08-21 08:47:16 -0700

    Update Wrapup_analysis_v4.py

--- Backgrounds/Wrapup_analysis_v4.py
+++ Backgrounds/Wrapup_analysis_v4.py
@@ -1823,9 +1823,9 @@ class integrated_analysis():
         # print("sum Cs 116", self.df_Cs_116_plot[['Seitz Threshold [keV]',
         #                                          "Rejection Rate KeV[/keV]", "Rejection Sigma KeV[/keV]",'Eion_rl-1_rhol-1 [GeVcm**2 g-1]']])
         #
-        # print("sum Cs 119", self.df_Cs_119_plot[['Seitz Threshold [keV]',
-        #                                          "Rejection Rate KeV[/keV]", "Rejection Sigma KeV[/keV]",
-        #                                          'Eion_rl-1_rhol-1 [GeVcm**2 g-1]']])
+        print("sum Cs 119", self.df_Cs_119_plot[['Seitz Threshold [keV]',
+                                                 "Rejection Rate KeV[/keV]", "Rejection Sigma KeV[/keV]",
+                                                 'Eion_rl-1_rhol-1 [GeVcm**2 g-1]']])
         # print("columns", self.df_Cs_119_plot["Rejection Rate KeV[/keV]"].iloc[0],
         #       self.df_Cs_119_plot["Rejection Rate Xenon Abs[]"].iloc[0])
 
