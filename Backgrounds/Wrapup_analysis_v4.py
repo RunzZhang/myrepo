b5f699fb6981afe44764313360f9a19fa64ba98e
RunzZhang <52052572+RunzZhang@users.noreply.github.com>
2026-08-20 20:27:26 -0700

    Update Wrapup_analysis_v4.py

--- Backgrounds/Wrapup_analysis_v4.py
+++ Backgrounds/Wrapup_analysis_v4.py
@@ -1824,13 +1824,13 @@ class integrated_analysis():
         print("sum Cs 119", self.df_Cs_119_plot[['Seitz Threshold [keV]',
                                                  "Rejection Rate KeV[/keV]", "Rejection Sigma KeV[/keV]",
                                                  'Eion_rl-1_rhol-1 [GeVcm**2 g-1]']])
-        print("columns", self.df_Cs_119_plot["Rejection Rate KeV[/keV]"],
-              self.df_Cs_119_plot["Rejection Rate Xenon Abs[]"])
+        print("columns", self.df_Cs_119_plot["Rejection Rate KeV[/keV]"].iloc[0],
+              self.df_Cs_119_plot["Rejection Rate Xenon Abs[]"].iloc[0])
 
         # ratio_116 = self.df_Cs_116_plot["Rejection Rate KeV[/keV]"][0] / \
         #             self.df_Cs_116_plot["Rejection Rate Xenon Abs[]"][0]
-        ratio_119 = self.df_Cs_119_plot["Rejection Rate KeV[/keV]"][0] / \
-                    self.df_Cs_119_plot["Rejection Rate Xenon Abs[]"][0]
+        ratio_119 = self.df_Cs_119_plot["Rejection Rate KeV[/keV]"].iloc[0] / \
+                    self.df_Cs_119_plot["Rejection Rate Xenon Abs[]"].iloc[0]
 
         print("columns",self.df_Cs_119_plot["Rejection Rate KeV[/keV]"][0], self.df_Cs_119_plot["Rejection Rate Xenon Abs[]"][0])
 
