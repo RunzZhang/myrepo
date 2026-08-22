e0e4b92345c993de951689e900990d312548d054
RunzZhang <52052572+RunzZhang@users.noreply.github.com>
2026-08-21 17:43:03 -0700

    Update Wrapup_analysis_v4.py

--- Backgrounds/Wrapup_analysis_v4.py
+++ Backgrounds/Wrapup_analysis_v4.py
@@ -2460,11 +2460,11 @@ class integrated_analysis():
 
         # compare to Drexel
         plt.tight_layout()
-        plt.show()
-        # if rate_cut==False:
-        #     plt.savefig(self.plot_path + f"gamma_rejection{self.volume_option}_{radi_source}_PSN_v2.pdf")
-        # else:
-        #     plt.savefig(self.plot_path + f"gamma_rejection{self.volume_option}_{radi_source}_PSN_ratecut_v2.pdf")
+        # plt.show()
+        if rate_cut==False:
+            plt.savefig(self.plot_path + f"gamma_rejection{self.volume_option}_{radi_source}_PSN_v2.pdf")
+        else:
+            plt.savefig(self.plot_path + f"gamma_rejection{self.volume_option}_{radi_source}_PSN_ratecut_v2.pdf")
         # if rate_cut==False:
         #     plt.savefig(self.plot_path + f"gamma_rejection{self.volume_option}_{radi_source}_PSN_v2.png")
         # else:
