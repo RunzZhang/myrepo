8fe3bcf43b6f6b66ad4c13a2be01583df29c580c
RunzZhang <52052572+RunzZhang@users.noreply.github.com>
2026-08-21 17:50:12 -0700

    Update Wrapup_analysis_v4.py

--- Backgrounds/Wrapup_analysis_v4.py
+++ Backgrounds/Wrapup_analysis_v4.py
@@ -1894,8 +1894,8 @@ class integrated_analysis():
         # Set main (left) y-axis and x-axis labels
         ax[0, 0].set_xlabel(r"Seitz threshold [keV]", fontsize=16)
 
-        # ax[0, 0].set_xlim(0.50, 4.2)
-        ax[0, 0].set_xlim(0.40, 2.8)
+        ax[0, 0].set_xlim(0.50, 4.2)
+        # ax[0, 0].set_xlim(0.40, 2.8)
         ax[0, 0].set_ylim(2e-11, 1e-4)
         ax[0, 0].set_ylabel("Probability per energy deposited (events/keV) ", fontsize=16)
         ax[0, 0].set_yscale("log")
