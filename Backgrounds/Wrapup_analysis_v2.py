4f51efba97aae57ad951efeca56d8bb179b40f2a
RunzZhang <52052572+RunzZhang@users.noreply.github.com>
2026-08-26 17:00:42 -0700

    Update Wrapup_analysis_v2.py

--- Backgrounds/Wrapup_analysis_v2.py
+++ Backgrounds/Wrapup_analysis_v2.py
@@ -166,9 +166,9 @@ class integrated_analysis():
         # self.plot_spectrum()
         # self.gamma_rejection_plot()
         # self.gamma_rejection_plot_v2()
-        # self.gamma_rejection_plot_v3()
+        self.gamma_rejection_plot_v3()
         # self.gamma_rejection_plot_PSN()
-        self.gamma_rejection_plot_PSN_v2()
+        # self.gamma_rejection_plot_PSN_v2()
         # self.doped_gamma_rejection_plot()
         # self.spectrums_plot()
 
